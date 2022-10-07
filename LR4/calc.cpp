9afc063b7b4c0def1e681c530b32ce8d17feb274
Stas Filimonov <celezen.bol@gmail.com>
2022-10-07 16:55:08 +0300

    LR4/calc.cpp

---
+++ LR4/calc.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int num1, num2,num3;
+	cout << "Введите первое число:"; cin >> num1;
+	cout << "Введите второе число:"; cin >> num2;
+	cout << "Введите третье число:"; cin >> num3;
+	if (!(num3>=0 && num1 >= 0 && num2 >= 0 && num1 > num2)){
+		cout << "Invalid data"<<endl;
+		return 0;
+	}
+	cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+	cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+	cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+	cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+	cout << (double)num1 << " / " << (double)num2 << " = " << (double)num1 / num2 << endl;
+	cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+	cout << num1 << " + " << num2 << " * " << num3 << " = " << num1 + num2 * num3 << endl;
+	cout << "(" << num1 << " + " << num2 << ")" << " * " << num3 << " = " << (num1 + num2) * num3 << endl;
+	return 0;
+}
