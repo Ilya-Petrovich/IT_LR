6f7c43433713769beb4c316a4d60d0311d6299df
Любовь Конькова <113627460+vkidia@users.noreply.github.com>
2022-10-14 21:11:10 +0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello world! WARNING: terminal is not fully functional
+	std::cout << "Hello world! WARNING: terminal is not fully functional";
 Press RETURN to continue";
 	return 0;
 }
