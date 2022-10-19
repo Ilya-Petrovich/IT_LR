206a9b9f53d05734948c73de9ef6e830b6901c96
Dinar Makhmutov <seishun@inbox.ru>
2022-10-19 10:23:09 +0300

    fixing branch step 2

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+<<<<<<< HEAD
 	std::cout << "Hello worlfdd!";
+=======
+	std::cout << "Hello world!";
+>>>>>>> 0236e82 (test.cpp fixed after mistake)
 	return 0;
 }
