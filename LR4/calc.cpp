0ee1d9cec703ff24a025e6ed64c115de66250f89
Ramil Khakimov <ramma19@mail.ru>
2022-11-07 14:01:14 +0300

    LR4

---
+++ LR4/calc.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    int num_1, num_2, num_3;
+
+    cout << "¬ведите первое число: ";
+    cin >> num_1;
+    cout << "¬ведите второе число: ";
+    cin >> num_2;
+    cout << "¬ведите третье число: ";
+    cin >> num_3;
+
+    float div = (float)num_1 / num_2;
+
+    if (num_1 > num_2 && num_1 >= 0 && num_2 >= 0 && num_3 >= 0)
+    {
+        cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
+        cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+        cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
+        cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
+        cout << setprecision(1) << fixed << (float)num_1 << " / " << (float)num_2 << " = ";
+        cout << setprecision(5) << fixed << div << endl;
+        cout << setprecision(0) << fixed;
+        cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
+        cout << num_1 << " + " << num_2 << " * " << num_3 << " = " <<
+            num_1 + num_2 * num_3 << endl;
+        cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " <<
+            (num_1 + num_2) * num_3 << endl;
+
+    }
+    else
+        cout << "Invalid data" << endl;
+    return 0;
+}
