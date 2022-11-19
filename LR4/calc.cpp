8a17c22fc2982dad0c5f5e10f90c645d38e19ad3
nuris123 <akhmetzyanov.n@mail.ru>
2022-11-19 16:12:13 +0300

    LR3 changed,LR4 added

---
+++ LR4/calc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int num_1, num_2, num_3;
+    std::cout << "Enter first number: ";
+    std::cin >> num_1;
+    std::cout << "Enter second number: ";
+    std::cin >> num_2;
+    std::cout << "Enter third number: ";
+    std::cin >> num_3;
+
+    if (num_1 > num_2) {
+        std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
+        std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
+        std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
+        std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
+        std::cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << float(num_1) / float(num_2) << std::endl;
+        std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
+        std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
+        std::cout << "( " << num_1 << " + " << num_2 << " ) " << "* " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
+    }else {
+        std::cout << "Invalid data" << std::endl;
+    }
+}
