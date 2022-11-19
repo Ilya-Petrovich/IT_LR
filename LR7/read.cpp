ead5fa728245e4e9ab6640df6de3e8e8fb434f8b
Dmitry Bannicin <mr.bannicin@gmail.com>
2022-11-19 16:51:59 +0300

    Changed read.cpp

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,16 +1,16 @@
-﻿// NUM1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <iostream>
 #include <bitset>
 
 using namespace std;
-void readText(char* buff, long fileSize, FILE* file) {
+
+FILE* file;
+
+void readText(char* buff, long fileSize) {
 
 	fread(buff, sizeof(char), fileSize, file);
 
-	for (size_t i = fileSize - 20 * 20 * 3; i < 138+83*4; i+=4)
-	{
+	for (size_t i = fileSize - 20 * 20 * 3; i < 138+83*4; i+=4) {
 		char united_byte = 0;
 
 		char firstByte= buff[i] & 3;
@@ -26,13 +26,6 @@ void readText(char* buff, long fileSize, FILE* file) {
 		cout << united_byte;
 	}
 
-	/*cout << endl;
-
-	for (size_t i = fileSize - 20 * 20 * 3; i < fileSize; i ++)
-	{
-		cout << bitset<8> (buff[i]) << " ";
-	}*/
-
 	fclose(file);
 
 	free(buff);
@@ -42,7 +35,6 @@ void readText(char* buff, long fileSize, FILE* file) {
 
 int main()
 {
-	FILE* file;
 
 	long fileSize;
 
@@ -54,16 +46,5 @@ int main()
 
 	char* buff = new char[fileSize]();
 
-	readText(buff, fileSize, file);
+	readText(buff, fileSize);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
