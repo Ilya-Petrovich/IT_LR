9ab0980558d010bb426442de15c9f16fc12ddb13
Nastya Smirnova <nastyaxolod2014@gmail.com>
2022-11-19 19:26:12 +0500

    Changed calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -5,13 +5,13 @@ int main() {
 
 	int num_1, num_2, num_3;
 	
-	std::cout << "Enter the first number: ";
+	std::cout << "Type the first number: ";
 	std::cin >> num_1;
 
-	std::cout << "Enter the second number: ";
+	std::cout << "Type the second number: ";
 	std::cin >> num_2;
 	
-	std::cout << "Enter the third number: ";
+	std::cout << "Type the third number: ";
 	std::cin >> num_3;
 
 	if (num_1 > num_2 && num_1 >= 0 && num_2 > 0 && num_3 >= 0) {	
@@ -19,7 +19,7 @@ int main() {
 		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
 		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
 		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
-		std::cout << num_1 << " / " << num_2 << " = " << (float) num_1 / num_2 << std::endl;
+		std::cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << (float) num_1 / num_2 << std::endl;
 		std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
 		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
 		std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
