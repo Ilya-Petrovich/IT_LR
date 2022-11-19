deb70c876a65da95db9447f080ae81886bb10723
Dinar Nuretdinov <dinar.nuretdinov12@mail.ru>
2022-11-19 21:51:23 +0300

    calc.cpp

---
+++ LR4/calc.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    int num_1, num_2, num_3;
+
+    cout << "Type the first number: "; cin >> num_1;
+    cout << "Type the second number: "; cin >> num_2;
+    cout << "Type the third number: "; cin >> num_3;
+
+    if (num_1 > num_2) {
+        cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
+        cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+        cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
+        cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
+        cout << float(num_1) << ".0 / " << float(num_2) << ".0 = " << float(num_1) / float(num_2) << endl;
+        cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
+        cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
+        cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
+
+    }
+    else {
+        cout << "Invalid data" << endl;
+    }
+    system("pause");
+}
