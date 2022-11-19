082d7ea781b6d0b2dee53a1897ddc97c7a6ee449
Albert Mardanov <mardanov1207@mail.ru>
2022-11-19 19:36:00 +0300

    fix2

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	float fuel, fuel_capacity=50, fuel_consumption=0, speed=0, s, max_speed = 110, needed_distance, distance = 360, distance_travelled, distance_left, time, fuel_consumed, fuel_remained;
+	float fuel, fuel_capacity=50, fuel_consumption=0, speed=0, s, max_speed = 110, distance = 360, distance_travelled, fuel_consumed;
 	cout << "How much fuel do you have at start? "; cin >> fuel;
 	
 	if (fuel < 0 or fuel > 50 or cin.fail()) {
