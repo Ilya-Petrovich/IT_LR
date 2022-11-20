4a6b6a162bedc23619b5d0974ad01e3200caeed8
Nikiforovich Arina <a79656934465@yandex.ru>
2022-11-20 20:04:22 +0300

    LW4

---
+++ LR4/calc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main() {
+	int num_1, num_2, num_3;
+	std::cout << "Введите первое число: ";
+	std::cin >> num_1;
+	std::cout << "Введите второе число: ";
+	std::cin >> num_2;
+	std::cout << "Введите третье число: ";
+	std::cin >> num_3;
+    if (num_1 > num_2) {
+	    std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
+	    std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
+	    std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
+	    std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
+	    std::cout << float(num_1) << " / " << float(num_2) << " = " << float(num_1) / float(num_2) << std::endl;
+	    std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
+	    std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3<< std::endl;
+	    std::cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3<< std::endl;
+} else {
+	std::cout << "Invalid data" << std::endl;
+}
+
+}
---
+++ LR4/game_stat_1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+int main() {
+    double fuelConsumption, s, sd, t;
+	int fuel, speed;
+	std::cout << "How much fuel do you have at start? ";
+	std::cin >> fuel;
+	if (fuel <= 50 && fuel >= 0) {
+	std::cout << "\nCar stats\n";
+	std::cout << "Fuel: " << fuel;
+	std::cout << "\nFuel capacity: 50\n";
+	std::cout << "Fuel consumption: 0\n";
+	std::cout << "Speed: 0\n";
+	std::cout << "Maximum speed: 110\n\n";
+	std::cout << "Route info\n";
+	std::cout << "Distance: 360 km\n";
+	std::cout << "---------------------------------------\n";
+	std::cout << "You have to drive a route 360 km long.\n";
+	std::cout << "But first you need to check if you have enough fuel.";
+	if (fuel < 25){
+	    fuel += 25;
+	std::cout << "\n\nYour fuel before start: " << fuel;	
+	}
+	std::cout << "\n\nWhat speed will you choose? " ;
+	std::cin >> speed;
+	if (speed >= 0){
+	if (speed > 110) {
+	    speed = 110;
+	    }
+	std::cout << "Your speed before start: " << speed << " km/h\n";
+	s = float(speed) / 10;
+	if (s != 0){
+	fuelConsumption = s * float(s) / 4 - 4.5 * s + 30;}
+	else{
+	    fuelConsumption = 0;
+	}
+	std::cout << "Fuel consumption with chosen speed: " << fuelConsumption << " litres per 100 km\n\n";
+	std::cout << "Destination result\n";
+	if (fuelConsumption != 0){
+	sd = float(fuel) / fuelConsumption;
+	}
+	else{
+	    sd = 0;
+	}
+	if (sd > 3.6) { 
+	    sd = 3.6;
+	}
+	std::cout << "Distance traveled: " << sd * 100 << " km";
+	if (sd != 0){
+	t = 60 * 60 * (sd * 100 / speed);
+	}
+	else{
+	    t = 0;
+	}
+	std::cout << "\nTime spent: " << int(t) << " sec";
+	std::cout << "\nFuel consumed: " << sd * fuelConsumption << " litres";
+	std::cout << "\nFuel remained: " << fuel - (sd * fuelConsumption) << " litres";
+	std::cout << "\n---------------------------------------\n";
+    	if (sd >= 3.6) {
+    	std::cout << "Congratulations! You have reached destination point!";
+    	}
+    	else{
+    	    std::cout << "You haven't reached destination point. " << 360 - (sd *100) << " km left.";
+    	}
+	}
+	else {
+	std::cout << "Invalid data.";
+}
+	}
+    else {
+	std::cout << "\nInvalid data.";
+}
+
+	
+
+}
