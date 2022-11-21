9ae871d939657894c80e3faefb8c785dfbe4b788
Olga Makeeva <fo1gusha@yandex.ru>
2022-11-21 10:09:32 +0300

    AA

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,13 +1,13 @@
 # include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
 	int num_1, num_2, num_3;
-    cout << "Input first number = "; cin >> num_1;
-    cout << "Input second number = "; cin >> num_2;
-    cout << "Input third number = "; cin >> num_3;
+    cout << "Type first number = "; cin >> num_1;
+    cout << "Type second number = "; cin >> num_2;
+    cout << "Type third number = "; cin >> num_3;
     if(num_1 > num_2 and num_1 >=0 and num_2 > 0 and num_3 >= 0)
     {
         cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
@@ -18,7 +18,8 @@ int main()
         cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
         cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
         cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
-    }else{
+    }
+	else{
         cout << "Invalid data" << endl;
     }
 
