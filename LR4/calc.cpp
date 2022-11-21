2d98c2c47d8a464c507d41959e9736fdf7f015ca
Yaroslav Vokhmyanin <yarik123lol@gmail.com>
2022-11-21 09:14:11 +0300

    calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
 	int num_1, num_2, num_3;
-    cout << "Input first number = "; cin >> num_1;
-    cout << "Input second number = "; cin >> num_2;
-    cout << "Input third number = "; cin >> num_3;
+    cout << "Type the first number = "; cin >> num_1;
+    cout << "Type the second number = "; cin >> num_2;
+    cout << "Type the third number = "; cin >> num_3;
     if(num_1 > num_2 and num_1 >=0 and num_2 > 0 and num_3 >= 0)
     {
         cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
