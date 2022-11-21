5925009d47938f0faf11589577b1841ba3b17480
Leonid Solovyev <dan_backyard07@mail.ru>
2022-11-21 10:39:13 +0300

    translated output

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 int main() {
 	int num_1, num_2, num_3;
-	std::cout << "Введите первое число: "; std::cin >> num_1;
-	std::cout << "Введите второе число: "; std::cin >> num_2;
-	std::cout << "Введите третье число: "; std::cin >> num_3;
+	std::cout << "Type the first number: "; std::cin >> num_1;
+	std::cout << "Type the second number: "; std::cin >> num_2;
+	std::cout << "Type the third number: "; std::cin >> num_3;
 	if (num_1 > num_2) {
 		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
 		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
 		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
 		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
 		std::cout << num_1 << ".0 / " << num_2 << ".0 = " << (float) num_1 / num_2 << std::endl;
+		std::cout << num_1 << " % " << num_2 << " = " <<  num_1 % num_2 << std::endl;
 		std::cout << num_1 << " + " << num_2 << " + " << num_3 << " = " << num_1 + num_2 + num_3 << std::endl;
 		std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
 	} else
