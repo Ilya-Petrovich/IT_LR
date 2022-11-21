d3cdcfed0a8fc72816a6b788662ed0d76d8f4ac5
Amir Arslanov <amir.arslanov.2004@mail.ru>
2022-11-21 15:57:46 +0300

    UP2LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main(){
 	int num_1, num_2, num_3;
-	cout << " num_1 = "; cin >> num_1;
-	cout << " num_2 = "; cin >> num_2;
-	cout << " num_3 = "; cin >> num_3;
+	cout << "Type the first number: "; cin >> num_1;
+	cout << "Type the second number: "; cin >> num_2;
+	cout << "Type the third number: "; cin >> num_3;
 	if (num_1 > num_2){
 	
 	
@@ -15,7 +15,7 @@ int main(){
 	cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 	cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
 	cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-	cout << float(num_1) << " / " << float(num_2) << " = " << float(num_1) / float(num_2) << endl;
+	cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << float(num_1) / float(num_2) << endl;
 	cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
 	
 	cout << num_1 << " + " << num_2 << " * "<< num_3 << " = " << num_1 + num_2 * num_3 << endl;
