718bd20bfde2557a112bacab4c0a0764c3f8a738
Renata Falyakhutdinova <falyakhutdinovarenata@mail.ru>
2022-11-22 18:42:49 +0300

    Changed calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -18,7 +18,7 @@ int main() {
         std::cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << double(num_1) / double(num_2) << std::endl;
         std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
         std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
-        std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * 5 << std::endl;
+        std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
     }
     else {
         std::cout << "Invalid data" << std::endl;
