3a23f9802a093184585870a072119c2d913bc7c6
Khalil Alimzhanov <alimzhanov.2005@list.ru>
2022-11-22 15:18:52 +0300

    Changed LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -6,19 +6,20 @@ int main() {
 	int num_1, num_2, num_3;
     cout << "Type the first number: "; cin >> num_1;
     cout << "Type the second number: "; cin >> num_2;
-    cout << "Type the third number = "; cin >> num_3;
+    cout << "Type the third number: "; cin >> num_3;
 
-    if(num_1 > num_2 and num_1 >=0 and num_2 > 0 and num_3 >= 0)
+    if(num_1 > num_2 and num_1 > 0 and num_2 > 0 and num_3 > 0)
     {
-        cout << "7 + 3 = " << num_1 + num_2 << endl;
-        cout << "7 - 3 = " << num_1 - num_2 << endl;
-        cout << "7 * 3 = "<< num_1 * num_2 << endl;
-        cout << "7 / 3 = "<< num_1 / num_2 << endl;
-        cout << "7.0 / 3.0 = "<< double(num_1) / double(num_2) << endl;
-        cout << "7 % 3 = " << num_1 % num_2 << endl;
-        cout << "7 + 3 * 5 = "<< num_1 + num_2 * num_3 << endl;
-        cout << "(7 + 3) * 5 = "<< (num_1 + num_2) * num_3 << endl;
+        cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
+        cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+        cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
+        cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
+        cout << double(num_1) << ".0" << " / " << double(num_2) << ".0" << " = " << double(num_1) / double(num_2) << endl;
+        cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
+        cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
+        cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
     }else{
         cout << "Invalid data" << endl;
     }
+    return 0;
 }
