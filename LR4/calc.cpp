38505bf4e5324041203bffa5f3387eb4df764560
Amir Rakhmatulov <amir.oren.28@gmail.com>
2022-11-24 17:48:27 +0300

    changed_calc

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -3,11 +3,10 @@
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
 	int n1, n2, n3;
-	std::cout << "¬ведите первое число: "; std::cin >> n1;
-	std::cout << "¬ведите второе число: "; std::cin >> n2;
-	std::cout << "¬ведите третье число: "; std::cin >> n3;
+	std::cout << "Type the first number: "; std::cin >> n1;
+	std::cout << "Type the second number: "; std::cin >> n2;
+	std::cout << "Type the third number: "; std::cin >> n3;
 	
 	if ((n1 >= 0 && n2 >= 0 && n3 >= 0) && (n1 > n2) && (n2 != 0)) {
 		std::cout << n1 << " + " << n2 << " = " << n1 + n2 << "\n";
