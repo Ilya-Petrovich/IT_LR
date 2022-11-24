0aefb7956a79299828963e17731f72ac6821c85f
Leonid Solovyev <dan_backyard07@mail.ru>
2022-11-24 16:41:29 +0300

    second try fixing calc

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 int main() {
 	int num_1, num_2, num_3;
-	std::cout << "Type the first number: "; std::cin >> num_1;
-	std::cout << "Type the second number: "; std::cin >> num_2;
-	std::cout << "Type the third number: "; std::cin >> num_3;
+	std::cout << "Type the first number: "; 
+	std::cin >> num_1;
+	std::cout << "Type the second number: "; 
+	std::cin >> num_2;
+	std::cout << "Type the third number: "; 
+	std::cin >> num_3;
 	if (num_1 > num_2 && num_1 >= 0 && num_2 >= 0 && num_3 >= 0) {
 		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
 		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
@@ -11,10 +14,11 @@ int main() {
 		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
 		std::cout << num_1 << ".0 / " << num_2 << ".0 = " << (float) num_1 / num_2 << std::endl;
 		std::cout << num_1 << " % " << num_2 << " = " <<  num_1 % num_2 << std::endl;
-		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + (num_2 * num_3) << std::endl;
-		std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
+		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = ";
+		std::cout << num_1 + (num_2 * num_3) << std::endl;
+		std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = ";
+		std::cout << (num_1 + num_2) * num_3;
 	} else
-		std::cout << "Invalid data" << std::endl;
-	system("pause");
+		std::cout << "Invalid data";
 	return 0;
 }
