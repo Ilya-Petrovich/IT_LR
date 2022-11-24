4b8a18c081f455ae15e94d65bd39e3ded7be75e3
Fedor Tabakov <fedortab7@gmail.com>
2022-11-24 18:22:26 +0300

    LR4 Changed

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -3,19 +3,19 @@ using namespace std;
 int main()
 {
 	int num_1, num_2, num_3;
-	cout << "Input the first number: ";
+	cout << "Type the first number: ";
 	cin >> num_1;
-	cout << "Input the second number: ";
+	cout << "Type the second number: ";
 	cin >> num_2;
-	cout << "Input the third number: ";
+	cout << "Type the third number: ";
 	cin >> num_3;
-	if (num_1 > num_2 && num_1 >= 0 && num_2 >= 0 && num_3 >= 0 && num_2 != 0)
+	if (num_1 > num_2 && num_1 >= 0 && num_2 > 0 && num_3 >= 0)
 	{
 		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
 		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
 		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-		cout << num_1 << ".0 / " << num_2 << ".0 = " << (float)num_1 / (float)num_2 << endl;
+		cout << num_1 << ".0 / " << num_2 << ".0 = " << (double)num_1 / num_2 << endl;
 		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
 		cout << num_1 << " + " << num_2 << " * " << num_3 << " = "  << num_1 + num_2 * num_3 << endl;
 		cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
