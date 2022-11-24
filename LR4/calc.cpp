1b2db7dc9ee62a7b2db2ada411ea46c8bb7dbdc3
Anna Abdullina <Abdullina.Anna.2004@yandex.ru>
2022-11-24 20:55:40 +0500

    Calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main() {
     int num_1, num_2, num_3;
+
     cout << "Type the first number: ";
     cin >> num_1;
     cout << "Type the second number: ";
     cin >> num_2;
     cout << "Type the third number: ";
     cin >> num_3;
+
     if ((num_1 > num_2) and num_1 >= 0 and num_2 > 0 and num_3 >= 0) {
         cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
         cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
@@ -24,5 +26,5 @@ int main() {
     else {
         cout << "Invalid data" << endl;
         return 0;
-    } 
+    }
 }
