22a177823f361042d5129408489c6e0187519681
Nikiforovich Arina <a79656934465@yandex.ru>
2022-11-24 19:14:49 +0300

    LW4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 int main() {
 	int num_1, num_2, num_3;
-	std::cout << "Type the first numbert: ";
+	std::cout << "Type the first number: ";
 	std::cin >> num_1;
 	std::cout << "Type the second number: ";
 	std::cin >> num_2;
-	std::cout << "Type the third number: ";
-	std::cin >> num_3;
+	std::cout << "Type the third number: 5\n";
+	num_3 = 5;
     if (num_1 > num_2) {
 	    std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
 	    std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
