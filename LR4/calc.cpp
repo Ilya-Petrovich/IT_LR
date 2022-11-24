94285eabfd8be7167b532960597bb95667b5652f
Amir Rakhmatulov <amir.oren.28@gmail.com>
2022-11-24 22:59:19 +0300

    z

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,23 +1,29 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-	int n1, n2, n3;
-	std::cout << "Type the first number: "; std::cin >> n1;
-	std::cout << "Type the second number: "; std::cin >> n2;
-	std::cout << "Type the third number: "; std::cin >> n3;
-	
-	if ((n1 > 0 && n2 > 0 && n3 > 0) && (n1 > n2) && (n2 != 0)) {
-		std::cout << n1 << " + " << n3 << " = " << n1 + n3 << "\n";
-		std::cout << n1 << " - " << n3 << " = " << n1 - n3 << "\n";
-		std::cout << n1 << " * " << n3 << " = " << n1 * n3 << "\n";
-		std::cout << n1 << " / " << n3 << " = " << n1 / n3 << "\n";
-		std::cout << std::fixed << std::setprecision(1) << n1 * 1. << " / " << n3 * 1. << " = " << std::fixed << std::setprecision(5) << n1 * 1. / n3 << "\n";
-		std::cout << n1 << " % " << n3 << " = " << n1 % n3 << "\n";
-		std::cout << n1 << " + " << n3 << " * " << n2 << " = " << n1 + n3 * n2 << "\n";
-		std::cout << "(" << n1 << " + " << n3 << ")" << " * " << n2 << " = " << (n1 + n3) * n2 << "\n";
+    int n1, n2, n3;
+	cout << "Type the first number: "; cin >> n1;
+	cout << "Type the second number: "; cin >> n2;
+	cout << "Type the third number: "; cin >> n3;
+	if (n1 > n2) 
+	{
+		if ((n1 >= 0) && (n2 > 0) && (n3 >= 0)) 
+		{
+			cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+			cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
+			cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+			cout << n1 << " / " << n2 << " = " << n1 / n2 << endl;
+			cout << n1 << ".0" << " / " << n2 << ".0" << " = " << n1 * 1. / n2 << endl;
+			cout << n1 << " % " << n2 << " = " << n1 % n2 << endl;
+			cout << n1 << " + " << n2 << " * " << n3 << " = " << n1 + n2 * n3 << endl;
+			cout << "(" << n1 << " + " << n2 << ")" << " * " << n3 << " = " << (n1 + n2) * n3 << endl;
+			
+		} 
+		else cout << "Invalid data" << endl;
 	}
-	else std::cout << "Invalid data";
+	else cout << "Invalid data" << endl;
 	return 0;
 }
