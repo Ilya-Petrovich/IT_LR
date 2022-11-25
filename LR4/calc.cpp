fa0d5be3990dd921496f6a66f542962fcb1f116e
Stas Filimonov <celezen.bol@gmail.com>
2022-11-25 10:30:30 +0300

    LR4/calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -16,6 +16,6 @@ int main() {
 	cout << num1 << ".0" << " / " << num2 << ".0" << " = " << (double)num1 / num2 << endl;
 	cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
 	cout << num1 << " + " << num2 << " * " << num3 << " = " << num1 + num2 * num3 << endl;
-	cout << "(" << num1 << " + " << num2 << ")" << " * " << num3 << " = " << (num1 + num2) * num3;
+	cout << "(" << num1 << " + " << num2 << ")" << " * " << num3 << " = " << (num1 + num2) * num3 << endl;
 	return 0;
 }
