5bc886025b8738127468136e657e87b1ab8e8635
Stas Filimonov <celezen.bol@gmail.com>
2022-11-25 09:39:47 +0300

    LR4/calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -6,7 +6,7 @@ int main() {
 	cout << "Type the first number: "; cin >> num1;
 	cout << "Type the second number: "; cin >> num2;
 	cout << "Type the third number: "; cin >> num3;
-	if (!(num1 > num2 && num2 != 0 && num1 * num2 * num3 > 0)) {
+	if (!(num1 > num2&& num1 >= 0 && num2 > 0 && num3 >= 0)) {
 		cout << "Invalid data";
 		return 0;
 	}
