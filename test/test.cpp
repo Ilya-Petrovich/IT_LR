ff7ec97d0b19ec3dede706b4ae951b7c2005cf15
Ideliya Safiullina <ideliya3004@gmail.com>
2022-11-25 17:12:33 +0300

    Fixed test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
 int main() {
-	while(1) {
-		int* i = new int[1]();
-		std::cout << "Hello world! " << i << std::endl;
-	}
+	std::cout << "Hello world! " << std::endl;
 	return 0;
 }
