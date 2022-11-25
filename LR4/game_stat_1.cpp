641a5db9c4a0cfd422de0ca268609542ce8076a5
Khalil Alimzhanov <alimzhanov.2005@list.ru>
2022-11-25 15:52:53 +0300

    LR4

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -20,7 +20,7 @@ int main(){
 
         //// *************** Route info *********************************
         cout << "\nRoute info\nDistance: " << distance << " km" << endl;
-        cout << "_________________________________" << endl;
+        cout << "-----------------------------------" << endl;
         cout << "You have to drive a route 360 km long.\nBut first you need to checkif you have enough fuel." << endl;
         
         if (fuel < (fuel_capacity / 2.)) {
@@ -30,7 +30,7 @@ int main(){
 
         cout << "\nWhat speed will you chose? ";
         cin >> speed;
-        
+
     }else{
         cout << "\nInvalid data.";
         return 0;
@@ -71,10 +71,10 @@ int main(){
         cout << "Time spent: " << time << " sec" << endl;
         cout << "Fuel consumed: " << f_consumed << " litres" << endl;
         cout << "Fuel remained: " << f_remained << " litres" << endl;
-        cout << "__________________________" << endl;
+        cout << "------------------------------" << endl;
         
         if (dist_t >= distance) {
-            cout << "\nCongratulations! You have reached destination point!" << endl;
+            cout << "Congratulations! You have reached destination point!" << endl;
         } else {
             cout << "You haven't reached destination point. " << distance - dist_t << " km left." << endl;
         }
