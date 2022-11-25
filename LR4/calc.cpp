410932dc531353b511445c36281081ed8eecd81e
ArturGilemov <artur_gilemov@mail.ru>
2022-11-25 10:41:10 +0300

    calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -8,19 +8,18 @@ int main()
 	std::cin >> num_2;
 	std::cout << "Type the third number: ";
 	std::cin >> num_3;
-	if (num_1 > num_2) {
+	if (num_1 > num_2 && num_2 > 0 && num_1 >= 0 && num_3 >= 0) {
 		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
 		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
 		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
 		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
-		std::cout << num_1 << ".0 / " << num_2 << ".0 = " << float (num_1) / float (num_2) << std::endl;
+		std::cout << num_1 << ".0 / " << num_2 << ".0 = " << (double)num_1 / num_2 << std::endl;
 		std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
 		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
-		std::cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;	
+		std::cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
 	}
 	else {
 		std::cout << "Invalid data" << std::endl;
 	}
-	system ("pause");
 	return 0;
 }
