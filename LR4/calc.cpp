babc324b4b07974d09f1fcb2f4735b7458eb934c
Misha Cheremykhin <mishamisha7266@gmail.com>
2022-11-25 08:23:58 +0300

    table_1_93.xlsx

---
+++ LR4/calc.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+ using namespace std;
+
+int main(){
+	setlocale(LC_ALL,"Rus");
+    int a ; 
+	int b ;
+	int c;
+	cout << "¬ведите первое число = ";
+	cin>>a;
+	cout << "¬ведите второе число = ";
+	cin>>b;
+	cout << "¬ведите третье число = ";
+	cin>>c;
+	cout<<endl;
+    cout << a << " + " << b << " = "<<a+b<<endl;
+    cout << a << " - " << b << " = "<<a-b<<endl;
+    cout << a << " * " << b << " = "<<a*b<<endl;
+	cout << a << " / " << b << " = "<< a / b <<endl;
+    cout << a <<".0"<< " / " << b <<".0"<< " = "<< double(a) / double(b) <<endl;
+	cout << a << " % " << b << " = "<< a % b<<endl;
+	cout << a << " + " << b << " * "<< c << " = "<< a + b * c<<endl;
+    cout <<"("<< a << " + " << b <<")"<< " * "<< c << " = "<<( a + b ) * c<<endl;
+    
+    return 0;
+}
