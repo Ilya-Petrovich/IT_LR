ad5ed77b38652a9a66cd3469e1bf67af59d83ffc
Liza Efarova <lizaefarova@mail.ru>
2022-11-26 15:47:19 +0300

    changed lr4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
-	float num_1, num_2, num_3;
+	int num_1, num_2, num_3;
 	cout << "¬ведите первое число: "; cin >> num_1;
 	cout << "¬ведите второе число: "; cin >> num_2;
 	cout << "¬ведите третье число: "; cin >> num_3;
+	float div = (float)num_1 / num_2;
 	if ((num_1 >= 0) && (num_2 >= 0) && (num_3 >= 0) && (num_2 != 0) && (num_1 > num_2))
 	{
 		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
 		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
 		cout << num_1 << " / " << num_2 << " = " << int(num_1 / num_2) << endl;
-		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-		cout << num_1 << " % " << num_2 << " = " << int(num_1) % int(num_2) << endl;
+		cout << setprecision(1) << fixed << (float)num_1 << " / " <<(float) num_2<<" = ";
+		cout << setprecision(5)<< fixed << div << endl;
+		cout << setprecision(0) << fixed;
+		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
 		cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + (num_2 * num_3) << endl;
 		cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
 	}
