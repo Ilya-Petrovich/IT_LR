bbd734f5e3a1c43ce31b410f7ad1df1fff90e66c
Guzel Khairullina <guzel1116@mail.ru>
2022-11-27 17:28:02 +0300

    Changed

---
+++ LR4/calc.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x, y, z;
+	cout << "Type the first number: ";
+	cin >> x;
+	cout << "Type the second number: ";
+	cin >> y;
+	cout << "Type the third number: ";
+	cin >> z;
+	if (x > y && x >= 0 && y > 0 && z >= 0) {
+		cout << x << " + " << y << " = " << x + y << endl;
+		cout << x << " - " << y << " = " << x - y << endl;
+		cout << x << " * " << y << " = " << x * y << endl;
+		cout << x << " / " << y << " = " << x / y << endl;
+		cout << double(x) << ".0" << " / " << double(y) << ".0" << " = " << double(x) / double(y) << endl;
+		cout << x << " % " << y << " = " << x % y << endl;
+		cout << x << " + " << y << " * " << z << " = " << x + y * z << endl;
+		cout <<"(" <<x << " + " << y <<")"<< " * " << z << " = " << (x + y) * z << endl;
+	}
+	else {
+		cout << "Invalid data" << endl;
+	}
+	return 0;
+}
+
