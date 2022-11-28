a4d31235e80b6a0d44d0b5b4587066ba9ca6502f
Maxim Dedyukhin <dedyukhin_m@mail.ru>
2022-11-28 13:48:15 +0300

    LR7

---
+++ LR7/reed.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void readText(char* buff, long fileSize) {
+	
+	FILE* file;
+	char filename[100] = "image.bmp";
+	file = fopen(filename, "rb");
+	
+	fread(buff, sizeof(char), fileSize, file);
+}
+
+int main()
+{
+	FILE* file;
+	char filename[100] = "image.bmp";
+	file = fopen(filename, "rb");
+	
+	long fileSize;
+	fseek(file, 0, SEEK_END);
+	fileSize = ftell(file);
+	rewind(file);
+	
+	char* buff = new char[fileSize]();
+
+	readText(buff, fileSize);
+
+	for (int i = 138; i < fileSize; i+=4)
+	{
+		char s = 0;
+		for (int j = 0; j < 4; j++) s = (s << 2) | (buff[i+j] & 3);
+		cout << s;
+	}
+}
