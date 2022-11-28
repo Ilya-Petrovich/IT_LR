13878de00c19f519e14c26813e31f1da881346d6
Anton Gusakov <anton.gusakov.02@mail.ru>
2022-11-28 13:50:16 +0300

    Add LR6

---
+++ LR6/animation.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+#include <string>
+#include "windows.h"
+
+using namespace std;
+
+void dancing(string* firstEl,  int t);
+
+int main() {
+	string pers[12] = {
+	"   ()()   ",
+	"   ()()   ",
+	"   ()()   ",
+	"   |..|   ",
+	"  \\|  |\/  ",
+	"  \/|UU|\\  ",
+	"  --\\\/--  ",
+	" /  ||  \\ ",
+	"/   ||   \\",
+	"    /\\    ",
+	"    ||    ",
+	"   _||_   ",
+	};
+
+	
+
+	string* pers0 = pers;
+	dancing(pers0, 500);
+
+	return 0;
+}
+
+void dancing(string* firstEl, int t) {
+	int col = 12;
+	string pers[12];
+	string* ptpers = pers;
+	for (int i = 0; i < col; i++, firstEl++) pers[i] = *firstEl;
+	for (int i = 0; i < col; i++) cout << pers[i] << endl;
+
+	for (int q = 0; q < 2; q++) {
+		pers[6] = " ---\\\/--- ";
+		pers[7] = "/   ||   \\";
+		pers[8] = "    ||    ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i]<< endl;
+		}
+		pers[6] = "  --\\\/----";
+		pers[7] = " /  ||    ";
+		pers[8] = "/   ||    ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout<< pers[i]<< endl;
+		}
+		pers[5] = "  \/|UU|\\ /";
+		pers[6] = " ---\\\/--- ";
+		pers[7] = "/   ||    ";
+		pers[8] = "    ||    ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout<< pers[i]<< endl;
+		}
+		pers[4] = "  \\|  |\/ /";
+		pers[5] = "  \/|UU|\\/";
+		pers[6] = "  --\\\/--  ";
+		pers[7] = " /  ||    ";
+		pers[8] = "/   ||    ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[4] = "  \\|  |\/ *";
+		pers[5] = "  \/|UU|\\/";
+		pers[6] = " ---\\\/--  ";
+		pers[7] = "/   ||    ";
+		pers[8] = "    ||    ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[4] = "  \\|  |\/ /";
+		pers[5] = "  \/|UU|\\/";
+		pers[6] = "----\\\/--  ";
+		pers[7] = "    ||    ";
+		pers[8] = "    ||    ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[4] = "  \\|  |\/ *";
+		pers[5] = "\\ \/|UU|\\/";
+		pers[6] = " ---\\\/--  ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[4] = "\\ \\|  |\/ /";
+		pers[5] = " \\\/|UU|\\/";
+		pers[6] = "  --\\\/--  ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[4] = "* \\|  |\/ *";
+		pers[5] = " \\\/|UU|\\/";
+		pers[6] = "  --\\\/--  ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[4] = "  \\|  |\/  ";
+		pers[5] = "* \/|UU|\\ *";
+		pers[6] = " ---\\\/--- ";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[5] = "  \/|UU|\\  ";
+		pers[6] = "*---||---*";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[6] = " ---||--- ";
+		pers[7] = "*   ||   *";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[6] = "  --||--  ";
+		pers[7] = " /  ||  \\ ";
+		pers[8] = "*   ||   *";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+		pers[8] = "/   ||   \\";
+		Sleep(t);
+		system("cls");
+
+		for (int i = 0; i < col; i++) {
+			cout << pers[i] << endl;
+		}
+	}
+	system("cls");
+	return;
+}
