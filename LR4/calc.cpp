9afcaf13cf067fd81f8a846268b7cf99a850d9a1
Nikiforovich Arina <a79656934465@yandex.ru>
2022-11-28 13:38:51 +0300

    LW4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -5,9 +5,9 @@ int main() {
 	std::cin >> num_1;
 	std::cout << "Type the second number: ";
 	std::cin >> num_2;
-	std::cout << "Type the third number: 5\n";
-	num_3 = 5;
-    if (num_1 > num_2) {
+	std::cout << "Type the third number: ";
+	std::cin >> num_2;
+    if ((num_1 > num_2) and (num_1 >= 0) and (num_3 >= 0) and (num_2 > 0)){
 	    std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
 	    std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
 	    std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
