bb8f7c66e506d75faeb1e5353fb971e0877a5860
Denis Khrunin <zak63507@gmail.com>
2022-11-29 19:02:01 +0300

    Changed

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
     int num_1, num_2, num_3;
 
-    cout << "Enter the first number: "; cin >> num_1;
-    cout << "Enter the second number: "; cin >> num_2;
-    cout << "Enter the third number: "; cin >> num_3;
+    cout << "Type the first number: "; cin >> num_1;
+    cout << "Type the second number: "; cin >> num_2;
+    cout << "Type the third number: "; cin >> num_3;
 
     if (num_1 >= 0 && num_2 > 0 && num_3 >= 0 && num_1 > num_2) {
         cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
