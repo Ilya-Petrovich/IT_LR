6c7cd31a9f9c0eb5fc72054fdaee230a3ae2a03d
ShakiroWladislaV <vlad22vlad22vlad@gmail.com>
2022-11-30 17:03:27 +0300

    win+R calc enter

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,11 +1,10 @@
 ﻿#include <iostream>
 using namespace std;
 int main() {
-	setlocale(0, "rus");
 	int a, b, c;
-	cout << "Введите первое число: "; cin >> a;
-	cout << "Введите второе число: "; cin >> b;
-	cout << "Введите третье число: "; cin >> c;
+	cout << "Type the first number: "; cin >> a;
+	cout << "Type the second number: "; cin >> b;
+	cout << "Type the third number: "; cin >> c;
 	if (a >= 0 && b > 0 && c >= 0 && a > b) {
 		cout << a << " + " << b << " = " << a + b << endl;
 		cout << a << " - " << b << " = " << a - b << endl;
