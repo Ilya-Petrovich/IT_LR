fe1eb78af5eabf62691dd5123480b5cebccbb7f2
Maxim Kochetkov <cdhv5755@gmail.com>
2022-12-01 18:42:58 +0300

    Changed LR4

---
+++ LR4/calc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int num, num_2, num_3;
+
+    cout << "Type the first number: ";
+    cin >> num;
+    cout << "Type the second number: ";
+    cin >> num_2;
+    cout << "Type the third number: ";
+    cin >> num_3;
+
+    float div = (float)num / num_2;
+
+    if (num > num_2 && num >= 0 && num_2 > 0 && num_3 >= 0)
+    {
+        cout << num << " + " << num_2 << " = " << num + num_2 << endl;
+        cout << num << " - " << num_2 << " = " << num - num_2 << endl;
+        cout << num << " * " << num_2 << " = " << num * num_2 << endl;
+        cout << num << " / " << num_2 << " = " << num / num_2 << endl;
+        cout << setprecision(1) << fixed << (float)num << " / " << (float)num_2 << " = ";
+        cout << setprecision(5) << fixed << div << endl;
+        cout << setprecision(0) << fixed;
+        cout << num << " % " << num_2 << " = " << num % num_2 << endl;
+        cout << num << " + " << num_2 << " * " << num_3 << " = " << num + num_2 * num_3 << endl;
+        cout << "(" << num << " + " << num_2 << ")" << " * " << num_3 << " = " << (num + num_2) * num_3 << endl;
+    }
+    else
+        cout << "Invalid data" << endl;
+    return 0;
+}
