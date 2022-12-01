3ef1e27fa0518eaa2ba272c6af38b0649077b8a1
Azat Gabidullin <alinagilmutdinova20@gmail.com>
2022-12-02 00:39:46 +0300

    calc 1

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
-int main()
-{
-	int a, b, c;
-	char opr;
-	cout << "Type the first number: ";
-	cin >> a;
-	cout << "Type the second number: ";
-	cin >> b;
-	cout << "Type the third number: ";
-	cin >> c;
-	if ( (a>0 && b>0) && a > b && b!=0) {
-	cout << a << '+' << b << "=" << a + b << endl;
-	cout << a << '-' << b << "=" << a - b << endl;
-	cout << a << '*' << b << "=" << a * b << endl;
-	cout << a << '/' << b << "=" << a / b << endl;
-	cout << a <<".0" << '/' << b <<".0" << "=" << float(a) / b * 1.0 << endl;
-	cout << a << '%' << b << "=" << a % b << endl;
-	cout << a << '+' << b << '*' << c << '=' << a + b * c << endl;
-	cout << "(" << a << '+' << b <<")" << '*' << c << '=' << (a + b) * c << endl;
-		}
-		else {
-		cout << "Invalid data";
-		}
+
+int main() {
+    int num, num_2, num_3;
+
+    cout << "Type the first number: ";
+    cin >> num;
+    cout << "Type the second number: ";
+    cin >> num_2;
+    cout << "Type the third number: ";
+    cin >> num_3;
+
+    float div = (float)num / num_2;
+
+    if (num > num_2 && num >= 0 && num_2 > 0 && num_3 >= 0)
+    {
+        cout << num << " + " << num_2 << " = " << num + num_2 << endl;
+        cout << num << " - " << num_2 << " = " << num - num_2 << endl;
+        cout << num << " * " << num_2 << " = " << num * num_2 << endl;
+        cout << num << " / " << num_2 << " = " << num / num_2 << endl;
+        cout << setprecision(1) << fixed << (float)num << " / " << (float)num_2 << " = ";
+        cout << setprecision(5) << fixed << div << endl;
+        cout << setprecision(0) << fixed;
+        cout << num << " % " << num_2 << " = " << num % num_2 << endl;
+        cout << num << " + " << num_2 << " * " << num_3 << " = " << num + num_2 * num_3 << endl;
+        cout << "(" << num << " + " << num_2 << ")" << " * " << num_3 << " = " << (num + num_2) * num_3 << endl;
+    }
+    else
+        cout << "Invalid data" << endl;
+    return 0;
 }
