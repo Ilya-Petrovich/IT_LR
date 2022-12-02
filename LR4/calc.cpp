abcf567c9ee8a8d66b704519778c2e9c6f28df82
Shamil Shaimardanov <mister.shaymardanov3@gmail.com>
2022-12-02 17:04:10 +0300

    calculator calc.cpp

---
+++ LR4/calc.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.3.32825.248
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "calc", "calc\calc.vcxproj", "{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Debug|x64.ActiveCfg = Debug|x64
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Debug|x64.Build.0 = Debug|x64
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Debug|x86.ActiveCfg = Debug|Win32
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Debug|x86.Build.0 = Debug|Win32
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Release|x64.ActiveCfg = Release|x64
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Release|x64.Build.0 = Release|x64
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Release|x86.ActiveCfg = Release|Win32
+		{69B55CE0-5569-4E11-A8C1-C8B8CD20DB7E}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {BEE61EB8-8201-4495-AE32-3686B1405EDB}
+	EndGlobalSection
+EndGlobal
