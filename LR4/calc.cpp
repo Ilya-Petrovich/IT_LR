2d8ca11a07191835b13a8df24b0b2ed472d60a5a
Liza Efarova <lizaefarova@mail.ru>
2022-12-02 14:58:43 +0300

    Lr4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 int main() {
 	int num_1, num_2, num_3;
-	std::cout << "Type the first number: "; std::cin >> num_1;
-	std::cout << "Type the second number: "; std::cin >> num_2;
-	std::cout << "Type the third number: "; std::cin >> num_3;
+	std::cout << "Type the first number: ";
+	std::cin >> num_1;
+	std::cout << "Type the second number: ";
+	std::cin >> num_2;
+	std::cout << "Type the third number: "; 
+	std::cin >> num_3;
 	float div = (float)num_1 / num_2;
 	if ((num_1 >= 0) && (num_2 >= 0) && (num_3 >= 0) && (num_2 != 0) && (num_1 > num_2))
 	{
