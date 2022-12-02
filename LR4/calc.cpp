016f5707f3f61db16e6e5c82c72ddce9aa5f8b27
Bina34 <gsabinarim2004@gmail.com>
2022-12-02 10:32:59 +0300

    LR4

---
+++ LR4/calc.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+
+	int num_1, num_2, num_3;
+
+	num_1 = 7;
+	num_2 = 3;
+	num_3 = 5;
+
+	std::cout << "Type the first number: ";
+	std::cin >> num_1;
+
+	std::cout << "Type the second number: ";
+	std::cin >> num_2;
+	std::cout << "Type the third number: ";
+	std::cin >> num_3;
+
+	if (num_1 > num_2) {
+		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
+		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
+		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
+		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
+		std::cout << num_1 << ".0 / " << num_2 << ".0" << " = " << float(num_1) / float(num_2) << std::endl;
+		std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
+		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
+		std::cout << "( " << num_1 << " + " << num_2 << " )" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
+	}
+	else {
+		std::cout << "Invalid data" << std::endl;
+	}
+	return 0;
+}
