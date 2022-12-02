6ffd63175de7a74a93a7b56e6dfbbdd9e8e4c77a
Karina Nurullina <karina_nurullina_7@inbox.ru>
2022-12-02 11:12:48 +0300

    Changed(LR4)

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
 
-int main{
-	int num_1, num_2, num_3;
-	num_1 = 7;
-	num_2 = 3;
+int main() 
+{
+	int n1, n2, n3;
 	
-	if (num_1 > num_2 && num_1 >= 0 && num_2 > 0 && num_3 >= 0) {
-		std::cout << "Type the first number: ";
+	std::cout << "¬ведите первое число:  ";
 
-		std::cin >> num_1;
+	std::cin >> n1;
 
-		std::cout << "Type the second number: ";
-		std::cin >> num_2;
+	std::cout << "¬ведите второе число: ";
+	std::cin >> n2;
 
-		std::cout << "Type the third number: ";
-		std::cin >> num_3;
+	std::cout << "¬ведите третье число: ";
+	std::cin >> n3;
 
-		std::cout << "7 + 3 = " << num_1 + num_2 << std::endl;
 
-		std::cout << "7 - 3 = " << num_1 - num_2 << std::endl;
+	if ((n1 >= 0) and (n2 > 0) and (n1 > n2) and (n3 >= 0)) {
+		
 
-		std::cout << "7 * 3 = " << num_1 * num_2 << std::endl;
-
-		std::cout << "7 / 3 = " << num_1 / num_2 << std::endl;
-
-		std::cout << "7.0 + 3.0 = " << (float)num_1 / num_2 << std::endl;
-
-		std::cout << "7 % 3 = " << num_1 % num_2 << std::endl;
-
-		std::cout << "7 + 3 * 5 = " << num_1 + num_2 * num_3 << std::endl;
-
-		std::cout << "(7 + 3) * 5 = " << (num_1 + num_2) * num_3 << std::endl;
+		cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+		cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
+		cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+		cout << n1 << " / " << n2 << " = " << n1 / n2 << endl;
+		cout << float(n1) << " / " << float(n2) << " = " << float(n1) / float(n2) << endl;
+		cout << n1 << " % " << n2 << " = " << n1 % n2 << endl;
+		cout << n1 << " + " << n2 << " * " << n3 << " = " << n1 + n2 * n3 << endl;
+		cout << "(" << n1 << " + " << n2 << ") * " << n3 << " = " << (n1 + n2) * n3 << endl;
 	}
 	else {
 		std::cout << "Invalid data" << std::endl;
