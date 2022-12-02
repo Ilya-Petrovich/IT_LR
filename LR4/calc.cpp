e54df5ad7e46e280d053d59f5008e37cb3ed3e5d
Liza Efarova <lizaefarova@mail.ru>
2022-12-02 18:09:14 +0300

    Change: LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -2,7 +2,6 @@
 
 int main() {
 	int num_1, num_2, num_3;
-
 	std::cout << "Type the first number: ";
 	std::cin >> num_1;
 
@@ -12,24 +11,21 @@ int main() {
 	std::cout << "Type the third number: ";
 	std::cin >> num_3;
 
-	float div = (float)num_1 / num_2;
-	if ((num_1 >= 0) && (num_2 > 0) && (num_3 >= 0) && (num_1 > num_2))
-	{
+	if (num_1 > num_2 && num_1 >= 0 && num_2 > 0 && num_3 >= 0) {
 		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
 		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
 		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
 		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
-		std::cout << num_1 << ".0" << " / " << num_2 << ".0 " << " = " << (float)num_1 / num_2 << std::endl;
+		std::cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << (float)num_1 / num_2 << std::endl;
 		std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
-		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + (num_2 * num_3) << std::endl;
+		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
 		std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
-	} else {
+	}
+	else
+	{
 		std::cout << "Invalid data" << std::endl;
 	}
 	return 0;
 }
-
-
-
 
 	
