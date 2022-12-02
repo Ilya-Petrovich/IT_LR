27587f287cf3de7a32cd8e04ec2d3086a46959b3
Afanasev Aleksandr <sanya100222@gmail.com>
2022-12-02 16:04:34 +0300

    1

---
+++ LR4/calc.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "Type the first number: ";
+	cin >> a;
+	cout << "Type the second number: "; 
+	cin >> b;
+	cout << "Typr the third number: ";
+	cin >> c;
+	
+	if ((a > b) & (a >= 0) & (b > 0) & (c >= 0)) {
+		cout << a << " + " << b << " = " << a + b << endl;
+		cout << a << " - " << b << " = " << a - b << endl;
+		cout << a << " * " << b << " = " << a * b << endl;
+		cout << a << " / " << b << " = " << a / b << endl;
+		cout << float(a) << " / " << float(b) << " = " << float(a) / float(b) << endl;
+		cout << a << " % " << b << " = " << a % b << endl;
+		cout << a << " + " << b << " = " << a + b << endl;
+		cout << a << " + " << b << " * " << c << " = " << a + b * c << endl;
+		cout << "(a + b) * c" << " = " << (a + b) * c << endl;
+	} else {
+		cout << "Invalid data" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
