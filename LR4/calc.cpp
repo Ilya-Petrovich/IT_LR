ce2bac2bab8b2d157587c8385142bc580de1d5b7
Egor Ershov <templ.ershov@mail.ru>
2022-12-02 16:16:26 +0300

    Changed LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -9,12 +9,18 @@ int main()
 	cout << "Type the second number: "; cin >> b;
 	cout << "Type the third numper "; cin >> c;
 
-	cout << a << " + " << b << " = " << a + b << endl;
-	cout << a << " - " << b << " = " << a - b << endl;
-	cout << a << " * " << b << " = " << a * b << endl;
-	cout << a << " / " << b << " = " << a / b << endl;
-	cout << a << ".0" << " / " << b << ".0" << " = " << (float(a) /float(b)) << endl;
-	cout << a << " % " << b << " = " << a % b << endl;
-	cout << a << " + " << b << " * " << c << " = " << a + b * c << endl;
-	cout << "(" << a << " + " << b << ")" << " * " << c << " = " << (a + b) * c << endl;
+	if(a > 0 and b > 0 and c > 0 and b != 0 and a > b){
+	    cout << a << " + " << b << " = " << a + b << endl;
+    	cout << a << " - " << b << " = " << a - b << endl;
+	    cout << a << " * " << b << " = " << a * b << endl;
+	    cout << a << " / " << b << " = " << a / b << endl;
+	    cout << a << ".0" << " / " << b << ".0" << " = " << (float(a) /float(b)) << endl;
+    	cout << a << " % " << b << " = " << a % b << endl;
+	    cout << a << " + " << b << " * " << c << " = " << a + b * c << endl;
+	    cout << "(" << a << " + " << b << ")" << " * " << c << " = " << (a + b) * c << endl;
+	}
+	
+	else{
+	    cout << "Invalid data" << endl;
+	}
 }
