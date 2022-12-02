183574ef1501277b3d87b021a42d1a41d3760982
Liza Efarova <lizaefarova@mail.ru>
2022-12-02 13:54:33 +0300

    LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
-using namespace std;
 int main() {
-	setlocale(LC_ALL, "Russian");
 	int num_1, num_2, num_3;
-	cout << "¬ведите первое число: "; cin >> num_1;
-	cout << "¬ведите второе число: "; cin >> num_2;
-	cout << "¬ведите третье число: "; cin >> num_3;
+	std::cout << "Type the first number:"; std::cin >> num_1;
+	std::cout << "Type the second number: "; std::cin >> num_2;
+	std::cout << "Type the tnird number: "; std::cin >> num_3;
 	float div = (float)num_1 / num_2;
 	if ((num_1 >= 0) && (num_2 >= 0) && (num_3 >= 0) && (num_2 != 0) && (num_1 > num_2))
 	{
-		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
-		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
-		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
-		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-		cout << num_1 << ".0" << " / " << num_2 << ".0 " << " = " << (float)num_1 / num_2 << endl;
-		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
-		cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + (num_2 * num_3) << endl;
-		cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
+		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << '\n';
+		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << '\n';
+		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << '\n';
+		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << '\n';
+		std::cout << num_1 << ".0" << " / " << num_2 << ".0 " << " = " << (float)num_1 / num_2 << '\n';
+		std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << '\n';
+		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + (num_2 * num_3) << '\n';
+		std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << '\n';
 	}
 	else {
-		cout << "Invalid data";
+		std::cout << "Invalid data";
 	}
 	return 0;
 }
-
 
 
 
