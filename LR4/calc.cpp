59eb88cb0eb6a4404510576626a596244bbad692
Nikita Nadezhdin <hitpro@bk.ru>
2022-12-02 13:14:24 +0300

    update 2. LR4/

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,29 +1,27 @@
 ﻿#include <iostream>
-using namespace std;
 
 int main()
 {
 	int num_1, num_2, num_3;
-	cout << "Type the first number: ";
-	cin >> num_1;
-	cout << "Type the second number: ";
-	cin >> num_2;
-	cout << "Type the third number: ";
-	cin >> num_3;
+	std::cout << "Type the first number: ";
+	std::cin >> num_1;
+	std::cout << "Type the second number: ";
+	std::cin >> num_2;
+	std::cout << "Type the third number: ";
+	std::cin >> num_3;
 	if (num_1 < num_2 || num_2 <= 0 || num_1 < 0 || num_3 < 0) {
-		cout << "Invalid data";
+		std::cout << "Invalid data";
 		return 1;
 	} else {
-		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
-		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
-		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
-		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-		cout << num_1 << ".0 / " << num_2 << ".0 = " << (double)num_1 / num_2 << endl;
-		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
-		cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
-		cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
+		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
+		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
+		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
+		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
+		std::cout << num_1 << ".0 / " << num_2 << ".0 = " << (double)num_1 / num_2 << std::endl;
+		std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
+		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
+		std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
 	}
-	system("pause");
 	return 0;
 }
 
