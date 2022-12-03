9ae88fd39d1a96a3dca28a7b18dc0839886d8e83
Ksenya Senyashina <Ksenyashina@mail.ru>
2022-12-03 11:36:14 +0300

    Changed LR4

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -7,37 +7,38 @@ int main()
 	std::cin>>t;
 	if( t>=0 && t<=50)
 	{
-		std::cout<<"Car stats"<<std::endl;
+		std::cout<<"\nCar stats"<<std::endl;
 		std::cout<<"Fuel: "<<t<<std::endl;
 		std::cout<<"Fuel capacity: "<<mt<<std::endl;
 		std::cout<<"Fuel consumption: 0"<<std::endl;
 		std::cout<<"Speed: 0"<<std::endl;
-		std::cout<<"Maximum speed: "<<ms<<std::endl;
+		std::cout<<"Maximum speed: "<<ms<<std::endl<<std::endl;
 		std::cout<<"Route info"<<std::endl;
-		std::cout<<"Distance: 360 km"<<std::endl;
+		std::cout<<"Distance: 360 km"<<std::endl<<std::endl;
 		std::cout<<"You have to drive a route 360 km long."<<std::endl;
-		std::cout<<"But first you need to check if you have enough fuel."<<std::endl;
-		std::cout<<"What speed will you choose? ";
-		std::cin>>speed;
+		std::cout<<"But first you need to check if you have enough fuel."<<std::endl<<std::endl;
 		if(t<mt/2)
 		{
 			t=t+mt/2;
-			std::cout<<"Your fuel before start: "<<t<<std::endl;
+			std::cout<<"Your fuel before start: "<<t<<std::endl<<std::endl;
 		}
+		std::cout<<"What speed will you choose? ";
+		std::cin>>speed;
+		
 		if(speed<0)
 		{
-			std::cout<<"Invalid data"<<std::endl;
+			std::cout<<"\nInvalid data"<<std::endl;
 			return 0;
 		}
 		if(speed>110)
 		{
 			speed=110;
 		}
-		std::cout<<"Your speed before start: "<<speed<<" km/h"<<std::endl;
+		std::cout<<"Your speed before start: "<<speed<<" km/h"<<std::endl<<std::endl;
 		if(speed==0)
 		{
 			fuelConsumption=0;
-			std::cout<<"Fuel consumption with chosen speed: "<<fuelConsumption<<" litres per 100 km"<<std::endl;
+			std::cout<<"Fuel consumption with chosen speed: "<<fuelConsumption<<" litres per 100 km"<<std::endl<<std::endl;
 			std::cout<<"Destination result"<<std::endl;
 			d=0;
 			std::cout<<"Distance traveled: "<<d<<" km"<<std::endl;
@@ -48,7 +49,7 @@ int main()
 		{
 			s=(double)speed/10;
 			fuelConsumption = s * s / 4 -4.5 * s + 30;
-			std::cout<<"Fuel consumption with chosen speed: "<<fuelConsumption<<" litres per 100 km"<<std::endl;
+			std::cout<<"Fuel consumption with chosen speed: "<<fuelConsumption<<" litres per 100 km"<<std::endl<<std::endl;
 			dm=t/fuelConsumption*100;
 			if(dm<d)
 			{
@@ -58,7 +59,7 @@ int main()
 		
 			std::cout<<"Destination result"<<std::endl;
 			std::cout<<"Distance traveled: "<<d<<" km"<<std::endl;
-			std::cout<<"Time spend: "<<time<<" sec "<<std::endl;
+			std::cout<<"Time spent: "<<time<<" sec "<<std::endl;
 		}
 		rt=fuelConsumption/100*d;
 		ot=t-rt;
