baec87bc02ad8c0de5ba2688e896d2c847a4d8ea
Salavat Asadullin <SalavatAsadullin2114@gmail.com>
2022-12-03 08:54:59 +0300

    lr4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int num_1, num_2;
-	cout << "Type the first number: ";
-	cin >> num_1;
-	cout << "Type the second number: ";
-	cin >> num_2;
-	cout << "Type the third number: 5" << endl;
-
-	if (num_1 > num_2) {
+	int num_1, num_2, num_3;
+	cout << "Type the first number: "; cin >> num_1;
+	cout << "Type the second nubmer: "; cin >> num_2;
+	cout << "Type the third number: "; cin >> num_3;
+	if (num_1 > num_2 && num_2 != 0) {
 		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
 		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
 		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-		cout << num_1 << ".0 / " << num_2 << ".0 = " << (float)num_1 / num_2 << endl;
+		cout << num_1 << ".0 / " << num_2 << ".0  = " << (float)num_1 / num_2 << endl;
 		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
-		cout << num_1 << " + " << num_2 << " * 5 = " << num_1 + num_2 * 5 << endl;
-		cout << "(" << num_1 << " + " << num_2 << ") * 5 = " << (num_1 + num_2) * 5 << endl;
+		cout << num_1 << " + " << num_2 << " * " << num_3 << num_1 + num_2 * num_3 << endl;
+		cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
 	}
 	else {
 		cout << "Invalid data" << endl;
