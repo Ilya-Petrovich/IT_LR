254807f6311e55d2793b4b84607ce6f777ba6512
Aliya Khakimova <Khakimova.alia@mail.ru>
2022-12-04 01:01:03 +0300

    LR6

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <windows.h>
+﻿//#include <iostream>
+//#include <string>
+//#include <windows.h>
+#include <bits/stdc++.h>
+#include <unistd.h>
+
 using namespace std;
-
-void print(vector < string > a) {
-
-	for (auto c : a)
-		cout << c << endl;
-}
+const int w = 10, h = 12;
 int main() {
-	vector<string> a = {"   ()()   ",  //0
+	
+
+	string a[h]{
+						"   ()()   ",  //0
 						"   ()()   ",  //1
 						"   ()()   ",  //2
 						"   |..|   ",  //3
@@ -23,266 +23,121 @@ int main() {
 						"    ||    ",  //10
 						"   _||_   "   //11
 	};
-	print(a);
-	Sleep(5000);
 
-	int i = 0;
-	while (i < 2)
-	{
-		system("cls");
+	for (int i = 0; i < 2; i++) {
+
+		for (int i = 0; i < h; i++) cout << a[i] << "\n"; //1
+		//Sleep(1000);
+		//system("cls");
 		a[6] = " ---\\/---  ";
 		a[7] = "/   ||   \\";
 		a[8] = "    ||     ";
-		print(a);
-		Sleep(2000);
 
 
-		system("cls");
+		for (int i = 0; i < h; i++) cout << a[i] << "\n"; //2
+		//Sleep(1000);
+		//system("cls");
 		a[6] = "  --\\/----  ";
 		a[7] = " /  ||       ";
 		a[8] = "/   ||       ";
-		print(a);
-		Sleep(2000);
 
 
-		system("cls");
+		for (int i = 0; i < h; i++) cout << a[i] << "\n"; //3
+		//Sleep(1000);
+		//system("cls");
 		a[5] = "  /|UU|\\ /";
 		a[6] = " ---\\/---  ";
 		a[7] = "/   ||       ";
 		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
 
+		for (int i = 0; i < h; i++) cout << a[i] << "\n"; //4
+		//Sleep(1000);
+		//system("cls");
 
-		system("cls");
 		a[4] = "  \\|  |/ /";
 		a[5] = "  /|UU|\\/";
 		a[6] = "  --\\/--  ";
 		a[7] = " /  ||       ";
 		a[8] = "/   ||       ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
+		for (int i = 0; i < h; i++) cout << a[i] << "\n"; //5
+		//Sleep(1000);
+		//system("cls");
 		a[4] = "  \\|  |/ *";
 		a[5] = "  /|UU|\\/";
 		a[6] = " ---\\/--  ";
 		a[7] = "/   ||       ";
 		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
+		for (int i = 0; i < h; i++) cout << a[i] << "\n"; //6
+		//Sleep(1000);
+		//system("cls");
 		a[4] = "  \\|  |/ /";
 		a[5] = "  /|UU|\\/";
 		a[6] = "----\\/--  ";
 		a[7] = "    ||       ";
-		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//7
+		//Sleep(1000);
+		//system("cls");
 		a[4] = "  \\|  |/ *";
 		a[5] = "\\ /|UU|\\/";
 		a[6] = " ---\\/--  ";
 		a[7] = "    ||       ";
-		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//8
+		//Sleep(1000);
+		//system("cls");
 		a[4] = "\\ \\|  |/ /";
 		a[5] = " \\/|UU|\\/";
 		a[6] = "  --\\/--  ";
 		a[7] = "    ||       ";
-		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
-		a[4] = "* \\|  |/ * ";
-		a[5] = " \\/|UU|\\/ ";
-		a[6] = "  --\\/--   ";
-		a[7] = "    ||      ";
-		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//9
+		//Sleep(1000);
+		//system("cls");
+		a[4] = "* \\|  |/ *";
 
-		system("cls");
+
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//10
+		//Sleep(1000);
+		//system("cls");
 		a[4] = "  \\|  |/   ";
 		a[5] = "* /|UU|\\ *";
 		a[6] = " ---\\/---  ";
-		a[7] = "    ||      ";
-		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
-		a[4] = "  \\|  |/  ";
+
+
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//11
+		//Sleep(1000);
+		//system("cls");
 		a[5] = "  /|UU|\\  ";
 		a[6] = "*---\\/---* ";
-		a[7] = "    ||     ";
-		a[8] = "    ||     ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//12
+		//Sleep(1000);
+		//system("cls");
 		a[6] = " ---\\/--- ";
 		a[7] = "*   ||   * ";
 		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//13
+		//Sleep(1000);
+		//system("cls");
 		a[6] = "  --\\/-- ";
 		a[7] = " /  ||  \\ ";
 		a[8] = "*   ||   *";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
-		a[6] = "  --\\/-- ";
-		a[7] = " /  ||  \\ ";
+
+
+		for (int i = 0; i < h; i++) cout << a[i] << "\n";//14
+		//Sleep(1000);
+		//system("cls");
 		a[8] = "/   ||   \\";
-		print(a);
-		Sleep(2000);
 
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
-		a[6] = " ---\\/---  ";
-		a[7] = "/   ||   \\ ";
-		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
-		a[6] = "  --\\/---- ";
-		a[7] = " /  ||      ";
-		a[8] = "/   ||      ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\ /  ";
-		a[6] = " ---\\/--- ";
-		a[7] = "/   ||      ";
-		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/ / ";
-		a[5] = "  /|UU|\\/  ";
-		a[6] = "  --\\/--    ";
-		a[7] = " /  ||       ";
-		a[8] = "/   ||       ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/ * ";
-		a[5] = "  /|UU|\\/  ";
-		a[6] = " ---\\/--    ";
-		a[7] = "/   ||       ";
-		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/ / ";
-		a[5] = "  /|UU|\\/  ";
-		a[6] = "----\\/--    ";
-		a[7] = "    ||       ";
-		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/ * ";
-		a[5] = "\\ /|UU|\\/ ";
-		a[6] = " ---\\/--   ";
-		a[7] = "    ||       ";
-		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "\\ \\|  |/ /";
-		a[5] = " \\/|UU|\\/";
-		a[6] = "  --\\/--  ";
-		a[7] = "    ||       ";
-		a[8] = "    ||       ";
-		print(a);
-		Sleep(2000); 
-
-
-		system("cls");
-		a[4] = "* \\|  |/ * ";
-		a[5] = " \\/|UU|\\/ ";
-		a[6] = "  --\\/--   ";
-		a[7] = "    ||      ";
-		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
-		
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "* /|UU|\\ *";
-		a[6] = " ---\\/---  ";
-		a[7] = "    ||      ";
-		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/  ";
-		a[5] = "  /|UU|\\  ";
-		a[6] = "*---\\/---* ";
-		a[7] = "    ||     ";
-		a[8] = "    ||     ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
-		a[6] = " ---\\/--- ";
-		a[7] = "*   ||   * ";
-		a[8] = "    ||      ";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
-		a[6] = "  --\\/-- ";
-		a[7] = " /  ||  \\ ";
-		a[8] = "*   ||   *";
-		print(a);
-		Sleep(2000);
-
-		system("cls");
-		a[4] = "  \\|  |/   ";
-		a[5] = "  /|UU|\\   ";
-		a[6] = "  --\\/-- ";
-		a[7] = " /  ||  \\ ";
-		a[8] = "/   ||   \\";
-		print(a);
-		Sleep(2000);
-
-		i = i++;
-		system("pause");
-		return 0;
 	}
-	
+	for (int i = 0; i < h; i++) cout << a[i] << "\n";
+
+	return 0;
 }
+
