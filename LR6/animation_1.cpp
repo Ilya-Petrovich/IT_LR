3885b5d0c6fa2630137826392e16bc653ff3cd3d
Anton Kalosha <antonkm221@gmail.com>
2022-12-03 08:48:34 +0300

    LR6

---
+++ LR6/animation_1.cpp
@@ -0,0 +1,346 @@
+#include <iostream>//N1.1
+#include <Windows.h>
+using namespace std;
+void dance()
+{
+	cout << "****************************************" << endl;//1
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\                              *" << endl;
+	cout << "*  --\\/--                              *" << endl;
+	cout << "* /  ||  \\                             *" << endl;
+	cout << "*/   ||   \\                            *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//2
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\                              *" << endl;
+	cout << "* ---\\/---                             *" << endl;
+	cout << "*/   ||   \\                            *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//3
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\                              *" << endl;
+	cout << "*  --\\/----                            *" << endl;
+	cout << "* /  ||                                *" << endl;
+	cout << "*/   ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//4
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\ /                            *" << endl;
+	cout << "* ---\\/---                             *" << endl;
+	cout << "*/   ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//5
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/ /                            *" << endl;
+	cout << "*  /|UU|\\/                             *" << endl;
+	cout << "*  --\\/--                              *" << endl;
+	cout << "* /  ||                                *" << endl;
+	cout << "*/   ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//6
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/ *                            *" << endl;
+	cout << "*  /|UU|\\/                             *" << endl;
+	cout << "* ---\\/--                              *" << endl;
+	cout << "*/   ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//7
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/ /                            *" << endl;
+	cout << "*  /|UU|\\/                             *" << endl;
+	cout << "*----\\/--                              *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//8
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/ *                            *" << endl;
+	cout << "*\\ /|UU|\\/                             *" << endl;
+	cout << "* ---\\/--                              *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//9
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*\\ \\|  |/ /                            *" << endl;
+	cout << "* \\/|UU|\\/                             *" << endl;
+	cout << "*  --\\/--                              *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//9
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "** \\|  |/ *                            *" << endl;
+	cout << "* \\/|UU|\\/                             *" << endl;
+	cout << "*  --\\/--                              *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//9
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "** /|UU|\\ *                            *" << endl;
+	cout << "* ---\\/---                             *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//9
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\                              *" << endl;
+	cout << "**---\\/---*                            *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//9
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\                              *" << endl;
+	cout << "* ---\\/---                             *" << endl;
+	cout << "**   ||    *                           *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//14
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\                              *" << endl;
+	cout << "*  --\\/--                              *" << endl;
+	cout << "* /  ||  \\                             *" << endl;
+	cout << "**   ||   *                            *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+	cout << "****************************************" << endl;//15
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*                                      *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   ()()                               *" << endl;
+	cout << "*   |..|                               *" << endl;
+	cout << "*  \\|  |/                              *" << endl;
+	cout << "*  /|UU|\\                              *" << endl;
+	cout << "*  --\\/--                              *" << endl;
+	cout << "* /  ||  \\                             *" << endl;
+	cout << "*/   ||   \\                            *" << endl;
+	cout << "*    /\\                                *" << endl;
+	cout << "*    ||                                *" << endl;
+	cout << "*   _||_                               *" << endl;
+	cout << "****************************************" << endl;
+	Sleep(500);
+}
+
+void run()
+{
+	cout << 34 % 3 << endl;
+}
+
+
+int main() 
+{
+	int z = 1;
+	switch (z)
+	{
+	case 1:
+	{
+		dance();
+		dance();
+	}
+	case 2:
+		run();
+	default:
+	{
+		return 0;
+	}
+	}
+}
