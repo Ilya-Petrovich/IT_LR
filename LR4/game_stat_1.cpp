d66b061bcd562a7cb00a73e65dfa870fabbe12a4
Fedor Tabakov <fedortab7@gmail.com>
2022-12-03 12:57:43 +0300

    LR4 1

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -5,12 +5,13 @@ int main()
 {
 	int speed = 0, time = 0, maxSpeed = 110;
 	bool pointReached = false;
-	double speedMS, fullDistance, fuel, fuelCapacity = 50.0, s = 0, distance = 360, distanceTraveled = 0;
-	double fuelConsumption = 0, fuelConsumed = 0;
+	float speedMS, fullDistance, fuel, fuelCapacity = 50.0, s = 0, distance = 360, distanceTraveled = 0;
+	float fuelConsumption = 0, fuelConsumed = 0;
 	cout << "How much fuel do you have at start? "; cin >> fuel;
+	cout << endl;
 	if (fuel >= 0 && fuel <= fuelCapacity)
 	{
-		cout << "\n\nCar stats" << endl;
+		cout << "Car stats" << endl;
 		cout << "Fuel: " << fuel << endl;
 		cout << "Fuel capacity: " << fuelCapacity << endl;
 		cout << "Fuel consumption: " << fuelConsumption << endl;
@@ -19,7 +20,7 @@ int main()
 
 		cout << "\nRoute info" << endl;
 		cout << "Distance: 360 km" << endl;
-		cout << "----------------------------------------------" << endl;
+		cout << "----------------------------------------" << endl;
 		cout << "You have to drive a route 360 km long." << endl;
 		cout << "But first you need to check if you have enough fuel." << endl;
 
@@ -33,7 +34,7 @@ int main()
 
 		if (speed < 0)
 		{
-			cout << "Invalid data" << endl;
+			cout << "Invalid data." << endl;
 			return 0;
 		}
 		else if (speed > maxSpeed)
@@ -42,8 +43,9 @@ int main()
 		}
 
 		cout << "Your speed before start: " << speed << " km/h" << endl;
-		s = (double)speed / 10;
+		s = (float)speed / 10;
 		speed == 0 ? fuelConsumption = 0 : fuelConsumption = s * s / 4 - 4.5 * s + 30;
+		cout << endl;
 		cout << "Fuel consumption with chosen speed: " << fuelConsumption << " litres per 100 km" << endl;
 
 		fullDistance = fuel / fuelConsumption * 100;
@@ -60,8 +62,8 @@ int main()
 			pointReached = true;
 		}
 
-		speedMS = (double)speed / 3.6;
-                speed == 0 ? time = 0 : time = (double)distanceTraveled * 1000 / speedMS; 
+		speedMS = (float)speed / 3.6;
+                speed == 0 ? time = 0 : time = (float)distanceTraveled * 1000 / speedMS; 
 				
 		speed == 0 ? distanceTraveled = 0: distanceTraveled;
 		cout << "\nDestination result" << endl;
@@ -69,7 +71,7 @@ int main()
 		cout << "Time spent: " << time << " sec" << endl;
 		cout << "Fuel consumed: " << fuelConsumed << " litres" << endl;
 		cout << "Fuel remained: " << (int)fuel - fuelConsumed << " litres" << endl;
-		cout << "----------------------------------------------" << endl;
+		cout << "----------------------------------------" << endl;
 		if (pointReached)
 			cout << "Congratulations! You have reached destination point!" << endl;
 		else
@@ -77,7 +79,7 @@ int main()
 	}
 	else
 	{
-		cout << "Invalid data" << endl;
+		cout << "Invalid data." << endl;
 		return 0;
 	}
 }
