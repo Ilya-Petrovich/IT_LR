021e07373323dc593cb60374abeeb1714911252f
Dima Bekker <dimabekker23062004@gmail.com>
2022-12-03 13:37:32 +0300

    Calculator

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -17,7 +17,7 @@ int main() {
         std::cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << float(num_1) / float(num_2) << std::endl;
         std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
         std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
-        std::cout << "( " << num_1 << " + " << num_2 << " ) " << "* " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
+        std::cout << "(" << num_1 << " + " << num_2 << ") " << "* " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
     }else {
         std::cout << "Invalid data" << std::endl;
     }
