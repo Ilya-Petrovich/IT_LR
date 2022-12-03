c3b83dc265fff83ad822b6af264df19a6a1a29a4
Babkin Nikita <winnikus@gmail.com>
2022-12-03 23:08:21 +0300

    Delete spaces

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
 	int num_1,num_2,num_3;
 
-	cout<<"Type the first number: ";cin>>num_1;
-	cout<<"Type the second number: ";cin>>num_2;
-	cout<<"Type the third number: ";cin>>num_3;
+	cout<<"Type the first number:";cin>>num_1;
+	cout<<"Type the second number:";cin>>num_2;
+	cout<<"Type the third number:";cin>>num_3;
 
 	if (num_1 < num_2 || num_2 <= 0 || num_3 < 0){
 		cout<<"Invalid data\n";
