9c245735e5fb3a1ea402616030e0a0747c869cd7
berk gurbuz <berkgurbuz978@gmail.com>
2022-12-04 05:10:48 +0300

    r u read these

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
 	double f, sp, s, d = 360, dt, fuelRemained, fuelConsumed, fuelConsumption;
 	int t;
-	cout << "How much fuel do you have at start? "; cin >> f;
+	std::cout << "How much fuel do you have at start? "; std::cin >> f;
 	if (f < 0 or f>50) {
 		cout << "\nInvalid data."; return 0;
 	}
-	cout << "\nCar stats\nFuel: "<<f<<"\nFuel capacity: 50\nFuel consumption: 0\nSpeed: 0\nMaximum speed: 110\n";
-	cout << "\nRoute info\nDistance: 360 km\n";
-	cout << "----------------------------------------\nYou have to drive a route 360 km long.\nBut first you need to chek if you have enough fuel.\n";
-	if (f < 26) { f += 25; cout << "\nYour fuel before start: " << f << endl;
+	std::cout << "\nCar stats\nFuel: "<<f<<"\nFuel capacity: 50\nFuel consumption: 0\nSpeed: 0\nMaximum speed: 110\n";
+	std::cout << "\nRoute info\nDistance: 360 km\n";
+	std::cout << "----------------------------------------\nYou have to drive a route 360 km long.\nBut first you need to chek if you have enough fuel.\n";
+	if (f < 26) { f += 25; std::cout << "\nYour fuel before start: " << f << std::endl;
 	}
-	cout << "\nWhat speed will you choose? "; cin >> sp;
+	std::cout << "\nWhat speed will you choose? "; std::cin >> sp;
 	if (sp > 110) sp = 110;
 	if (sp < 0) {
-	    cout << "Invalid data."; return 0;
+	    std::cout << "Invalid data."; return 0;
 	}
-	cout << "Your speed before start: " << sp << " km/h\n";
+	std::cout << "Your speed before start: " << sp << " km/h\n";
 	s = sp / 10;
 	fuelConsumption = s * s / 4 - 4.5 * s + 30;
 	if (sp == 0) fuelConsumption = 0;
-	cout << "\nFuel consumption with chosen speed: " << fuelConsumption << " litres per 100 km\n";
+	std::cout << "\nFuel consumption with chosen speed: " << fuelConsumption << " litres per 100 km\n";
 	dt = f / fuelConsumption * 100;
 	if (dt > 360) dt = 360;
 	t = dt / sp * 3600;
@@ -32,7 +31,7 @@ int main()
 	if (sp == 0) {
 	    t = 0; dt = 0;
 	}
-	cout << "\nDestination result\nDistance traveled: " << dt << " km\nTime spent: " << t << " sec\nFuel consumed: " << fuelConsumed << " litres\nFuel remained " << fuelRemained << " litres\n----------------------------------------\n";
+	std::cout << "\nDestination result\nDistance traveled: " << dt << " km\nTime spent: " << t << " sec\nFuel consumed: " << fuelConsumed << " litres\nFuel remained " << fuelRemained << " litres\n----------------------------------------\n";
 	if (dt == 360) cout << "Congratulations! You have reached destination point!";
 	else cout << "You haven't reached destination point. " << 360 - dt << " km left.";
 	system ("pause");
