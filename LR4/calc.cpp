5247e06adbb470a781c1d248a8ac1ceab6ad30b5
Valeriya Zanina <valeriyazanina2004@mail.ru>
2022-12-05 09:58:38 +0300

    fix: LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -8,7 +8,7 @@ int main()
     cout << "Type the first number: "; cin >> num_1;
     cout << "Type the second number: "; cin >> num_2;
     cout << "Type the third number: "; cin >> num_3;
-	if (num_1 > num_2 && num_2 > 0) {
+	if (num_1 > num_2 && num_2 > 0 && num_3 >= 0 && num_1 >= 0) {
 		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
 		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
