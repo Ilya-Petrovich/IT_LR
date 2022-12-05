2d9c79f329f509ae7f2fc18f64b1b01c79ef0462
Ksenya Senyashina <Ksenyashina@mail.ru>
2022-12-05 20:27:29 +0300

    Changed LR7

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,32 +1,33 @@
 #include <stdio.h>
 #include <iostream>
-using namespace std;
 void ReadText(char* buff, long fSize);
-int main()
-{
-	FILE* file;
-	file = fopen("image.bmp", "rb");
-	long fSize;
-	fseek(file, 0, SEEK_END);
-	fSize = ftell(file);
-	rewind(file);
-	char* buff = new char[fSize]();
-	fread(buff, 1, fSize, file);
-	ReadText(buff, fSize);
-	fclose(file);
-	free(buff);
-}
-void ReadText(char* buff, long fSize) 
-{
-	int start = fSize - 1200;
-	while (start< (fSize)) 
-	{
-		int firstByte = buff[start] & 0x03;	
-		int secondByte = buff[start+1] & 0x03;
-		int thirdByte = buff[start+2] & 0x03;	
-		int fourthByte = buff[start + 3] & 0x03;
-		cout <<(char) ((firstByte << 6) | (secondByte << 4) | (thirdByte << 2) | fourthByte);
-		start += 4;
-	}
+
+int main() {
+    FILE* file;
+    char filename[100];
+    file = fopen("image.bmp", "rb");
+
+    long fSize;
+    fseek(file, 0, SEEK_END);
+    fSize = ftell(file);
+    rewind(file);
+
+    char* buff = new char[fSize]();
+    fread(buff, 1, fSize, file);
+    ReadText(buff, fSize);
+
+    fclose(file);
+    free(buff);
 }
 
+void ReadText(char* buff, long fSize) {
+    int start_of_byte = fSize - 1200;
+    while (start_of_byte < fSize) {
+    int first_byte = buff[start_of_byte] & 3;	
+    int second_byte = buff[start_of_byte + 1] & 3;
+    int third_byte = buff[start_of_byte + 2] & 3;	
+    int fourth_byte = buff[start_of_byte + 3] & 3;
+    std::cout <<(char) ((first_byte << 6) | (second_byte << 4) | (third_byte << 2) | fourth_byte);
+    start_of_byte += 4;
+    }
+}
