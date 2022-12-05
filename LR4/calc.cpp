e8c5376b93ec061ab1adcff4854de70c01e14468
Sabina Nizamova <sabina.nzzz@yandex.ru>
2022-12-05 23:23:57 +0300

    Changed LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-	setlocale(LC_ALL, "Russian");
+int main() {
 	int num_1, num_2, num_3;
-	cout << "¬ведите первое число: "; cin >> num_1;
-	cout << "¬ведите второе число: "; cin >> num_2;
-	cout << "¬ведите третье число: "; cin >> num_3;
+	cout << "Type the first number: "; cin >> num_1;
+	cout << "Type the second number: "; cin >> num_2;
+	cout << "Type the third number: "; cin >> num_3;
+
 	if (num_1 > 0 && num_2 > 0 && num_3 > 0 && num_1 > num_2 && num_2 != 0) {
 		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
 		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
 		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-		cout << "7.0 / 3.0 = " << static_cast<float>(num_1) / num_2 << endl;
+		cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << ((float)num_1) / num_2 << endl;
 		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
 		cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
 		cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
