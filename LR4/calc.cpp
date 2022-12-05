a78486047cd37c9663872e179e6966aa45feee6e
Yaroslav Vokhmyanin <yarik123lol@gmail.com>
2022-12-05 10:00:18 +0300

    calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 int main()
 {
-	int num_1, num_2, num_3;
-    cout << "Type the first number: "; cin >> num_1;
-    cout << "Type the second number: "; cin >> num_2;
-    cout << "Type the third number: "; cin >> num_3;
+    int num_1, num_2, num_3;
+    cout << "Type the first number: ";
+    cin >> num_1;
+    cout << "Type the second number: ";
+    cin >> num_2;
+    cout << "Type the third number: ";
+    cin >> num_3;
     if(num_1 > num_2 and num_1 >=0 and num_2 > 0 and num_3 >= 0)
     {
-        cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 <<' ' << endl;
-        cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 <<' ' << endl;
-        cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 <<' ' << endl;
-        cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 <<' ' << endl;
-        cout << double(num_1) << ".0" << " / " << double(num_2) << ".0" << " = " << double(num_1) / double(num_2) <<' ' << endl;
-        cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 <<' ' << endl;
-        cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 <<' ' << endl;
-        cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 <<' ' << endl;
+        cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
+        cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+        cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
+        cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
+        cout << double(num_1) << ".0" << " / " << double(num_2) << ".0" << " = " << double(num_1) / double(num_2) << endl;
+        cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
+        cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3<< endl;
+        cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
     }else{
         cout << "Invalid data" <<' ' << endl;
     }
