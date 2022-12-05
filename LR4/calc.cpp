57994c3df1faa36b748382a2fccb0dd3ce70bfb3
Babkin Nikita <winnikus@gmail.com>
2022-12-05 15:17:08 +0300

    Lets try it

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main(){
 	int num_1,num_2,num_3;
 
-	cout << "Type the first number: "; cin >> num_1;
-	cout << "Type the second number: "; cin >> num_2;
-	cout << "Type the third number: "; cin >> num_3;
+	cout << "Type the first number: "; cin >> num_1; 
+	cout << "\nType the second number: "; cin >> num_2;
+	cout << "\nType the third number: "; cin >> num_3;
 
 	if (num_1 < num_2 || num_2 <= 0 || num_3 < 0){
-		cout << "Invalid data";
+		cout << "\nInvalid data";
 	}
 
 	else{
-		printf("%d + %d = %d\n", num_1, num_2, num_1+num_2);
+		printf("\n%d + %d = %d\n", num_1, num_2, num_1+num_2);
 		printf("%d - %d = %d\n", num_1, num_2, num_1-num_2);
 		printf("%d * %d = %d\n", num_1, num_2, num_1*num_2);
 		printf("%d / %d = %d\n", num_1, num_2, num_1/num_2);
