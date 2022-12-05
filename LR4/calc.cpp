8ced0205a4d3b6af5635f89e98eb22b55925323c
Aidar Yumaguzhin <aidar3420@gmail.com>
2022-12-05 14:41:46 +0300

    calc for lab4

---
+++ LR4/calc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	cout << "Type the first number: "; cin >> a;
+	cout << "Type the second number: "; cin >> b;
+	cout << "Type the third number: "; cin >> c;
+	if (a >= 0 && b > 0 && c >= 0 && a > b) {
+		cout << a << " + " << b << " = " << a + b << endl;
+		cout << a << " - " << b << " = " << a - b << endl;
+		cout << a << " * " << b << " = " << a * b << endl;
+		cout << a << " / " << b << " = " << a / b << endl;
+		cout << (float)a << ".0" << " / " << (float)b << ".0" << " = " << (float)a / (float)b << endl;
+		cout << a << " % " << b << " = " << a % b << endl;
+		cout << a << " + " << b << " * " << c << " = " << a + b * c << endl;
+		cout << "(" << a << " + " << b << ")"<< " * "<< c << " = " << (a + b) * c << endl;
+	}
+	else {
+		cout << "Invalid data" << endl;
+	}
+	return 0;
+}
