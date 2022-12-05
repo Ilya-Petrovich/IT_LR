83a4640850eefd97c8f1d6589dc36e8afe0b201e
Babkin Nikita <winnikus@gmail.com>
2022-12-05 13:34:28 +0300

    I have no idea

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,14 +1,16 @@
-#include <iostream>	
+#include <iostream>
+
 using namespace std;
+
 int main(){
 	int num_1,num_2,num_3;
 
-	cout<<"Type the first number:";cin>>num_1;
-	cout<<"Type the second number:";cin>>num_2;
-	cout<<"Type the third number:";cin>>num_3;
+	cout << "Type the first number: "; cin >> num_1;
+	cout << "Type the second number: "; cin >> num_2;
+	cout << "Type the third number: "; cin >> num_3;
 
 	if (num_1 < num_2 || num_2 <= 0 || num_3 < 0){
-		cout<<"Invalid data";
+		cout << "Invalid data";
 	}
 
 	else{
