682dd0c2499c90b91f308e8d86781915e1ad9486
Valeriya Zanina <valeriyazanina2004@mail.ru>
2022-12-05 08:30:49 +0300

    df

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -8,19 +8,15 @@ int main()
     cout << "Type the first number: "; cin >> num_1;
     cout << "Type the second number: "; cin >> num_2;
     cout << "Type the third number: "; cin >> num_3;
-    cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
-    cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
-    cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
-    cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-    cout << num_1 << ".0 " << " / " << num_2 << ".0" << " = " << num_1 * 1.0 / num_2 << endl;
-    cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
-    cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
-    cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
-
-    if (num_1 > num_2)
-    {
-        cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
-        cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+	if (num_1 > num_2 && num_2 > 0) {
+		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
+		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
+		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
+		cout << num_1 << ".0 " << " / " << num_2 << ".0" << " = " << num_1 * 1.0 / num_2 << endl;
+		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
+		cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
+		cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
     }
     else
     {
