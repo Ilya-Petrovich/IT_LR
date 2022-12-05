153728925ba968f699077a770f29647341500ff2
Danil Proskura <proskura.danil2017@yandex.ru>
2022-12-05 08:45:41 +0300

    Added LR4 calc try 2

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 	int num_1, num_2, num_3;
-	cout << "¬ведите первое число: ";
+	cout << "Type the first number: ";
 	cin >> num_1;
-	cout << "¬ведите второе число: ";
+	cout << "Type the second number: ";
 	cin >> num_2;
-	cout << "¬ведите третье число: ";
+	cout << "Type the third number: ";
 	cin >> num_3;
 	if (num_1 > num_2) {
 		if ((num_1 >= 0) and (num_2 >= 0) and (num_3 >= 0)) {
@@ -18,7 +17,7 @@ int main()
 			cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 			cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
 			cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-			cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << double(num_1) / num_2 << endl;
+			cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << num_1 * 1. / num_2 << endl;
 			cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
 			cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
 			cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
@@ -30,5 +29,6 @@ int main()
 	}
 	else {
 		cout << "Invalid data" << endl;
+	return 0;
 	}
 }
