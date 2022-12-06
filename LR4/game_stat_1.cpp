a8f21d0a3eaad9ba8cb9a31b8e301548784a9c40
Илья Сапожников <ilushasap@mail.ru>
2022-12-06 13:23:24 +0300

    game stat 1

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -6,9 +6,8 @@ int main()
 	const float max_speed = 110, fuel_capacity = 50, distance = 360;
 	float fuelConsumption, s, speed, distance_left;
 	float fuel, distance_traveled, fuel_consumed, fuel_remained;
-
 	cout << "How much fuel do you have at start? "; cin >> fuel;
-	
+
 	if (0 <= fuel && fuel < fuel_capacity) {
 		cout << "\nCar stats";
 		cout << "\nFuel: " << fuel;
@@ -17,7 +16,7 @@ int main()
 		cout << "\nSpeed: 0";
 		cout << "\nMaximum speed: 110";
 		cout << "\n\nRoute info" << "\nDistance: 360 km";
-		cout << "\n-----------------------------------";
+		cout << "\n----------------------------------------";
 		cout << "\nYou have to drive a route 360 km long.";
 		cout << "\nBut first you need to check if you have enough fuel.";
 
@@ -32,50 +31,40 @@ int main()
 			speed = 110;
 			cout << "\nYour speed before start: " << speed << " km/h";
 		}
-
 		if (speed <= 0) {
 			cout << "\nInvalid data";
 		} else {
 			cout << "\nYour speed before start: " << speed << " km/h";
-
 			s = speed / 10;
 			fuelConsumption = s * s / 4 - 4.5 * s + 30;
 			cout << "\n\nFuel consumption with chosen speed: " << fuelConsumption << " litres per 100 km";
-
 			cout << "\n\nDestination result";
-
 			distance_traveled = (fuel / fuelConsumption) * 100;
 
 			if (distance_traveled > 360) {
 				distance_traveled = 360;
 			}
-
 			if (distance_traveled <= 360) {
 				cout << "\nDistance traveled: " << distance_traveled << " km";
-
 				int time = (distance_traveled / speed) * 3600;
 				cout << "\nTime spent: " << time << " sec";
-
 				fuel_consumed = fuelConsumption * (distance / 100.0F);
 				cout << "\nFuel consumed: " << fuel_consumed << " litres";
-
 				fuel_remained = fuel - fuel_consumed;
 				cout << "\nFuel remained: " << fuel_remained << " litres";
-
-				cout << "\n--------------------------------------------";
+				cout << "\n----------------------------------------";
 			}
-
 			if (distance_traveled == 360) {
 				cout << "\nCongratulations! You have reached destination point!";
 			}
-
 			if (distance_traveled < 360) {
 				distance_left = distance - distance_traveled;
 				cout << "\nYou haven't reached destination point. " << distance_left << " km left.";
 			}
 		}
-	} 
-	else cout << "Invalid data" << endl;
-	
+	} else {
+		cout << "Invalid data" << endl;
+	}
+
 	return 0;
 }
