ec0093a6407828d626bf351b6615b6cf3f5783dd
Khalil Alimzhanov <alimzhanov.2005@list.ru>
2022-12-06 20:32:42 +0300

    Changed LR6

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,177 +1,162 @@
-﻿#include <unistd.h>
+﻿#include <iostream>
 #include <vector>
-#include <iostream>
 #include <string>
+#include <ctime>
+#include <unistd.h>
 using namespace std;
 
-int count = 0;
-
-void print(vector<string> sticman);
-void box();
-void dance(vector<string> sticman);
-void start_state();
-
-int main()
-{ // 1
-	vector<string> sticman =
-	{
-	"    /\\    ",//0
-	"   |oo|   ",//1
-	"  (|..|)  ",//2
-	"   |--|   ",//3
-	"   \\__/   ",//4
-	"    ||    ",//5
-	"  -| *|-  ",//6
-	" / |  | \\ ",//7
-	"/   \\/   \\",//8
-	"    /\\    ",//9
-	"    ||    ",//10
-	"   _||_   "//11
-	};
-	
-	dance(sticman);
-	//cout << count;  
-	return 0;
-}
-void print(vector<string> person)
-{
-	for (auto c : person)
+void print(vector<string>a){
+	for (auto c : a) {
 		cout << c << endl;
-	count+= 1;
-}
-
-void dance(vector<string> sticman)
-{
-	print(sticman);
-	int x = 650;
-	for (int i = 0; i < 2; i++)
-	{	// state 1
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[6] = " --| *|-- ";
-		sticman[7] = "/  |  |  \\";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 2
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[6] = "  -| *|---";
-		sticman[7] = " / |  |   ";
-		sticman[8] = "/   \\/    ";
-		print(sticman);
-
-		//state 3
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[5] = "    ||   /";
-		sticman[6] = " --| *|-- ";
-		sticman[7] = "/  |  |   ";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 4
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/  /";
-		sticman[5] = "    ||  / ";
-		sticman[6] = "  -| *|-  ";
-		sticman[7] = " / |  |   ";
-		sticman[8] = "/   \\/    ";
-		print(sticman);
-
-		//state 5
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/  *";
-		sticman[5] = "    ||  / ";
-		sticman[6] = " --| *|-  ";
-		sticman[7] = "/  |  |   ";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 6
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/  /";
-		sticman[5] = "    ||  / ";
-		sticman[6] = "---| *|-  ";
-		sticman[7] = "   |  |   ";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 7
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/  *";
-		sticman[5] = "\\   ||  / ";
-		sticman[6] = " --| *|-  ";
-		sticman[7] = "   |  |   ";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		// state 8
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "\\  \\__/  /";
-		sticman[5] = " \\  ||  / ";
-		sticman[6] = "  -| *|- ";
-		sticman[7] = "   |  |  ";
-		sticman[8] = "    \\/   ";
-		print(sticman);
-
-		// state 9
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "*  \\__/  *";
-		sticman[5] = " \\  ||  / ";
-		sticman[6] = "  -| *|-  ";
-		sticman[7] = "   |  |   ";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 10
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/   ";
-		sticman[5] = "*   ||   *";
-		sticman[6] = " --| *|-- ";
-		sticman[7] = "   |  |   ";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 11
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/   ";
-		sticman[5] = "    ||    ";
-		sticman[6] = "*--| *|--*";
-		sticman[7] = "   |  |   ";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 12
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/   ";
-		sticman[5] = "    ||    ";
-		sticman[6] = " --| *|-- ";
-		sticman[7] = "*  |  |  *";
-		sticman[8] = "    \\/    ";
-		print(sticman);
-
-		//state 13
-		//Sleep(x * 1000);
-		//system("cls");
-		sticman[4] = "   \\__/   ";
-		sticman[5] = "    ||    ";
-		sticman[6] = "  -| *|-  ";
-		sticman[7] = " / |  | \\ ";
-		sticman[8] = "*   \\/   *";
-		print(sticman);
-
-		// state 14
-		sticman[8] = "/   \\/   \\";
-		print(sticman);
 	}
+}
+void anim1(vector<string>b){
+	b[6] = " --| *|-- ";
+	b[7] = "/  |  |  \\";
+	b[8][0] = ' ';
+	b[8][size(b[8])-1] = ' ';
+	print(b);
+}
+void anim2(vector<string>b) {
+	b[6] = "  -| *|---";
+	b[7] = " / |  |";
+	b[8] = "/   \\/";
+	int d;
+	print(b);
+}
+void anim3(vector<string>b) {
+	b[5] = "    ||   /";
+	b[6] = " --| *|--";
+	b[7] = "/  |  | ";
+	b[8] = "    \\/   ";
+	print(b);
+}
+void anim4(vector<string>b) {
+	b[5][8] = '/';
+	b[4][9] = '/';
+	b[7][8] = ' ';
+	b[8][9] = ' ';
+	print(b);
+}
+void anim5(vector<string>b) {
+	b[4][9] = '*';
+	b[5] = "    ||  / ";
+	b[6] = " --| *|- ";
+	b[7] = "/  |  | ";
+	b[8] = "    \\/   ";
+	print(b);
+}
+void anim6(vector<string>b) {
+	b[4][9] = '/';
+	b[5] = "    ||  / ";
+	b[6] = "---| *|- ";
+	b[7] = "   |  | ";
+	b[8] = "    \\/   ";
+	print(b);
+}
+void anim7(vector<string>b) {
+	b[4][9] = '*';
+	b[5] = "\\   ||  / ";
+	b[6] = " --| *|- ";
+	b[7] = "   |  | ";
+	b[8] = "    \\/   ";
+	print(b);
+}
+void anim8(vector<string>b){
+	b[4] = "\\  \\__/  /";
+	b[5] = " \\  ||  /";
+	b[6] = "  -| *|-  ";
+	b[7] = "   |  |   ";
+	b[8] = "    \\/     ";
+	print(b);
+}
+void anim9(vector<string>b) {
+	b[4] = "*  \\__/  *";
+	b[5] = " \\  ||  /";
+	b[6] = "  -| *|-  ";
+	b[7] = "   |  |   ";
+	b[8] = "    \\/     ";
+	print(b);
+}
+void anim10(vector<string>b) {
+	b[4] = "   \\__/  ";
+	b[5] = "*   ||   *";
+	b[6] = " --| *|-- ";
+	b[7] = "   |  |   ";
+	b[8] = "    \\/     ";
+	print(b);
+}
+void anim11(vector<string>b) {
+	b[4] = "   \\__/  ";
+	b[5] = "    ||    ";
+	b[6] = "*--| *|--*";
+	b[7] = "   |  |   ";
+	b[8] = "    \\/     ";
+	print(b);
+}
+void anim12(vector<string>b) {
+	b[4] = "   \\__/  ";
+	b[5] = "    ||    ";
+	b[6] = " --| *|-- ";
+	b[7] = "*  |  |  *";
+	b[8] = "    \\/     ";
+	print(b);
+}
+void anim13(vector<string>b) {
+	b[4] = "   \\__/  ";
+	b[5] = "    ||    ";
+	b[6] = "  -| *|-  ";
+	b[7] = " / |  |  \\";
+	b[8] = "*   \\/    *";
+	print(b);
+}
+int main(){
+					//012345678910
+vector<string> a = {"    /\\    ",//0
+					"   |oo|   ",//1
+					"  (|..|)  ",//2
+					"   |--|   ",//3
+					"   \\__/   ",//4
+					"    ||     ",//5
+					"  -| *|-  ",//6
+					" / |  | \\ ",//7
+					"/   \\/   \\",//8
+					"    /\\    ",//9
+					"    ||    ",//10
+					"   _||_   "};//11
+for (int j = 0; j < 2; ++j) {
+	for (int i = 0; i < 14; i++) {
+		if (i == 0)
+			print(a);
+		if (i == 1)
+			anim1(a);
+		if (i == 2)
+			anim2(a);
+		if (i == 3)
+			anim3(a);
+		if (i == 4)
+			anim4(a);
+		if (i == 5)
+			anim5(a);
+		if (i == 6)
+			anim6(a);
+		if (i == 7)
+			anim7(a);
+		if (i == 8)
+			anim8(a);
+		if (i == 9)
+			anim9(a);
+		if (i == 10)
+			anim10(a);
+		if (i == 11)
+			anim11(a);
+		if (i == 12)
+			anim12(a);
+		if (i == 13)
+			anim13(a);
+		//Sleep(5000);
+		//system("cls");
+	}
+}
+	return 0;
 }
