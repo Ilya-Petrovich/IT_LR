b0f81fd3143f9fb15f2fd5fc8f570e8b7b218952
Matizheva <annaanna.mat2004@gmail.com>
2022-12-06 13:37:43 +0300

    30 вариант

---
+++ LR6/animation_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <unistd.h>
+int main()
+{
+    int x=1000,y=1;
+    system("cls");
+    for(y;y<3;y++){
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n  -| *|-\n / |  | \\\n/   \\/   \\\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n --| *|--\n/  |  |  \\\n    \\/    \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n  -| *|---\n / |  |\n/   \\/\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||   /\n --| *|--\n/  |  |\n    \\/\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  /\n    ||  /\n  -| *|-\n / |  | \n/   \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  *\n    ||  /\n --| *|-\n/  |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  /\n    ||  /\n---| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  *\n\\   ||  /\n --| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n\\  \\__/  /\n \\  ||  /\n  -| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n*  \\__/  *\n \\  ||  /\n  -| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n*   ||   *\n --| *|--\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||   \n*--| *|--*\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||   \n --| *|--\n*  |  |  *\n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n  -| *|-\n / |  | \\\n*   \\/   *\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        
+    }
+    std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n  -| *|-\n / |  | \\\n/   \\/   \\\n    /\\\n    ||\n   _||_";
+    //Sleep(x);
+    //system("cls");
+    return 0;
+}
+
