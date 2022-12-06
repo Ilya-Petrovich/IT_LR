807de47e29852dcda2d527e3a546d86d590d9b4e
Khalil Alimzhanov <alimzhanov.2005@list.ru>
2022-12-06 18:37:50 +0300

    Changed LR6

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -13,18 +13,18 @@ int main()
 { // 1
 	vector<string> sticman =
 	{
-	"      /\\      ",//0
-	"     |oo|     ",//1
-	"    (|..|)    ",//2
-	"     |--|     ",//3
-	"     \\__/     ",//4
-	"      ||      ",//5
-	"    -| *|-    ",//6
-	"   / |  | \\   ",//7
-	"  /   \\/   \\   ",//8
-	"      /\\      ",//9
-	"      ||      ",//10
-	"     _||_     ",//11
+	"    /\\    ",//0
+	"   |oo|   ",//1
+	"  (|..|)  ",//2
+	"   |--|   ",//3
+	"   \\__/   ",//4
+	"    ||    ",//5
+	"  -| *|-  ",//6
+	" / |  | \\ ",//7
+	"/   \\/   \\",//8
+	"    /\\    ",//9
+	"    ||    ",//10
+	"   _||_   ",//11
 	};
 	print(sticman);
 	dance(sticman);
@@ -43,126 +43,126 @@ void dance(vector<string> sticman)
 	{	// state 1
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[6] = "   --| *|--    ";
-		sticman[7] = "  /  |  |  \\  ";
-		sticman[8] = "      \\/      ";
+		sticman[6] = " --| *|-- ";
+		sticman[7] = "/  |  |  \\";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 2
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[6] = "    -| *|---  ";
-		sticman[7] = "   / |  |     ";
-		sticman[8] = "  /   \\/     ";
+		sticman[6] = "  -| *|---";
+		sticman[7] = " / |  |   ";
+		sticman[8] = "/   \\/    ";
 		print(sticman);
 
 		//state 3
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[5] = "      ||   /   ";
-		sticman[6] = "   --| *|--    ";
-		sticman[7] = "  /  |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[5] = "    ||   /";
+		sticman[6] = " --| *|-- ";
+		sticman[7] = "/  |  |   ";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 4
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/  /    ";
-		sticman[5] = "      ||  /   ";
-		sticman[6] = "    -| *|-    ";
-		sticman[7] = "   / |  |      ";
-		sticman[8] = "  /   \\/      ";
+		sticman[4] = "   \\__/  /";
+		sticman[5] = "    ||  / ";
+		sticman[6] = "  -| *|-  ";
+		sticman[7] = " / |  |   ";
+		sticman[8] = "/   \\/    ";
 		print(sticman);
 
 		//state 5
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/  *    ";
-		sticman[5] = "      ||  /   ";
-		sticman[6] = "   --| *|-    ";
-		sticman[7] = "  /  |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "   \\__/  *";
+		sticman[5] = "    ||  / ";
+		sticman[6] = " --| *|-  ";
+		sticman[7] = "/  |  |   ";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 6
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/  /    ";
-		sticman[5] = "      ||  /   ";
-		sticman[6] = "  ---| *|-    ";
-		sticman[7] = "     |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "   \\__/  /";
+		sticman[5] = "    ||  / ";
+		sticman[6] = "---| *|-  ";
+		sticman[7] = "   |  |   ";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 7
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/  *    ";
-		sticman[5] = "  \\   ||  /   ";
-		sticman[6] = "   --| *|-    ";
-		sticman[7] = "     |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "   \\__/  *";
+		sticman[5] = "\\   ||  / ";
+		sticman[6] = " --| *|-  ";
+		sticman[7] = "   |  |   ";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		// state 8
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = " \\   \\__/  /    ";
-		sticman[5] = "  \\   ||  /   ";
-		sticman[6] = "   --| *|-    ";
-		sticman[7] = "     |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "\\  \\__/  /";
+		sticman[5] = " \\  ||  / ";
+		sticman[6] = "  -| *|- ";
+		sticman[7] = "   |  |  ";
+		sticman[8] = "    \\/   ";
 		print(sticman);
 
 		// state 9
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = " *   \\__/  *   ";
-		sticman[5] = "  \\   ||  /   ";
-		sticman[6] = "   --| *|-    ";
-		sticman[7] = "     |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "*  \\__/  *";
+		sticman[5] = " \\  ||  / ";
+		sticman[6] = "  -| *|-  ";
+		sticman[7] = "   |  |   ";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 10
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/     ";
-		sticman[5] = "  *   ||   *   ";
-		sticman[6] = "   --| *|--   ";
-		sticman[7] = "     |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "   \\__/   ";
+		sticman[5] = "*   ||   *";
+		sticman[6] = " --| *|-- ";
+		sticman[7] = "   |  |   ";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 11
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/     ";
-		sticman[5] = "      ||       ";
-		sticman[6] = "  *--| *|--*  ";
-		sticman[7] = "     |  |      ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "   \\__/   ";
+		sticman[5] = "    ||    ";
+		sticman[6] = "*--| *|--*";
+		sticman[7] = "   |  |   ";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 12
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/     ";
-		sticman[5] = "      ||       ";
-		sticman[6] = "   --| *|--   ";
-		sticman[7] = "  *  |  |  *   ";
-		sticman[8] = "      \\/      ";
+		sticman[4] = "   \\__/   ";
+		sticman[5] = "    ||    ";
+		sticman[6] = " --| *|-- ";
+		sticman[7] = "*  |  |  *";
+		sticman[8] = "    \\/    ";
 		print(sticman);
 
 		//state 13
 		//Sleep(x * 1000);
 		//system("cls");
-		sticman[4] = "     \\__/     ";
-		sticman[5] = "      ||       ";
-		sticman[6] = "    -| *|-    ";
-		sticman[7] = "  /  |  |  \\   ";
-		sticman[8] = " *    \\/    *  ";
+		sticman[4] = "   \\__/   ";
+		sticman[5] = "    ||    ";
+		sticman[6] = "  -| *|-  ";
+		sticman[7] = " / |  | \\ ";
+		sticman[8] = "*   \\/   *";
 		print(sticman);
         start_state();
 	}
@@ -173,20 +173,18 @@ void start_state() {
 	//system("cls");
 	vector<string> sticman =
 	{
-
-	"      /\\      ",//0
-	"     |oo|     ",//1
-	"    (|..|)    ",//2
-	"     |--|     ",//3
-	"     \\__/     ",//4
-	"      ||      ",//5
-	"    -| *|-    ",//6
-	"   / |  | \\   ",//7
-	"  /   \\/   \\   ",//8
-	"      /\\      ",//9
-	"      ||      ",//10
-	"     _||_     ",//11
-	
+	"    /\\    ",//0
+	"   |oo|   ",//1
+	"  (|..|)  ",//2
+	"   |--|   ",//3
+	"   \\__/   ",//4
+	"    ||    ",//5
+	"  -| *|-  ",//6
+	" / |  | \\ ",//7
+	"/   \\/   \\",//8
+	"    /\\    ",//9
+	"    ||    ",//10
+	"   _||_   ",//11
 	};
 	print(sticman);
 }
