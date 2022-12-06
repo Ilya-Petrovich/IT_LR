e2d0b28bb61d3a1a897e3eaf3d90ab58de935f2a
Anastasia Soloveva <solanand04@mail.ru>
2022-12-06 12:05:32 +0300

    LR4/calc.cpp

---
+++ LR4/calc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num1, num2, num3;
+	cout << "Type the first number: "; cin >> num1;
+	cout << "Type the second number: "; cin >> num2;
+	cout << "Type the third number: "; cin >> num3;
+	if (!(num1 > num2&& num1 >= 0 && num2 > 0 && num3 >= 0)) {
+		cout << "Invalid data" << endl;
+		return 0;
+	}
+	cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+	cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+	cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+	cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+	cout << num1 << ".0" << " / " << num2 << ".0" << " = " << (double)num1 / num2 << endl;
+	cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+	cout << num1 << " + " << num2 << " * " << num3 << " = " << num1 + num2 * num3 << endl;
+	cout << "(" << num1 << " + " << num2 << ")" << " * " << num3 << " = " << (num1 + num2) * num3 << endl;
+	return 0;
+}
