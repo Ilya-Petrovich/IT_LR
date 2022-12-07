f9225285ea64d22b82550474e3b0cfbb9454e267
Amir Arslanov <amir.arslanov.2004@mail.ru>
2022-12-07 18:53:17 +0300

    UP16LR6

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -7,6 +7,7 @@ int main() {
 	float s, speed, fuelconsumption, distance;
 	std::cout << "How much fuel do you have at start? "; std::cin >> fuel;
 	if ( fuel < 0 || fuel > 50) {
+		std::cout << "\n";
 		std::cout << "Invalid data.";
 		return 0;
 	} 
