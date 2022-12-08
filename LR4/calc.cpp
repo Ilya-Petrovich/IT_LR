0b5f18ca9913bee94169f5b6116b10c24ffe0b62
Starikov Roman <im.big.rat@gmail.com>
2022-12-08 10:19:38 +0300

    lr4_1 repush

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,16 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 int InvalidData() {
-    cout << "Invalid data";
+    cout << "Invalid data"<<endl;
     return 0;
 }
 
 int main()
 {
     int num_1, num_2, num_3;
-    cout << "Enter first number: "; cin >> num_1;
-    cout << "Enter second number: "; cin >> num_2;
-    cout << "Enter third number: ";  cin >> num_3;
+    cout << "Type the first number: "; cin >> num_1;
+    cout << "Type the second number: "; cin >> num_2;
+    cout << "Type the third number: ";  cin >> num_3;
     if (num_1 < 0) return InvalidData();
     if (num_2 < 0) return InvalidData();
     if (num_3 < 0) return InvalidData();
@@ -24,3 +24,5 @@ int main()
     cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
     cout <<"("<< num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
 }
+
+
