74722754f8e3c996e231d9665270fac09d00fd81
Alisa Akhtyamova <akhtyamova04@bk.ru>
2022-12-10 10:40:51 +0300

    Changed LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 
 	int num_1, num_2, num_3;
-	
+
 	cout << "Type the first number: ";
 	cin >> num_1;
 	cout << "Type the second number: ";
@@ -12,7 +12,7 @@ int main() {
 	cout << "Type the third number: ";
 	cin >> num_3;
 
-	if (num_1 > num_2) {
+	if (num_1 > num_2 and num_1>=0 and num_2>0 and num_3>=0) {
 		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
 		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
@@ -20,7 +20,7 @@ int main() {
 		cout << (float)num_1 << ".0" << " / " << (float)num_2 << ".0" << " = " << (float)num_1 / (float)num_2 << endl;
 		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
 		cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
-		cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2)*num_3 << endl;
+		cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
 	}
 	else {
 		cout << "Invalid data" << endl;
