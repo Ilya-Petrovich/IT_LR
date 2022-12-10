a63c65800a1d9885cbaf995ba762421032814412
Daniil Bubnov <bublik.den14@gmail.com>
2022-12-10 12:24:39 +0300

    LR4

---
+++ LR4/calc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<clocale>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int num_1, num_2, num_3;
+
+	cout << "¬ведите первое число: ";
+	cin >> num_1;
+	cout << "¬ведите второе число: ";
+	cin >> num_2;
+	cout << "¬ведите третье число: ";
+	cin >> num_3;
+
+	if (num_1 > num_2)
+	{
+		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
+		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
+		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
+		cout << (double) num_1 << ".0" << " / " << (double) num_2 << ".0" << " = " << (double)num_1 / (double)num_2 << endl;
+		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
+		cout << num_1 << " + " << num_2 << " * "<<num_3<< " = " << num_1 + num_2 *num_3<< endl;
+		cout <<"(" << num_1<< " + " << num_2<<")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
+
+	}
+	else
+	{
+		cout << "Invalid data" << endl;
+	}
+}
