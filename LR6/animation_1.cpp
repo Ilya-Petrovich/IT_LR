fd09f8f882fd06aabf524257d548085e93b1fbe1
Arseniy Bayramshin <man.bayramshin@gmail.com>
2022-12-12 01:00:07 +0300

    animation lvl1

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,22 +1,22 @@
 #include <unistd.h>
 #include <iostream>
 
-// void del_one_frame();
+// void rmFrame();
 void head(); void legs();
 void dance();
 
 int main() {
-    // del_one_frame();
+    // rmFrame();
     dance();
     return 0;
 }
 
-// void del_one_frame() {system("cls");}
+// void rmFrame() {system("cls");}
 void head() {
-    std::cout << "    /\\    \n";
-    std::cout << "   |oo|   \n";
-    std::cout << "  (|..|)  \n";
-    std::cout << "   |--|   \n";
+    std::cout << "   ()()   \n";
+    std::cout << "   ()()   \n";
+    std::cout << "   ()()   \n";
+    std::cout << "   |..|   \n";
 }
 void legs() {
     std::cout << "    /\\    \n";
@@ -25,96 +25,96 @@ void legs() {
 }
 void dance() {
     for (int i = 0; i <= 28; i++) {
-        // del_one_frame();
+        // rmFrame();
         head();
         switch (i % 14) {
             case 0: { 
-                std::cout << "   \\__/   \n"; 
-                std::cout << "    ||    \n"; 
-                std::cout << "  -| *|-  \n"; 
-                std::cout << " / |  | \\ \n"; 
-                std::cout << "/   \\/   \\\n"; break;}
-            case 1: { 
-                std::cout << "   \\__/   \n"; 
-                std::cout << "    ||    \n";
-                std::cout << " --| *|-- \n"; 
-                std::cout << "/  |  |  \\\n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "  \\|  |/  \n";
+                std::cout << "  /|UU|\\  \n";
+                std::cout << "  --\\/--  \n";
+                std::cout << " /  ||  \\ \n";
+                std::cout << "/   ||   \\\n";break;}
+            case 1: {
+                std::cout << "  \\|  |/  \n";
+                std::cout << "  /|UU|\\  \n";
+                std::cout << " ---\\/--- \n";
+                std::cout << "/   ||   \\\n";
+                std::cout << "    ||    \n";break;}
             case 2: { 
-                std::cout << "   \\__/   \n"; 
-                std::cout << "    ||    \n";
-                std::cout << "  -| *|---\n"; 
-                std::cout << " / |  |   \n"; 
-                std::cout << "/   \\/    \n"; break;}
+                std::cout << "  \\|  |/  \n";
+                std::cout << "  /|UU|\\  \n";
+                std::cout << "  --\\/----\n";
+                std::cout << " /  ||    \n";
+                std::cout << "/   ||    \n"; break;}
             case 3: { 
-                std::cout << "   \\__/   \n"; 
-                std::cout << "    ||   /\n";
-                std::cout << " --| *|-- \n"; 
-                std::cout << "/  |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "  \\|  |/  \n";
+                std::cout << "  /|UU|\\ /\n";
+                std::cout << " ---\\/--- \n";
+                std::cout << "/   ||   \n";
+                std::cout << "    ||    \n"; break;}
             case 4: { 
-                std::cout << "   \\__/  /\n"; 
-                std::cout << "    ||  / \n";
-                std::cout << "  -| *|-  \n"; 
-                std::cout << " / |  |   \n"; 
-                std::cout << "/   \\/    \n"; break;}
+                std::cout << "  \\|  |/ / \n";
+                std::cout << "  /|UU|\\/  \n";
+                std::cout << "  --\\/--  \n";
+                std::cout << " /  ||   \n";
+                std::cout << "/   ||    \n"; break;}
             case 5: { 
-                std::cout << "   \\__/  *\n"; 
-                std::cout << "    ||  / \n";
-                std::cout << " --| *|-  \n"; 
-                std::cout << "/  |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "  \\|  |/ *\n";
+                std::cout << "  /|UU|\\/ \n";
+                std::cout << " ---\\/--  \n";
+                std::cout << "/   ||    \n";
+                std::cout << "    ||    \n"; break;}
             case 6: { 
-                std::cout << "   \\__/  /\n"; 
-                std::cout << "    ||  / \n";
-                std::cout << "---| *|-  \n"; 
-                std::cout << "   |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "  \\|  |/ /\n";
+                std::cout << "  /|UU|\\/ \n";
+                std::cout << "----\\/--  \n";
+                std::cout << "    ||    \n";
+                std::cout << "    ||    \n"; break;}
             case 7: { 
-                std::cout << "   \\__/  *\n"; 
-                std::cout << "\\   ||  / \n";
-                std::cout << " --| *|-  \n"; 
-                std::cout << "   |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "  \\|  |/ *\n";
+                std::cout << "\\ /|UU|\\/ \n";
+                std::cout << " ---\\/--  \n";
+                std::cout << "    ||    \n";
+                std::cout << "    ||    \n"; break;}
             case 8: { 
-                std::cout << "\\  \\__/  /\n"; 
-                std::cout << " \\  ||  / \n";
-                std::cout << "  -| *|-  \n"; 
-                std::cout << "   |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "\\ \\|  |/ /\n";
+                std::cout << " \\/|UU|\\/ \n";
+                std::cout << "  --\\/--  \n";
+                std::cout << "    ||    \n";
+                std::cout << "    ||    \n"; break;}
             case 9: { 
-                std::cout << "*  \\__/  *\n"; 
-                std::cout << " \\  ||  / \n";
-                std::cout << "  -| *|-  \n"; 
-                std::cout << "   |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "* \\|  |/ *\n";
+                std::cout << "\\ /|UU|\\/ \n";
+                std::cout << "  --\\/--  \n";
+                std::cout << "    ||    \n";
+                std::cout << "    ||    \n"; break;}
             case 10: { 
-                std::cout << "   \\__/   \n"; 
-                std::cout << "*   ||   *\n";
-                std::cout << " --| *|-- \n"; 
-                std::cout << "   |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "  \\|  |/  \n";
+                std::cout << "* /|UU|\\ *\n";
+                std::cout << " ---\\/--- \n";
+                std::cout << "    ||    \n";
+                std::cout << "    ||    \n"; break;}
             case 11: { 
-                std::cout << "   \\__/   \n"; 
+                std::cout << "  \\|  |/  \n";
+                std::cout << "  /|UU|\\  \n";
+                std::cout << "*---\\/---*\n";
                 std::cout << "    ||    \n";
-                std::cout << "*--| *|--*\n"; 
-                std::cout << "   |  |   \n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "    ||    \n"; break;}
             case 12: { 
-                std::cout << "   \\__/   \n"; 
-                std::cout << "    ||    \n";
-                std::cout << " --| *|-- \n"; 
-                std::cout << "*  |  |  *\n"; 
-                std::cout << "    \\/    \n"; break;}
+                std::cout << "  \\|  |/  \n";
+                std::cout << "  /|UU|\\  \n";
+                std::cout << " ---\\/--- \n";
+                std::cout << "*   ||   *\n";
+                std::cout << "    ||    \n"; break;}
             case 13: { 
-                std::cout << "   \\__/   \n"; 
-                std::cout << "    ||    \n";
-                std::cout << "  -| *|-  \n"; 
-                std::cout << " / |  | \\ \n"; 
-                std::cout << "*   \\/   *\n"; break;}
+                std::cout << "  \\|  |/  \n";
+                std::cout << "  /|UU|\\  \n";
+                std::cout << "  --\\/-- \n";
+                std::cout << " /  ||  \\\n";
+                std::cout << "*   ||   *\n"; break;}
         }
         legs();
         // Sleep(500);
-        // del_one_frame();
+        // rmFrame();
     }
 }
