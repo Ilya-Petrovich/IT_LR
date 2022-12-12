b9bf6bf41166788a84975548f57d753bcdf7198d
Oscar Yakubov <greengo1375@gmail.com>
2022-12-12 12:49:36 +0300

    Changed LR4

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -1,91 +1,46 @@
-#include<iostream>
-#include<string>
-#include<conio.h>
-#include<unistd.h>
+﻿#include <iostream>
 using namespace std;
-inline void foo(string* ar);
-int main() {
-	string  ar[12];
-	string str1 = ar[0] = "   ()()   ";
-	string str2 = ar[1] = "   ()()   ";
-	string str3 = ar[2] = "   ()()   ";
-	string str4 = ar[3] = "   |..|   ";
-	string str5 = ar[4] = "  \\|  |/  ";
-	string str6 = ar[5] = "  /|UU|\\  ";
-	string str7 = ar[6] = "  --\\/--  ";
-	string str8 = ar[7] = " /  ||  \\ ";
-	string str9 = ar[8] = "/   ||   \\";
-	string str10 = ar[9] = "    /\\    ";
-	string str11 = ar[10] = "    ||    ";
-	string str12 = ar[11] = "   _||_   ";
-	foo(&ar[0]);
-	ar[6] = " ---\\/--- ";
-	ar[7] = "/   ||   \\";
-	ar[8] = "    ||    ";
-	foo(&ar[0]);
-	ar[6] = "  --\\/----";
-	ar[7] = " /  ||    ";
-	ar[8] = "/   ||    ";
-	foo(&ar[0]);
-	ar[5] = "  /|UU|\\ /";
-	ar[6] = " ---\\/--- ";
-	ar[7] = "/   ||    ";
-	ar[8] = "    ||    ";
-	foo(&ar[0]);
-	ar[4] = "  \\|  |/ /";
-	ar[5] = "  /|UU|\\/ ";
-	ar[6] = "  --\\/--  ";
-	ar[7] = " /  ||    ";
-	ar[8] = "/   ||    ";
-	foo(&ar[0]);
-	ar[4] = "  \\|  |/ *";
-	ar[5] = "  /|UU|\\/ ";
-	ar[6] = " ---\\/--  ";
-	ar[7] = "/   ||    ";
-	ar[8] = "    ||    ";
-	foo(&ar[0]);
-	ar[4] = "  \\|  |/ /";
-	ar[5] = "  /|UU|\\/ ";
-	ar[6] = "----\\/--  ";
-	ar[7] = "    ||    ";
-	foo(&ar[0]);
-	ar[4] = "  \\|  |/ *";
-	ar[5] = "\\ /|UU|\\/ ";
-	ar[6] = " ---\\/--  ";
-	foo(&ar[0]);
-	ar[4] = "\\ \\|  |/ /";
-	ar[5] = " \\/|UU|\\/ ";
-	ar[6] = "  --\\/--  ";
-	foo(&ar[0]);
-	ar[4] = "* \\|  |/ *";
-	foo(&ar[0]);
-	ar[4] = "  \\|  |/  ";
-	ar[5] = "* /|UU|\\ *";
-	ar[6] = " ---\\/--- ";
-	foo(&ar[0]);
-	system("cls");
-	ar[5] = "  /|UU|\\  ";
-	ar[6] = "*---\\/---*";
-	foo(&ar[0]);
-	ar[6] = " ---\\/--- ";
-	ar[7] = "*   ||   *";
-	foo(&ar[0]);
-	ar[6] = "  --\\/--  ";
-	ar[7] = " /  ||  \\ ";
-	ar[8] = "*   ||   *";
-	foo(&ar[0]);
-	ar[6] = "  --\\/--  ";
-	ar[7] = " /  ||  \\ ";
-	ar[8] = "/   ||   \\";
 
-	foo(&ar[0]);
-	return 0;
-}
-inline void foo(string* ar) {
-	for (int i = 0; i < 12; i++) {
-		cout << *ar << endl;
-		*ar++;
-	}
-	usleep(? * 1000);
-	system("cls");
+int main()
+{
+    float f = 0, sp = 0, s = 0, d = 360, dt = 0, fuelConsumed = 0, fuelConsumption = 0, fuelRemained = 0;
+    int t = 0;
+    cout << "How much fuel do you have at start? "; cin >> f;
+    if (f < 0 or f>50) {
+        cout << "\nInvalid data." << endl;
+        system("pause");
+        return 0;
+    }
+    cout << "\nCar stats\nFuel: " << f << "\nFuel capacity: 50\nFuel consumption: 0\nSpeed: 0\nMaximum speed: 110\n";
+    cout << "\nRoute info\nDistance: 360 km\n";
+    cout << "----------------------------------------\nYou have to drive a route 360 km long.\nBut first you need to check if you have enough fuel." << endl;
+    if (f < 26) {
+        f += 25; cout << "\nYour fuel before start: " << f << endl;
+    }
+    cout << "\nWhat speed will you choose? "; cin >> sp;
+    if (sp > 110) sp = 110;
+    if (sp < 0) {
+        cout << "\nInvalid data." << endl;
+        system("pause");
+        return 0;
+    }
+    cout << "Your speed before start: " << sp << " km/h\n";
+    s = sp / 10;
+    fuelConsumption = s * s / 4 - 4.5 * s + 30;
+    if (sp == 0) fuelConsumption = 0;
+    cout << "\nFuel consumption with chosen speed: " << fuelConsumption << " litres per 100 km\n";
+    dt = f / fuelConsumption * 100;
+    if (dt > 360) dt = 360;
+    t = dt / sp * 3600;
+    fuelConsumed = fuelConsumption * dt / 100;
+    fuelRemained = f - fuelConsumed;
+    if (fuelRemained < 0) fuelRemained = 0;
+    if (sp == 0) {
+        t = 0; dt = 0;
+    }
+    cout << "\nDestination result\nDistance traveled: " << dt << " km\nTime spent: " << t << " sec\nFuel consumed: " << fuelConsumed << " litres\nFuel remained: " << fuelRemained << " litres\n----------------------------------------\n";
+    if (dt == 360) cout << "Congratulations! You have reached destination point!" << endl;
+    else cout << "You haven't reached destination point. " << 360 - dt << " km left." << endl;
+    system("pause");
+    return 0;
 }
