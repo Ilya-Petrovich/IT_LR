d62504eef8b60ce0df11d04a1276bfd33f25d10d
Liza Efarova <lizaefarova@mail.ru>
2022-12-12 12:44:08 +0300

    Change: LR7

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -24,12 +24,12 @@ int main() {
 	fread(buff, sizeof(char), fileSize, file);
 	readText(buff, fileSize);
 	fclose(file);
-	cin >> filename;
+	/*cin >> filename;
 	file = fopen(filename, "wb");
 	changeColor(buff, fileSize);
 	writeText(buff, fileSize);
 	fwrite(buff, sizeof(char), fileSize, file);
-	fclose(file);
+	fclose(file);*/
 	delete[] buff;
 	return 0;
 }
