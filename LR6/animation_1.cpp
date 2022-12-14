af5f4e440a9758a4750cc3e8af2e55d869be1597
Matizheva <annaanna.mat2004@gmail.com>
2022-12-14 15:19:57 +0300

    30 вариант

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,95 +1,54 @@
-#include<iostream>
-#include<unistd.h>
-#include<string>
+#include <iostream>
+#include <unistd.h>
+int main()
+{
+    int x=700,y=1;
+    //system("cls");
+    for(y;y<4;y++){
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n  -| *|-\n / |  | \\\n/   \\/   \\\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        if (y==3)exit(0);
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n --| *|--\n/  |  |  \\\n    \\/    \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n  -| *|---\n / |  |\n/   \\/\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||   /\n --| *|--\n/  |  |\n    \\/\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  /\n    ||  /\n  -| *|-\n / |  | \n/   \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  *\n    ||  /\n --| *|-\n/  |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  /\n    ||  /\n---| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/  *\n\\   ||  /\n --| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n\\  \\__/  /\n \\  ||  /\n  -| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n*  \\__/  *\n \\  ||  /\n  -| *|-\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n*   ||   *\n --| *|--\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||   \n*--| *|--*\n   |  | \n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||   \n --| *|--\n*  |  |  *\n    \\/   \n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        std::cout<<"    /\\\n   |oo|\n  (|..|)\n   |--|\n   \\__/\n    ||\n  -| *|-\n / |  | \\\n*   \\/   *\n    /\\\n    ||\n   _||_";
+        //Sleep(x);
+        //system("cls");
+        
+    }
+}
 
-using namespace std;
-void nnn(string chel[],int x) {
-	for (int i = 0; i < x; i++) cout << chel[i] << endl;
-	//Sleep(600);
-	//system("cls");
-}
-void dance(string chel[],int x) {
-	int i = 0;
-	while (i < 2) {
-		nnn(chel, x);
-		chel[6] = "  --| *|--     ";
-		chel[7] = " /  |  |  \\";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[6] = "   -| *|---";
-		chel[7] = "  / |  |";
-		chel[8] = " /   \\/";
-		nnn(chel, x);
-		chel[5] = "     ||   /";
-		chel[6] = "  --| *|--";
-		chel[7] = " /  |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[4] = "    \\__/   /";
-		chel[5] = "     ||   /";
-		chel[6] = "   -| *|--";
-		chel[7] = "  / |  |";
-		chel[8] = " /   \\/";
-		nnn(chel, x);
-		chel[4] = "    \\__/   *";
-		chel[5] = "     ||   /";
-		chel[6] = "  --| *|-";
-		chel[7] = " /  |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[4] = "    \\__/  /";
-		chel[5] = "     ||  /";
-		chel[6] = " ---| *|-";
-		chel[7] = "    |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[4] = "    \\__/  *";
-		chel[5] = " \\   ||  /";
-		chel[6] = "  --| *|-";
-		chel[7] = "    |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[4] = " \\  \\__/  /";
-		chel[5] = "  \\  ||  /";
-		chel[6] = "   -| *|-";
-		chel[7] = "    |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[4] = " *  \\__/  *";
-		chel[5] = "  \\  ||  /";
-		chel[6] = "   -| *|-";
-		chel[7] = "    |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[4] = "    \\__/";
-		chel[5] = " *   ||   *";
-		chel[6] = "  --| *|--";
-		chel[7] = "    |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[5] = "     ||";
-		chel[6] = " *--| *|--*";
-		chel[7] = "    |  |";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[6] = "  --| *|--";
-		chel[7] = " *  |  |  *";
-		chel[8] = "     \\/";
-		nnn(chel, x);
-		chel[6] = "   -| *|-";
-		chel[7] = "  / |  | \\";
-		chel[8] = " *   \\/   *";
-		nnn(chel, x);
-		chel[6] = "   -| *|-";
-		chel[7] = "  / |  | \\";
-		chel[8] = " /   \\/   \\";
-		nnn(chel, x);
-		i++;
-	}
-}
-int main() {
-	const int x = 12;
-	string chel[x] = {"     /\\     ","    |oo|      ","   (|..|)        ","    |--|      ","    \\__/               ","     ||         ","   -| *|-        ","  / |  | \\        "," /   \\/   \\        ","     /\\        ","     ||","    _||_"};
-	dance(chel,x);
-	return 0;
-}
