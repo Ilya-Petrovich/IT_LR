0afe0129e497f8ec6a667355c0481712af3d7d67
milenaminova <aminovamilena@gmail.com>
2022-12-14 13:39:00 +0300

    game_stat_1.cpp

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -13,7 +13,7 @@ int main()
 	}
 
 	std::cout << "\nCar stats" << "\nFuel: " << fuel << "\nFuel capacity: 50\nFuel consumption: 0\n";
-	std::cout << "\nSpeed: 0\nMaximum speed: 110\n";
+	std::cout << "Speed: 0\nMaximum speed: 110\n";
 	std::cout << "\nRoute info\nDistance: 360 km\n----------------------------------------";
 	std::cout << "\nYou have to drive a route 360 km long.\nBut first you need to check if you have enough fuel.\n";
 
