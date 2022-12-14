39232ae330b67fdfd2261b8e64cbd5d29063a4ae
Dinar Nuretdinov <dinar.nuretdinov12@mail.ru>
2022-12-14 21:47:47 +0300

    calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -9,7 +9,7 @@ int main() {
     cout << "Type the second number: "; cin >> num_2;
     cout << "Type the third number: "; cin >> num_3;
 
-    if (num_1 > num_2) {
+    if (num_1 > num_2 && num_2 > 0 && num_3 >= 0) {
         cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
         cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
         cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
