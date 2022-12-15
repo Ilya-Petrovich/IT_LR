36703aa7ab681a2a291dabde3cda4cb221ea6c25
Rail Gussamov <railgussamov@gmail.com>
2022-12-15 14:28:05 +0300

    Changed

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -16,7 +16,7 @@ int main()
 		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
 		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
 		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
-		cout << num_1 << ".0 / " << num_2 << ".0 = " << float(num_1) / float(num_2) << endl;
+		cout << float(num_1) << ".0 / " << num_2 << ".0 = " << float(num_1) / float(num_2) << endl;
 		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
 		cout << num_1 << " + " << num_2 << " * 5 = " << num_1 + num_2 * 5 << endl;
 		cout << "(" << num_1 << ") + (" << num_2 << ") * 5 = " << (num_1 + num_2) * 5 << endl;
