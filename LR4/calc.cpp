eb0acd58064cb1286ec2f22f90f47ba7550806cd
Babkin Nikita <winnikus@gmail.com>
2022-12-15 10:19:13 +0300

    Calc

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -6,15 +6,15 @@ int main(){
 	int num_1,num_2,num_3;
 
 	cout << "Type the first number: "; cin >> num_1; 
-	cout << "\nType the second number: "; cin >> num_2;
-	cout << "\nType the third number: "; cin >> num_3;
+	cout << "Type the second number: "; cin >> num_2;
+	cout << "Type the third number: "; cin >> num_3;
 
 	if (num_1 < num_2 || num_2 <= 0 || num_3 < 0){
-		cout << "\nInvalid data";
+		cout << "Invalid data\n";
 	}
 
 	else{
-		printf("\n%d + %d = %d\n", num_1, num_2, num_1+num_2);
+		printf("%d + %d = %d\n", num_1, num_2, num_1+num_2);
 		printf("%d - %d = %d\n", num_1, num_2, num_1-num_2);
 		printf("%d * %d = %d\n", num_1, num_2, num_1*num_2);
 		printf("%d / %d = %d\n", num_1, num_2, num_1/num_2);
