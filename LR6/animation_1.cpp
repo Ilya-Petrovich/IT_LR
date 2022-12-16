431d2c0d321df010fabadc97c92bf2b3f8d9a65d
Aidar Yumaguzhin <aidar3420@gmail.com>
2022-12-16 14:05:49 +0300

    g

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,174 +1,158 @@
-﻿#include <iostream>
-#include <string>
-//#include <unistd.h> 
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unistd.h>
 using namespace std;
-void animation(string *person)
-{
-	for (int i = 0; i < 12; i++) {
-		cout << *person++ << endl;
-	}
-}
+
+const int a = 10, b = 12;
+
 int main()
 {
-	string human0[12] = { "   _[]_   ", "   |~~|   ", "   |oq|   ", "  (|<>|)  ", "   \\--/   ", "   _><_   ", "  -\\. /-  ", " / |. | \\ ", "/  |. |  \\", "   |__|   ", "    ||    ", "   _||_   " };
-	animation(human0);
-	human0[6] = " --\\. /-- ";
-	human0[7] = "/  |. |  \\";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[6] = "  -\\. /---";
-	human0[7] = " / |. |   ";
-	human0[8] = "/  |. |   ";
-	animation(human0);
-	human0[5] = "   _><_  /";
-	human0[6] = " --\\. /-- ";
-	human0[7] = "/  |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  /";
-	human0[5] = "   _><_ / ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = " / |. |   ";
-	human0[8] = "/  |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  *";
-	human0[5] = "   _><_ / ";
-	human0[6] = " --\\. /-  ";
-	human0[7] = "/  |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  /";
-	human0[5] = "   _><_ / ";
-	human0[6] = "---\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  *";
-	human0[5] = "\\  _><_ / ";
-	human0[6] = " --\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "\\  \\--/  /";
-	human0[5] = " \\ _><_ / ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "*  \\--/  *";
-	human0[5] = " \\ _><_ / ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "*  _><_  *";
-	human0[6] = " --\\. /-- ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = "*--\\. /--*";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = " --\\. /-- ";
-	human0[7] = "*  |. |  *";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = " / |. | \\ ";
-	human0[8] = "*  |. |  *";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = " / |. | \\ ";
-	human0[8] = "/  |. |  \\";
-	animation(human0);
-	human0[6] = " --\\. /-- ";
-	human0[7] = "/  |. |  \\";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[6] = "  -\\. /---";
-	human0[7] = " / |. |   ";
-	human0[8] = "/  |. |   ";
-	animation(human0);
-	human0[5] = "   _><_  /";
-	human0[6] = " --\\. /-- ";
-	human0[7] = "/  |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  /";
-	human0[5] = "   _><_ / ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = " / |. |   ";
-	human0[8] = "/  |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  *";
-	human0[5] = "   _><_ / ";
-	human0[6] = " --\\. /-  ";
-	human0[7] = "/  |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  /";
-	human0[5] = "   _><_ / ";
-	human0[6] = "---\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/  *";
-	human0[5] = "\\  _><_ / ";
-	human0[6] = " --\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "\\  \\--/  /";
-	human0[5] = " \\ _><_ / ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "*  \\--/  *";
-	human0[5] = " \\ _><_ / ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "*  _><_  *";
-	human0[6] = " --\\. /-- ";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = "*--\\. /--*";
-	human0[7] = "   |. |   ";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = " --\\. /-- ";
-	human0[7] = "*  |. |  *";
-	human0[8] = "   |. |   ";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = " / |. | \\ ";
-	human0[8] = "*  |. |  *";
-	animation(human0);
-	human0[4] = "   \\--/   ";
-	human0[5] = "   _><_   ";
-	human0[6] = "  -\\. /-  ";
-	human0[7] = " / |. | \\ ";
-	human0[8] = "/  |. |  \\";
-	animation(human0);
-	return 0;
+    string anim[b] = {
+        "   ()()   ",
+        "   ()()   ",
+        "   ()()   ",
+        "   |..|   ",
+        "  \\|  |/  ",
+        "  /|UU|\\  ",
+        "  --\\/--  ",
+        " /  ||  \\ ",
+        "/   ||   \\",
+        "    /\\    ",
+        "    ||    ",
+        "   _||_   ",
+    };
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+
+        //Sleep(300);
+        //system("cls");
+        anim[6] = " ---\\/--- ";
+        anim[7] = "/   ||   \\";
+        anim[8] = "    ||    ";
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+
+        //Sleep(300);
+        //system("cls");
+        anim[6] = "  --\\/----";
+        anim[7] = " /  ||    ";
+        anim[8] = "/   ||    ";
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+
+        //Sleep(300);
+        //system("cls");
+
+        anim[5] = "  /|UU|\\ /";
+        anim[6] = " ---\\/--- ";
+        anim[7] = "/   ||    ";
+        anim[8] = "    ||    ";
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[4] = "  \\|  |/ /";
+        anim[5] = "  /|UU|\\/ ";
+        anim[6] = "  --\\/--  ";
+        anim[7] = " /  ||    ";
+        anim[8] = "/   ||    ";
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[4] = "  \\|  |/ *";
+        anim[5] = "  /|UU|\\/ ";
+        anim[6] = " ---\\/--  ";
+        anim[7] = "/   ||    ";
+        anim[8] = "    ||    ";
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[4] = "  \\|  |/ /";
+        anim[5] = "  /|UU|\\/ ";
+        anim[6] = "----\\/--  ";
+        anim[7] = "    ||    ";
+
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[4] = "  \\|  |/ *";
+        anim[5] = "\\ /|UU|\\/ ";
+        anim[6] = " ---\\/--  ";
+        anim[7] = "    ||    ";
+
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[4] = "\\ \\|  |/ /";
+        anim[5] = " \\/|UU|\\/ ";
+        anim[6] = "  --\\/--  ";
+        anim[7] = "    ||    ";
+
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[4] = "* \\|  |/ *";
+        anim[5] = " \\/|UU|\\/ ";
+        anim[7] = "    ||    ";
+
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[4] = "  \\|  |/  ";
+        anim[5] = "* /|UU|\\ *";
+        anim[6] = " ---\\/--- ";
+        anim[7] = "    ||    ";
+
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[5] = "  /|UU|\\  ";
+        anim[6] = "*---\\/---*";
+        anim[7] = "    ||    ";
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[6] = " ---\\/--- ";
+        anim[7] = "*   ||   *";
+
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[6] = "  --\\/--  ";
+        anim[7] = " /  ||  \\ ";
+        anim[8] = "*   ||   *";
+
+        for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+        //Sleep(300);
+        //system("cls");
+
+        anim[7] = " /  ||  \\ ";
+        anim[8] = "/   ||   \\";
+
+
+    }
+    for (int i = 0; i < b; i++) cout << anim[i] << "\n";
+
+    return 0;
 }
+
+
