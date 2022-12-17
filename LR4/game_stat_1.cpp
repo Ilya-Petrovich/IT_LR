5c7f0a8f38e60192c93edbdb5e8c986d5afcb51c
Karina Pavlova <karilova493@gmail.com>
2022-12-17 16:29:18 +0300

    Changed game_stat_1.cpp

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main()
-{
+int main() {
 	int t = 0;
 	double f, s, fct, d, fc = 0, fr = 0, nd;
 	cout << "How much fuel do you have at start? "; cin >> f;
-	if (f >= 0 && f <= 50)
-	{
-		if (f < 25)
-		{
+	if (f >= 0 && f <= 50) {
+		if (f < 25) {
 			f = f + 25;
 		}
 		cout << "\nCar stats" << "\nFuel: " << f << "\nFuel capacity: 50" << "\nFuel consumption: 0" << "\nSpeed: 0"
@@ -17,30 +14,25 @@ int main()
 			<< "\nYou have to drive a route 360 km long." << "\nBut first you need to check if you have enough fuel."
 			<< "\n\nWhat speed will you choose? "; cin >> s;
 		cout << "\nYour speed before start: ";
-		if (s >= 0 && s <= 110)
-		{
+		if (s >= 0 && s <= 110) {
 			cout << s;
 		}
-		else
-		{
+		else {
 			s = 110;
 			cout << s;
 		}
 		cout << " km/h";
-		if (s == 0)
-		{
+		if (s == 0) {
 			fct = 0;
 			d = 0;
 			t = 0;
 		}
-		else
-		{
+		else {
 			fct = s / 10 * s / 10 / 4 - 4.5 * s / 10 + 30;
 			d = f / fct * 100;
 			t = d / s * 360;
 		}
-		if (d > 360)
-		{
+		if (d > 360) {
 			d = 360;
 		}
 		fc = d / 100 * fct;
@@ -49,17 +41,16 @@ int main()
 		cout << "\n\nFuel consumption with chosen speed: " << fct << " litres per 100 km" << "\n\nDestination result"
 			<< "\nDistance traveled: " << d << " km" << "\nTime spent: " << t << " sec" << "\nFuel consumed: "
 			<< fc << " litres" << "\nFuel remained: " << fr << " litres" << "\n----------------------------------------";
-		if (d == 360)
-		{
+		if (d == 360) {
 			cout << "\nCongratulations! You have reached destination point!" << endl;
 		}
-		else
-		{
+		else {
 			cout << "\nYou haven't reached destination point. " << nd << " km left." << endl;
 		}
 	}
-	else
-	{
+	else {
 		cout << "\nInvalid Data" << endl;
 	}
+	system("pause");
+	return 0;
 }
