49e451f8e66f018d18fe83b5224bb35ef39ac3e1
Ksenia Gorodilova <ketksusa10@gmail.com>
2022-12-17 14:28:32 +0300

    Changed LR7

---
+++ LR7/read.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<fstream>
+#include<iostream>
+using namespace std;
+FILE* file;
+FILE* fp;
+
+void readText(char* buff, long fileSize)
+{
+	fread(buff, sizeof(char), fileSize, file);
+	int offset = fileSize - 20 * 20 * 3;
+
+	for (offset; offset < fileSize; offset += 4) {
+		char firstByte = buff[offset] & 3;
+		firstByte <<= 6;
+		char secondByte = buff[offset + 1] & 3;
+		secondByte <<= 4;
+		char thirdByte = buff[offset + 2] & 3;
+		thirdByte <<= 2;
+		char fourthByte = buff[offset + 3] & 3;
+		char byte = firstByte | secondByte | thirdByte | fourthByte;
+		cout << byte;
+	}
+}
+
+int main()
+{
+
+	long fileSize;
+
+	fopen_s(&file, "image.bmp", "rb");// предлагают fopen заменитть на fopen_s ==> запись меняется
+	fopen_s(&fp, "image.bmp", "wb");//если в строке выше ошибка, пишем "r+b"
+
+	fseek(file, 0, SEEK_END);
+	fileSize = ftell(file);
+	rewind(file);
+	char* buff = new char[fileSize]();
+	//cout << readText(char *buff, long fileSize, file);
+
+	fread(buff, sizeof(char), fileSize, file);
+
+	readText(buff, fileSize);
+	return 0;
+}
