3b4b4162978b5fdca9a4cadd9270bfb1e5da99cc
Dmitri Mashurov <mashurov03@mail.ru>
2022-12-19 15:50:33 +0300

    anim

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <windows.h>
+#include <unistd.h>
 using namespace std;
 int main() {
 	const int n = 12;
@@ -22,28 +22,28 @@ int main() {
 		cout << mas[i] << endl;
 	};
 	for (int i = 0; i < 2; i++) {
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[6] = " --|  |-- ";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		system("cls");
+		//system("cls");
 		mas[7] = "/  |  |  \\";
 		mas[8] = "    \\/    ";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[6] = "  -|  |---";
 		mas[7] = " / |  |   ";
 		mas[8] = "/   \\/    ";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[5] = "    ||   /";
 		mas[6] = " --|  |-- ";
 		mas[7] = "/  |  |   ";
@@ -51,8 +51,8 @@ int main() {
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(400);
-		system("cls");
+		//Sleep(400);
+		//system("cls");
 		mas[4] = "   \\__/  /";
 		mas[5] = "    ||  / ";
 		mas[6] = "  -|  |-  ";
@@ -61,8 +61,8 @@ int main() {
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[4] = "   \\__/  *";
 		mas[6] = " --|  |-  ";
 		mas[7] = "/  |  |   ";
@@ -70,68 +70,68 @@ int main() {
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[4] = "   \\__/  /";
 		mas[6] = "---|  |-  ";
 		mas[7] = "   |  |   ";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[4] = "   \\__/  *";
 		mas[5] = "\\   ||  / ";
 		mas[6] = " --|  |-  ";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[4] = "\\  \\__/  /";
 		mas[5] = " \\  ||  / ";
 		mas[6] = "  -|  |-  ";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(400);
-		system("cls");
+		//Sleep(400);
+		//system("cls");
 		mas[4] = "*  \\__/  *";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[4] = "   \\__/   ";
 		mas[5] = "*   ||   *";
 		mas[6] = " --|  |-- ";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(400);
-		system("cls");
+		//Sleep(400);
+		//system("cls");
 		mas[5] = "    ||    ";
 		mas[6] = "*--|  |--*";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[6] = " --|  |-- ";
 		mas[7] = "*  |  |  *";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[6] = "  -|  |-  ";
 		mas[7] = " / |  | \\ ";
 		mas[8] = "*   \\/   *";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
 		}
-		Sleep(300);
-		system("cls");
+		//Sleep(300);
+		//system("cls");
 		mas[8] = "/   \\/   \\";
 		for (int i = 0; i < n; i++) {
 			cout << mas[i] << endl;
