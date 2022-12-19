d971b7f28c5e995fcaf4d428c650efbb5bc8cd9c
Nikiforovich Arina <a79656934465@yandex.ru>
2022-12-19 14:54:17 +0300

    LW4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -7,12 +7,12 @@ int main() {
 	std::cin >> num_2;
 	std::cout << "Type the third number: ";
 	std::cin >> num_3;
-    if ((num_1 > num_2) and (num_1 >= 0) and (num_3 >= 0) and (num_2 > 0)){
+    if ((num_1 > num_2) and (num_1 > 0) and (num_3 >= 0) and (num_2 > 0)){
 	    std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
 	    std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
 	    std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
 	    std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
-	    std::cout << float(num_1) << " / " << float(num_2) << " = " << float(num_1) / float(num_2) << std::endl;
+	    std::cout << float(num_1)<<".0" << " / " << float(num_2)<<".0"  << " = " << float(num_1) / float(num_2) << std::endl;
 	    std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
 	    std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3<< std::endl;
 	    std::cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3;
