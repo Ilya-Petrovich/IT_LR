55e995771404cb1e0f892af1a25b2c788fa3bf16
Nikiforovich Arina <a79656934465@yandex.ru>
2022-12-19 15:40:49 +0300

    LW4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -15,7 +15,7 @@ int main() {
 	    std::cout << float(num_1)<<".0" << " / " << float(num_2)<<".0"  << " = " << float(num_1) / float(num_2) << std::endl;
 	    std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
 	    std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3<< std::endl;
-	    std::cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3;
+	    std::cout << "(" << num_1 << " + " << num_2 << ") * " << num_3 << " = " << (num_1 + num_2) * num_3<< std::endl;
 } else {
 	std::cout << "Invalid data" << std::endl;
 }
