01a0b5a8bc803051702430cc155c4bfa1acf2f32
Khasanshina Eliza <elizakhasanshina4@gmail.com>
2022-12-20 13:50:02 +0300

    Changed game_stat_1.cpp

--- LR4/game_stat_1.cpp
+++ LR4/game_stat_1.cpp
@@ -6,7 +6,7 @@ int main() {
 	float fuelConsumption, s, dis, dis1, lit, l2;
 	int fuel, speed, h=0;
 
-	cout << "How much fuel do you have at start?";
+	cout << "How much fuel do you have at start? ";
 	cin >> fuel;
 
 	if (fuel < 50 and fuel > 0) {
