45fc01f2a3e660915ff6cbd80d06f55a995eab48
vlflex <lecsinvlad@mail.ru>
2022-12-20 23:41:40 +0300

    Laba 7

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,38 +1,79 @@
-#include <stdio.h> 
-#include <iostream> 
- 
-using namespace std; 
- 
-void ReadText(char* buff, long fSize); 
- 
-int main() { 
-    FILE* file; 
-    char filename[100]; 
-    cout << "Input file name"; cin >> filename; 
-    file = fopen(filename, "rb"); 
- 
-    long fSize; 
-    fseek(file, 0, SEEK_END); 
-    fSize = ftell(file); 
-    rewind(file); 
- 
-    char* buff = new char[fSize](); 
-    fread(buff, 1, fSize, file); 
-    ReadText(buff, fSize); 
- 
-    fclose(file); 
-    free(buff); 
-} 
- 
-void ReadText(char* buff, long fSize) { 
-    int i = fSize - 1200, first_byte, second_byte, third_byte, fourth_byte; 
- 
-    while (i < fSize) { 
-        first_byte = buff[i] & 3; 
-        second_byte = buff[i + 1] & 3; 
-        third_byte = buff[i + 2] & 3; 
-        fourth_byte = buff[i + 3] & 3; 
-        cout << (char)((first_byte << 6) | (second_byte << 4) | (third_byte << 2) | fourth_byte); 
-        i += 4; 
-    } 
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+void readText(char* buff, long fileSize);
+void changeColor(char* buff, long fileSize);
+void writeText(char* buff, long fileSize);
+
+int main() {
+
+	FILE* file;
+
+	char filename[10];
+	cin >> filename;
+	file = fopen(filename, "rb");
+
+	long fileSize;
+	fseek(file, 0, SEEK_END);
+	fileSize = ftell(file);
+	rewind(file);
+
+	char* buff = new char[fileSize]();
+	fread(buff, sizeof(char), fileSize, file);
+	readText(buff, fileSize);
+
+	fclose(file);
+	cin >> filename;
+	file = fopen(filename, "wb");
+	changeColor(buff, fileSize);
+	writeText(buff, fileSize);
+	fwrite(buff, sizeof(char), fileSize, file);
+	fclose(file);
+	delete[] buff;
+	return 0;
+}
+void readText(char* buff, long fileSize) {
+	char firstByte, secondByte, thirdByte, fourthByte, unitedByte;
+	const int inter = 3;
+
+	for (int i = 138; i < fileSize; i += 4) {
+		firstByte = (buff[i] & inter) << 6;
+		secondByte = (buff[i + 1] & inter) << 4;	
+		thirdByte = (buff[i + 2] & inter) << 2;	
+		fourthByte = buff[i + 3] & inter;
+		unitedByte = firstByte | secondByte | thirdByte | fourthByte;
+		printf("%c", unitedByte);
+	}
+	cout << endl;
+}
+void changeColor(char* buff, long fileSize) {
+
+	for (int i = 138; i < fileSize; i += 3) {
+		buff[i] = 0;	
+		buff[i + 1] = 0;	
+		buff[i + 2] = 139;
+	} 
+}
+void writeText(char* buff, long fileSize) {
+
+	char text[] = "ISUB-1-22-3";
+	int count = 0, text_size = size(text), inter = 3;
+
+	for (int i = 138; i < fileSize; i += 4) {
+
+		if (count < text_size) {
+			buff[i] = (buff[i] & 0xfc) | ((text[count] >> 6) & inter);
+			buff[i + 1] = (buff[i + 1] & 0xfc) | ((text[count] >> 4) & inter);
+			buff[i + 2] = (buff[i + 2] & 0xfc) | ((text[count] >> 2) & inter);
+			buff[i + 3] = (buff[i + 3] & 0xfc) | (text[count] & inter);
+		}
+		else {
+			buff[i] = buff[i] & 0xfc;
+			buff[i + 1] = (buff[i + 1] & 0xfc) | 0x2;
+			buff[i + 2] = buff[i + 2] & 0xfc;
+			buff[i + 3] = buff[i + 3] & 0xfc; 
+		}
+		count++;
+	}
 }
