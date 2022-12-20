51b5c94a0f20a448d45eabc1cabd316d035ef26c
Anton Kalosha <antonkm221@gmail.com>
2022-12-20 17:41:14 +0300

    LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,7 +1,9 @@
-#include <iostream>//N1.1
+#include <iostream>
 #include <cmath>
 using namespace std;
-int main() {
+
+int main()
+{
 	int num1, num2, num3;
 	cout << "Type the first number: ";
 	cin >> num1;
@@ -9,7 +11,7 @@ int main() {
 	cin >> num2;
 	cout << "Type the third number: ";
 	cin >> num3;
-	if (num1 > num2) {
+	if (num1 > num2 and num 1>=0 and num2 > 0 and num3 >=0) {
 		std::cout << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
 		std::cout << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
 		std::cout << num1 << " * " << num2 << " = " << num1 * num2 << std::endl;
