bde3f167c5a0ac1698f8258ecd78022347db3f64
Islamov Timur <islamovt28@mail.ru>
2022-12-20 22:49:05 +0300

    LR6

--- LR6/animation_2.cpp
+++ LR6/animation_2.cpp
@@ -219,7 +219,7 @@ void jump(string* firstEl, int col, int t) {
 	show(ptpers, col, t);
 	for (int q = 13; q <= 16; q++) {
 		pers[q - 11] = emptyStr;
-
+		pers[q - 10] = emptyStr;
 		pers[q - 9] = a;
 		pers[q - 8] = b;
 		pers[q - 7] = c;
