32e4c4b4cb33f72880936a82b21174f33e7b30c9
eranoviskander <eranoviskander7@gmail.com>
2022-12-21 17:27:18 +0300

    LR7

---
+++ LR9/build/CMakeFiles/LR9.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LR9.dir/src/main.cpp.obj"
+  "CMakeFiles/LR9.dir/src/main.cpp.obj.d"
+  "LR9.exe"
+  "LR9.exe.manifest"
+  "LR9.pdb"
+  "libLR9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LR9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
