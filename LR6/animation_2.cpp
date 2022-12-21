1a10149e86797be174b51eb54ebbfc3ba90c6c88
Anastasia Kirillova <nastyakirilliva2004@gmail.com>
2022-12-21 18:57:49 +0300

    LR6

--- LR6/animation_2.cpp
+++ LR6/animation_2.cpp
@@ -48,35 +48,31 @@ int main()
 		std::cin >> choise;
 		if (choise == 1)
 		{
-			//system("clear");
+			system("clear");
 			dance(a);
 		}
 		else if (choise == 2)
 		{
-			//system("clear");
+			system("clear");
 			run(a);
 		}
 		else if (choise == 3)
 		{
-		    //system("clear");
+		    system("clear");
 		    sit(a);
 		}
 		else if (choise == 4)
 		{
-		    //system("clear");
+		    system("clear");
 		    ball(a);
-		}
-		else
-		{
-		    return 0;
 		}
 	}
 }
 char sit(vector <string> a)
 {
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
     a[7] = "*                                      *";
 	a[8] = "*   _[]_                               *";
 	a[9] = "*   |~~|                               *";
@@ -90,8 +86,8 @@ char sit(vector <string> a)
 	a[17] = "*   |__|                               *";
 	a[18] = "*   _\\/_                               *";
 	print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[7] = "*   _[]_                               *";
 	a[8] = "*   |~~|                               *";
 	a[9] = "*   |oq|                               *";
@@ -105,8 +101,8 @@ char sit(vector <string> a)
     a[17] = "*    ||                                *";
     a[18] = "*    ||                                *";
     print(a);
-    //sleep(1);
-    //system("clear");
+    sleep(1);
+    system("clear");
 	for (int i = 17; i > 13; i--)
 	{
 	    a[i-11] = "*   _[]_                               *";
@@ -123,10 +119,10 @@ char sit(vector <string> a)
 	    a[i] = "*    ||                                *";
 	    a[i+1] = "*                                      *";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	}
-	for (int i = 15; i < 19; i++)
+	for (int i = 14; i < 19; i++)
 	{
 	    a[i-11] = "*   _[]_                               *";
 	    a[i-10] = "*   |~~|                               *";
@@ -142,8 +138,8 @@ char sit(vector <string> a)
 	    a[i] = "*    ||                                *";
 	    a[i-12] = "*                                      *";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	}
 	a[7] = "*                                      *";
 	a[8] = "*   _[]_                               *";
@@ -158,8 +154,8 @@ char sit(vector <string> a)
 	a[17] = "*   |__|                               *";
 	a[18] = "*   _\\/_                               *";
 	print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[7] = "*   _[]_                               *";
 	a[8] = "*   |~~|                               *";
 	a[9] = "*   |oq|                               *";
@@ -173,166 +169,166 @@ char sit(vector <string> a)
     a[17] = "*    ||                                *";
     a[18] = "*   _||_                               *";
     print(a);
-    //sleep(1);
-    //system("clear");
+    sleep(1);
+    system("clear");
     return 0;
 }
 char ball(vector <string> a)
 {
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
     a[14] = "* / |. | \\o                            *";
     a[15] = "*/  |. |  -                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[13] = "*  -\\. /- o                            *";
 	a[14] = "* / |. | \\-                            *";
     a[15] = "*/  |. |                               *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[12] = "*   _><_  o                            *";
 	a[13] = "*  -\\. /---                            *";
 	a[14] = "* / |. |                               *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[11] = "*   \\--/  o                            *";
 	a[12] = "*   _><_                               *";
 	a[13] = "*  -\\. /--/                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[10] = "*  (|<>|) o                            *";
 	a[11] = "*   \\--/                               *";
 	a[13] = "*  -\\. /---                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[9] = "*   |oq|  o                            *";
 	a[10] = "*  (|<>|)                              *";
 	a[13] = "*  -\\. /--                             *";
 	a[14] = "* / |. |  \\                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[8] = "*   |~~|  o                            *";
 	a[9] = "*   |oq|                               *";
 	a[13] = "*  -\\. /-                              *";
 	a[14] = "* / |. | \\                             *";
 	a[15] = "*/  |. |  \\                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[7] = "*   _[]_  o                            *";
 	a[8] = "*   |~~|                               *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[6] = "*        o                             *";
 	a[7] = "*   _[]_                               *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[5] = "*       o                              *";
 	a[6] = "*                                      *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[4] = "*      o                               *";
 	a[5] = "*                                      *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[3] = "*     o                                *";
 	a[4] = "*                                      *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[3] = "*    o                                 *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[3] = "*                                      *";
 	a[4] = "*   o                                  *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[4] = "*                                      *";
 	a[5] = "*  o                                   *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[5] = "*                                      *";
 	a[6] = "* o                                    *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[6] = "*                                      *";
 	a[7] = "*o  _[]_                               *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[7] = "*   _[]_                               *";
 	a[8] = "*o  |~~|                               *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[8] = "*   |~~|                               *";
 	a[9] = "*o  |oq|                               *";
 	a[13] = "* --\\. /-                              *";
 	a[14] = "*/  |. | \\                             *";
 	a[15] = "*   |. |  \\                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[9] = "*   |oq|                               *";
 	a[10] = "*o (|<>|)                              *";
 	a[13] = "*---\\. /-                              *";
 	a[14] = "*   |. | \\                             *";
 	a[15] = "*   |. |  \\                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[10] = "*  (|<>|)                              *";
 	a[11] = "*o  \\--/                               *";
 	a[13] = "*\\--\\. /-                              *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[11] = "*   \\--/                               *";
 	a[12] = "*o  _><_                               *";
 	a[13] = "*---\\. /-                              *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[12] = "*   _><_                               *";
 	a[13] = "*o -\\. /-                              *";
 	a[14] = "*-/ |. | \\                             *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[13] = "*  -\\. /-                              *";
 	a[14] = "*o/ |. | \\                             *";
 	a[15] = "*-  |. |  \\                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	a[14] = "* / |. | \\                             *";
 	a[15] = "*/  |. |  \\                            *";
     print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	return 0;
 }
 char run(vector <string> a)
 {
     int i = 0;
 	print(a);
-	//sleep(1);
-	//system("clear");
+	sleep(1);
+	system("clear");
 	while (i < 28)
 	{
 	    a[7] = "*" + string(i, ' ') + "   _[]_   " + string(28 - i, ' ') + "*";
@@ -348,8 +344,8 @@ char run(vector <string> a)
 	    a[17] = "*" + string(i, ' ') + "    | \\   " + string(28 - i, ' ') + "*";
 	    a[18] = "*" + string(i, ' ') + "   _|  \\_ " + string(28 - i, ' ') + "*";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	    i = i + 1;
 	    a[7] = "*" + string(i, ' ') + "   _[]_   " + string(28 - i, ' ') + "*";
 	    a[8] = "*" + string(i, ' ') + "   |~~|   " + string(28 - i, ' ') + "*";
@@ -364,8 +360,8 @@ char run(vector <string> a)
 	    a[17] = "*" + string(i, ' ') + "   / |    " + string(28 - i, ' ') + "*";
 	    a[18] = "*" + string(i, ' ') + " _/  |_   " + string(28 - i, ' ') + "*";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	    a[7] = "*" + string(i, ' ') + "   _[]_   " + string(28 - i, ' ') + "*";
 	    a[8] = "*" + string(i, ' ') + "   |~~|   " + string(28 - i, ' ') + "*";
 	    a[9] = "*" + string(i, ' ') + "   |oq|   " + string(28 - i, ' ') + "*";
@@ -379,8 +375,8 @@ char run(vector <string> a)
 	    a[17] = "*" + string(i, ' ') + "    ||    " + string(28 - i, ' ') + "*";
 	    a[18] = "*" + string(i, ' ') + "   _||_   " + string(28 - i, ' ') + "*";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	}
 	while (i > 0)
 	{
@@ -394,11 +390,11 @@ char run(vector <string> a)
 	    a[14] = "*" + string(i, ' ') + " / |. | \\ " + string(28 - i, ' ') + "*";
 	    a[15] = "*" + string(i, ' ') + "/  |. |  \\" + string(28 - i, ' ') + "*";
 	    a[16] = "*" + string(i, ' ') + "   |__|   " + string(28 - i, ' ') + "*";
-	    a[17] = "*" + string(i, ' ') + "    | \\   " + string(28 - i, ' ') + "*";
-	    a[18] = "*" + string(i, ' ') + "   _|  \\_ " + string(28 - i, ' ') + "*";
+	    a[17] = "*" + string(i, ' ') + "   / |    " + string(28 - i, ' ') + "*";
+	    a[18] = "*" + string(i, ' ') + " _/  |_   " + string(28 - i, ' ') + "*";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	    i = i - 1;
 	    a[7] = "*" + string(i, ' ') + "   _[]_   " + string(28 - i, ' ') + "*";
 	    a[8] = "*" + string(i, ' ') + "   |~~|   " + string(28 - i, ' ') + "*";
@@ -410,11 +406,11 @@ char run(vector <string> a)
 	    a[14] = "*" + string(i, ' ') + " / |. | \\ " + string(28 - i, ' ') + "*";
 	    a[15] = "*" + string(i, ' ') + "/  |. |  \\" + string(28 - i, ' ') + "*";
 	    a[16] = "*" + string(i, ' ') + "   |__|   " + string(28 - i, ' ') + "*";
-	    a[17] = "*" + string(i, ' ') + "   / |    " + string(28 - i, ' ') + "*";
-	    a[18] = "*" + string(i, ' ') + " _/  |_   " + string(28 - i, ' ') + "*";
+	    a[17] = "*" + string(i, ' ') + "    | \\   " + string(28 - i, ' ') + "*";
+	    a[18] = "*" + string(i, ' ') + "   _|  \\_ " + string(28 - i, ' ') + "*";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	    a[7] = "*" + string(i, ' ') + "   _[]_   " + string(28 - i, ' ') + "*";
 	    a[8] = "*" + string(i, ' ') + "   |~~|   " + string(28 - i, ' ') + "*";
 	    a[9] = "*" + string(i, ' ') + "   |oq|   " + string(28 - i, ' ') + "*";
@@ -428,117 +424,117 @@ char run(vector <string> a)
 	    a[17] = "*" + string(i, ' ') + "    ||    " + string(28 - i, ' ') + "*";
 	    a[18] = "*" + string(i, ' ') + "   _||_   " + string(28 - i, ' ') + "*";
 	    print(a);
-	    //sleep(1);
-	    //system("clear");
+	    sleep(1);
+	    system("clear");
 	}
 	return 0;
 }
 char dance(vector <string> a)
 {
 	print(a);
-	//sleep(1);
+	sleep(1);
 	int i = 0;
 	while (i < 2)
 	{
-		//system("clear");
+		system("clear");
 		a[13] = "* --\\. /--                             *";
 		a[14] = "*/  |. |  \\                            *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[13] = "*  -\\. /---                            *";
 		a[14] = "* / |. |                               *";
 		a[15] = "*/  |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[12] = "*   _><_  /                            *";
 		a[13] = "* --\\. /--                             *";
 		a[14] = "*/  |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[11] = "*   \\--/  /                            *";
 		a[12] = "*   _><_ /                             *";
 		a[13] = "*  -\\. /-                              *";
 		a[14] = "* / |. |                               *";
 		a[15] = "*/  |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[11] = "*   \\--/  *                            *";
 		a[13] = "* --\\. /-                              *";
 		a[14] = "*/  |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[11] = "*   \\--/  /                            *";
 		a[13] = "*---\\. /-                              *";
 		a[14] = "*   |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[11] = "*   \\--/  *                            *";
 		a[12] = "*\\  _><_ /                             *";
 		a[13] = "* --\\. /-                              *";
 		a[14] = "*   |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[11] = "*\\  \\--/  /                            *";
 		a[12] = "* \\ _><_ /                             *";
 		a[13] = "*  -\\. /-                              *";
 		a[14] = "*   |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[11] = "**  \\--/  *                            *";
 		a[12] = "* \\ _><_ /                             *";
 		a[13] = "*  -\\. /-                              *";
 		a[14] = "*   |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[11] = "*   \\--/                               *";
 		a[12] = "**  _><_  *                            *";
 		a[13] = "* --\\. /--                             *";
 		a[14] = "*   |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[12] = "*   _><_                               *";
 		a[13] = "**--\\. /--*                            *";
 		a[14] = "*   |. |                               *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[13] = "* --\\. /--                             *";
 		a[14] = "**  |. |  *                            *";
 		a[15] = "*   |. |                               *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[13] = "*  -\\. /-                              *";
 		a[14] = "* / |. | \\                             *";
 		a[15] = "**  |. |  *                            *";
 		print(a);
-		//sleep(1);
-		//system("clear");
+		sleep(1);
+		system("clear");
 		a[14] = "* / |. | \\                             *";
 		a[15] = "*/  |. |  \\                            *";
 		print(a);
-		//sleep(1);
+		sleep(1);
 		i = i + 1;
 	}
-	//system("clear");
+	system("clear");
 	return 0;
 }
