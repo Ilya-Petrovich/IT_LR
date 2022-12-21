55140b684fccdce2d9be38f79f3050626386cefb
nuris123 <akhmetzyanov.n@mail.ru>
2022-12-21 22:08:26 +0300

    Changed calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 	int num_1, num_2, num_3;
 
-	std::cout << "Type the first number: ";
-	std::cin >> num_1;
+	cout << "Type the first number: ";
+	cin >> num_1;
 
-	std::cout << "Type the second number: ";
-	std::cin >> num_2;
+	cout << "Type the second number: ";
+	cin >> num_2;
 
-	std::cout << "Type the third number: ";
-	std::cin >> num_3;
+	cout << "Type the third number: ";
+	cin >> num_3;
 
 	if (num_1 >= 0 && num_2 > 0 && num_3 >= 0 && num_1 > num_2) {
-		std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
-		std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
-		std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
-		std::cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << std::endl;
-		std::cout << float(num_1) << " / " << float(num_2) << " = " << float(num_1) / float(num_2) << std::endl;
-		std::cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << std::endl;
-		std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
-		std::cout<<"(" << num_1 << " + " << num_2<<")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
+		cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << endl;
+		cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << endl;
+		cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << endl;
+		cout << num_1 << " / " << num_2 << " = " << num_1 / num_2 << endl;
+		cout << float(num_1) << " / " << float(num_2) << " = " << float(num_1) / float(num_2) << endl;
+		cout << num_1 << " % " << num_2 << " = " << num_1 % num_2 << endl;
+		cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << endl;
+		cout<<"(" << num_1 << " + " << num_2<<")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << endl;
 	}
 	else {
-		std::cout << "Invalid data" << std::endl;
+		cout << "Invalid data" << endl;
 	}
-	int a;
-	std::cin >> a;
 }
