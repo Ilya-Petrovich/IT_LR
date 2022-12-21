af1223ca8d502247ec8b01d2b98d6481677f17db
Fedor Tabakov <fedortab7@gmail.com>
2022-12-21 16:54:44 +0300

    LR6

--- LR6/animation_2.cpp
+++ LR6/animation_2.cpp
@@ -170,7 +170,7 @@ void jump(vector<string> person)
     //int jumpDelay = 270 * 1000;
     //usleep(jumpDelay);//system("clear");
     person[7] = "*                                      *";
-    person[8] = "*    /\\                              *";
+    person[8] = "*    /\\                                *";
     person[9] = "*   |oo|                               *";
     person[10] = "*  (|..|)                              *";
     person[11] = "*   |--|                               *";
