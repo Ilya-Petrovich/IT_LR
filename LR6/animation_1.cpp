33acf8f2dffcd3612ddfadc224306853aa918cfb
emre ugurluel <halilemreugurluel@icloud.com>
2022-12-22 12:31:45 +0300

    animations

---
+++ LR6/animation_1.cpp
@@ -0,0 +1,341 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+char a1[11]{ "   _[]_   " };
+char a2[11]{ "   |~~|   " };
+char a3[11]{ "   |oq|   " };
+char a4[11]{ "  (|<>|)  " };
+char a5[11]{ "   \\--/   " };
+char a6[11]{ "   _><_   " };
+char a7[11]{ "  -\\. /-  " };
+char a8[11]{ " / |. | \\ " };
+char a9[11]{ "/  |. |  \\" };
+char a10[11]{ "   |__|   " };
+char a11[11]{ "    ||    " };
+char a12[11]{ "   _||_   " };
+void nach() {
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+
+void arms1() {
+	char a7[11]{ " --\\. /-- " };
+	char a8[11]{ "/  |. |  \\" };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms2() {
+	char a7[11]{ "  -\\. /---" };
+	char a8[11]{ " / |. |   " };
+	char a9[11]{ "/  |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms3() {
+	char a6[11]{ "   _><_  /" };
+	char a7[11]{ " --\\. /-- " };
+	char a8[11]{ "/  |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms4() {
+	char a5[11]{ "   \\--/  /" };
+	char a6[11]{ "   _><_ / " };
+	char a7[11]{ "  -\\. /-  " };
+	char a8[11]{ " / |. |   " };
+	char a9[11]{ "/  |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms5() {
+	char a5[11]{ "   \\--/  *" };
+	char a6[11]{ "   _><_ / " };
+	char a7[11]{ " --\\. /-  " };
+	char a8[11]{ "/  |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms6() {
+	char a5[11]{ "   \\--/  /" };
+	char a6[11]{ "   _><_ / " };
+	char a7[11]{ "---\\. /-  " };
+	char a8[11]{ "   |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms7() {
+	char a5[11]{ "   \\--/  *" };
+	char a6[11]{ "\\  _><_ / " };
+	char a7[11]{ " --\\. /-  " };
+	char a8[11]{ "   |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms8() {
+	char a5[11]{ "\\  \\--/  /" };
+	char a6[11]{ " \\ _><_ / " };
+	char a7[11]{ "  -\\. /-  " };
+	char a8[11]{ "   |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms9() {
+	char a5[11]{ "*  \\--/  *" };
+	char a6[11]{ " \\ _><_ / " };
+	char a7[11]{ "  -\\. /-  " };
+	char a8[11]{ "   |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms10() {
+	char a5[11]{ "   \\--/   " };
+	char a6[11]{ "*  _><_  *" };
+	char a7[11]{ " --\\. /-- " };
+	char a8[11]{ "   |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms11() {
+	char a5[11]{ "   \\--/   " };
+	char a6[11]{ "   _><_   " };
+	char a7[11]{ "*--\\. /--*" };
+	char a8[11]{ "   |. |   " };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms12() {
+	char a5[11]{ "   \\--/   " };
+	char a6[11]{ "   _><_   " };
+	char a7[11]{ " --\\. /-- " };
+	char a8[11]{ "*  |. |  *" };
+	char a9[11]{ "   |. |   " };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms13() {
+	char a5[11]{ "   \\--/   " };
+	char a6[11]{ "   _><_   " };
+	char a7[11]{ "  -\\. /-  " };
+	char a8[11]{ " / |. | \\ " };
+	char a9[11]{ "*  |. |  *" };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+void arms14() {
+	char a5[11]{ "   \\--/   " };
+	char a6[11]{ "   _><_   " };
+	char a7[11]{ "  -\\. /-  " };
+	char a8[11]{ " / |. | \\ " };
+	char a9[11]{ "/  |. |  \\" };
+	cout << a1 << endl;
+	cout << a2 << endl;
+	cout << a3 << endl;
+	cout << a4 << endl;
+	cout << a5 << endl;
+	cout << a6 << endl;
+	cout << a7 << endl;
+	cout << a8 << endl;
+	cout << a9 << endl;
+	cout << a10 << endl;
+	cout << a11 << endl;
+	cout << a12 << endl;
+}
+int main() {
+	nach();
+	//Sleep(1500);
+	//system("cls");
+	for (int i = 0; i < 2; i++) {
+		arms1();
+		//Sleep(1500);
+		//system("cls");
+		arms2();
+		//Sleep(1500);
+		//system("cls");
+		arms3();
+		//Sleep(1500);
+		//system("cls");
+		arms4();
+		//Sleep(1500);
+		//system("cls");
+		arms5();
+		//Sleep(1500);
+		//system("cls");
+		arms6();
+		//Sleep(1500);
+		//system("cls");
+		arms7();
+		//Sleep(1500);
+		//system("cls");
+		arms8();
+		//Sleep(1500);
+		//system("cls");
+		arms9();
+		//Sleep(1500);
+		//system("cls");
+		arms10();
+		//Sleep(1500);
+		//system("cls");
+		arms11();
+		//Sleep(1500);
+		//system("cls");
+		arms12();
+		//Sleep(1500);
+		//system("cls");
+		arms13();
+		//Sleep(1500);
+		//system("cls");
+		arms14();
+		//Sleep(1500);
+		//system("cls");
+	}
+	return 0;
+}
