88bce4140e823196b04ab9d6ed4b11e2c207077f
Sharafan <sharafanmarina@gmail.com>
2022-12-22 16:49:42 +0300

    animation_1.cpp

---
+++ LR6/animation_1.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <unistd.h>
+
+int main() {
+    for (int i = 0; i < 2; i++) {
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "  -| *|-  " << std::endl;
+        std::cout << " / |  | \\ " << std::endl;
+        std::cout << "/   \\/   \\" << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << " --| *|-- " << std::endl;
+        std::cout << "/  |  |  \\" << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "  -| *|---" << std::endl;
+        std::cout << " / |  |   " << std::endl;
+        std::cout << "/   \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "    ||   /" << std::endl;
+        std::cout << " --| *|-- " << std::endl;
+        std::cout << "/  |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/  /" << std::endl;
+        std::cout << "    ||  / " << std::endl;
+        std::cout << "  -| *|-  " << std::endl;
+        std::cout << " / |  |   " << std::endl;
+        std::cout << "/   \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/  *" << std::endl;
+        std::cout << "    ||  / " << std::endl;
+        std::cout << " --| *|-  " << std::endl;
+        std::cout << "/  |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/  /" << std::endl;
+        std::cout << "    ||  / " << std::endl;
+        std::cout << "---| *|-  " << std::endl;
+        std::cout << "   |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/  *" << std::endl;
+        std::cout << "\\   ||  / " << std::endl;
+        std::cout << " --| *|-  " << std::endl;
+        std::cout << "   |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "\\  \\__/  /" << std::endl;
+        std::cout << " \\  ||  / " << std::endl;
+        std::cout << "  -| *|-  " << std::endl;
+        std::cout << "   |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "*  \\__/  *" << std::endl;
+        std::cout << " \\  ||  / " << std::endl;
+        std::cout << "  -| *|-  " << std::endl;
+        std::cout << "   |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "*   ||   *" << std::endl;
+        std::cout << " --| *|-- " << std::endl;
+        std::cout << "   |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "*--| *|--*" << std::endl;
+        std::cout << "   |  |   " << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << " --| *|-- " << std::endl;
+        std::cout << "*  |  |  *" << std::endl;
+        std::cout << "    \\/    " << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "   |oo|   " << std::endl;
+        std::cout << "  (|..|)  " << std::endl;
+        std::cout << "   |--|   " << std::endl;
+        std::cout << "   \\__/   " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "  -| *|-  " << std::endl;
+        std::cout << " / |  | \\ " << std::endl;
+        std::cout << "*   \\/   *" << std::endl;
+        std::cout << "    /\\    " << std::endl;
+        std::cout << "    ||    " << std::endl;
+        std::cout << "   _||_   " << std::endl;
+    }
+    return 0;
+}
