f31b3d3bb284c46209a0edcfccca7c86df56ffcc
AnastasiyaAllayarova <allayarova.anastasiyaa@mail.ru>
2022-12-23 11:07:38 +0300

    LR 7

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,32 +1,36 @@
-#include<iostream>
-#include<stdio.h>
-#include <cstdlib>
-#define _CRT_SECURE_NO_WARNINGS
-using namespace std;
-void ReadText(char* buff, long fileSize);
+#include <stdio.h>
+#include <iostream>
+
+void ReadText(char* buff, long fSize);
 
 int main() {
-	FILE* file;
-	file = fopen("image.bmp", "rb");
-	cout << " - " << char(0) << endl;
-	long fileSize;
-	fseek(file, 0, SEEK_END);
-	fileSize = ftell(file);
-	rewind(file); 
-	char* buff = new char[fileSize];
-	fread(buff, sizeof(char), fileSize, file);
-	ReadText(buff, fileSize);
-	fclose(file);
-	free(buff);
+    FILE* file;
+    char filename[100];
+    std::cout << "Input file for read: "; std::cin >> filename;
+    file = fopen(filename, "rb");
 
-	return 0;
+    long fSize;
+    fseek(file, 0, SEEK_END);
+    fSize = ftell(file);
+    rewind(file);
+
+    char* buff = new char[fSize]();
+    fread(buff, 1, fSize, file);
+    ReadText(buff, fSize);
+
+    fclose(file);
+    free(buff);
 }
 
-void changeColor(char* buff, long fileSize) {
-	int b = 179, g = 222, r = 245;
-	for (int i = 138; i < fileSize; i += 3) {
-		buff[i] = b;
-		buff[i + 1] = g;
-		buff[i + 2] = r;
-	}
+void ReadText(char* buff, long fSize) {
+    int start_of_byte = fSize - 1200;
+    
+    while (start_of_byte < fSize) {
+        int first_byte = buff[start_of_byte] & 3;	
+        int second_byte = buff[start_of_byte + 1] & 3;
+        int third_byte = buff[start_of_byte + 2] & 3;	
+        int fourth_byte = buff[start_of_byte + 3] & 3;
+        std::cout <<(char) ((first_byte << 6) | (second_byte << 4) | (third_byte << 2) | fourth_byte);
+        start_of_byte += 4;
+    }
 }
