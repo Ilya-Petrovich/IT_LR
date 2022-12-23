d0f4dbc177c87026ed97b8218b7580426212f92d
Bulat Galimov <bulatbulatgit@gmail.com>
2022-12-23 22:19:01 +0300

    calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -3,22 +3,21 @@ using namespace std;
 int main()
 {
 	int a, b, c;
-	setlocale(LC_ALL, "Rus");
-	cout << "¬ведите первое число:"; cin >> a;
-	cout << "¬ведите второе число:"; cin >> b;
-	cout << "¬ведите третье число:"; cin >> c;
+	cout << "Type the first number: "; cin >> a;
+	cout << "Type the second number: "; cin >> b;
+	cout << "Type the third number: "; cin >> c;
 	if (a >= 0 and c >= 0 and b > 0 and a > b) {
-		cout << a << "+" << b << "=" << a + b << '\n';
-		cout << a << "-" << b << "=" << a - b << '\n';
-		cout << a << "*" << b << "=" << a * b << '\n';
-		cout << a << "/" << b << "=" << a / b << '\n';
-		cout << a << "/" << b << "=" << (float)a / b << '\n';
-		cout << a << "%" << b << "=" << a % b << '\n';
-		cout << a << "+" << b << "*" << c << "=" << a + b * c << '\n';
-		cout << "(" << a << "+" << b << ")" << "*" << c << "=" << (a + b) * c << '\n';
-		return 0;
+		cout << a << " + " << b << " = " << a + b << '\n';
+		cout << a << " - " << b << " = " << a - b << '\n';
+		cout << a << " * " << b << " = " << a * b << '\n';
+		cout << a << " / " << b << " = " << a / b << '\n';
+		cout << a << " / " << b << " = " << (float)a / (float)b << '\n';
+		cout << a << " % " << b << " = " << a % b << '\n';
+		cout << a << " + " << b << " * " << c << " = " << a + b * c << '\n';
+		cout << "(" << a << " + " << b << ")" << " * " << c << " = " << (a + b) * c << '\n';
 	}
 	else {
-		cout << "invalid data";
+		cout << "Invalid data";
 	}
+	return 0;
 }
