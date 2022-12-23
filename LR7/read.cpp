71cb642b692b6d5f6c9ae256c3dbc8144124a860
Dmitri Mashurov <mashurov03@mail.ru>
2022-12-23 22:46:16 +0300

    read

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,83 +1,31 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <stdio.h>
-
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream> 
+#include <stdio.h> 
 using namespace std;
-
-void readText(char buff[], long fileSize);
-void changeColor(char* buff, long fileSize);
-void writeText(char* buff, long fileSize);
-
+void readText(char*& buff, long& fileSize);
 int main() {
-	cout << 0;
-	FILE* file;
-	char filename[100] = "image.bmp";
-
-	file = fopen(filename, "rb");
-
-	long fileSize;
-	fseek(file, 0, SEEK_END);
-	fileSize = ftell(file);
-	rewind(file);
-
-	char* buff = new char[fileSize];
-	fread(buff, sizeof(char), fileSize, file);
-	readText(buff, fileSize);
-	fclose(file);
-
-	FILE *file1 = fopen("change_image.bmp", "wb");
-	changeColor(buff, fileSize);
-	writeText(buff, fileSize);
-	fwrite(buff, sizeof(char), fileSize, file1);
-	fclose(file1);
-
-	free(buff);
-
-	return 0;
-
+    char firstByte, secondByte, thirdByte, fourthByte, united_byte, mask = 0x3;
+    char* buff = NULL;
+    long fileSize = 0, start = 138;
+    readText(buff, fileSize);
+    for (int i = 142; i < fileSize; i += 4) {
+        firstByte = (buff[i - 4] & mask) << 6;
+        secondByte = (buff[i - 3] & mask) << 4;
+        thirdByte = (buff[i - 2] & mask) << 2;
+        fourthByte = (buff[i - 1] & mask);
+        united_byte = firstByte | secondByte | thirdByte | fourthByte;
+        cout << united_byte;
+    }
+    return 0;
 }
-
-void readText(char buff[], long fileSize) {
-	char fB, sB, tB, foB, all;
-	int mask = 0x03;
-
-	for (int i = 138; i < fileSize; i += 4) {
-		fB = buff[i] & mask; fB <<= 6;
-		sB = buff[i + 1] & mask; sB <<= 4;
-		tB = buff[i + 2] & mask; tB <<= 2;
-		foB = buff[i + 3] & mask;
-		all = fB | sB | tB | foB;
-		printf("%c", all);
-	}
-}
-
-void changeColor(char* buff, long fileSize) {
-	for (int i = 138; i < fileSize; i += 3) {
-		buff[i] = 211;
-		buff[i + 1] = 85;
-		buff[i + 2] = 186;
-	}
-}
-
-void writeText(char* buff, long fileSize) {
-
-	char text[13] = "TRP-1-22-16";
-	int count = 0;
-
-	for (int i = 138; i < fileSize; i += 4) {
-
-		if (count < 12) {
-			buff[i] = (buff[i] & 0xfc) | ((text[count] >> 6) & 0x3);
-			buff[i + 1] = (buff[i + 1] & 0xfc) | ((text[count] >> 4) & 0x3);
-			buff[i + 2] = (buff[i + 2] & 0xfc) | ((text[count] >> 2) & 0x3);
-			buff[i + 3] = (buff[i + 3] & 0xfc) | (text[count] & 0x3);
-		}
-		else {
-			buff[i] = buff[i] & 0xfc;
-			buff[i + 1] = (buff[i + 1] & 0xfc) | 0x2;
-			buff[i + 2] = buff[i + 2] & 0xfc;
-			buff[i + 3] = buff[i + 3] & 0xfc;
-		}
-		count++;
-	}
+void readText(char*& buff, long& fileSize) {
+    FILE* file;
+    char filename[] = "image.bmp";
+    file = fopen(filename, "rb");
+    fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    rewind(file);
+    buff = new char[fileSize]();
+    fread(buff, sizeof(char), fileSize, file);
+    fwrite(buff, sizeof(char), fileSize, file);
 }
