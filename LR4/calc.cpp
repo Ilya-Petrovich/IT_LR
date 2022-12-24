ba74cb9399c7017668c8bc52d5d5d019b544f91f
Vsevolod Dubolazov <seva2004d@mail.ru>
2022-12-24 10:28:32 +0300

    LR3,LR4,LR6

---
+++ LR4/calc.cpp
@@ -0,0 +1,29 @@
+	#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3;
+	cout << "Type the first number: "; cin >> n1;
+	cout << "Type the second number: "; cin >> n2;
+	cout << "Type the third number: "; cin >> n3;
+	if (n1 > n2) 
+	{
+		if ((n1 >= 0) && (n2 > 0) && (n3 >= 0)) 
+		{
+			cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+			cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
+			cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+			cout << n1 << " / " << n2 << " = " << n1 / n2 << endl;
+			cout << n1 << ".0" << " / " << n2 << ".0" << " = " << n1 * 1. / n2 << endl;
+			cout << n1 << " % " << n2 << " = " << n1 % n2 << endl;
+			cout << n1 << " + " << n2 << " * " << n3 << " = " << n1 + n2 * n3 << endl;
+			cout << "(" << n1 << " + " << n2 << ")" << " * " << n3 << " = " << (n1 + n2) * n3 << endl;
+			
+		} 
+		else cout << "Invalid data" << endl;
+	}
+	else cout << "Invalid data" << endl;
+	return 0;
+}
