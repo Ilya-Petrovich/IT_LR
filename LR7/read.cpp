57bc744cacb5c5935fee82642113029e9740efa6
Ksenia Gorodilova <ketksusa10@gmail.com>
2022-12-24 18:11:22 +0300

    Changed LR7

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,49 +1,36 @@
-#include<stdio.h>
-#include<fstream>
-#include<iostream>
-using namespace std;
-FILE* file;
-FILE* fp;
+#include <stdio.h>
+#include <iostream>
 
-void readText(char* buff, long fileSize)
-{
-	fread(buff, sizeof(char), fileSize, file);
-	int offset = fileSize - 20 * 20 * 3;
+void ReadText(char* buff, long fSize);
 
-	for (offset; offset < fileSize; offset += 4) {
-		char firstByte = buff[offset] & 3;
-		firstByte <<= 6;
-		char secondByte = buff[offset + 1] & 3;
-		secondByte <<= 4;
-		char thirdByte = buff[offset + 2] & 3;
-		thirdByte <<= 2;
-		char fourthByte = buff[offset + 3] & 3;
-		char byte = firstByte | secondByte | thirdByte | fourthByte;
-		cout << byte;
-	}
-}
-void changeColor(char* buff, long fileSize) {
-	fread(buff, sizeof(char), fileSize, file);
-	buff[138] = 192
-	cout << buff;
+int main() {
+    FILE* file;
+    char filename[100];
+    std::cout << "Input file for read: "; std::cin >> filename;
+    file = fopen(filename, "rb");
+
+    long fSize;
+    fseek(file, 0, SEEK_END);
+    fSize = ftell(file);
+    rewind(file);
+
+    char* buff = new char[fSize]();
+    fread(buff, 1, fSize, file);
+    ReadText(buff, fSize);
+
+    fclose(file);
+    free(buff);
 }
 
-int main()
-{
-
-	long fileSize;
-
-	fopen_s(&file, "image.bmp", "rb");// предлагают fopen заменитть на fopen_s ==> запись меняется
-	fopen_s(&fp, "image.bmp", "wb");//если в строке выше ошибка, пишем "r+b"
-
-	fseek(file, 0, SEEK_END);
-	fileSize = ftell(file);
-	rewind(file);
-	char* buff = new char[fileSize]();
-	changeColor(buff, fileSize);
-
-	fread(buff, sizeof(char), fileSize, file);
-
-	//readText(buff, fileSize);
-	return 0;
+void ReadText(char* buff, long fSize) {
+    int start_of_byte = fSize - 1200;
+    
+    while (start_of_byte < fSize) {
+        int first_byte = buff[start_of_byte] & 3;	
+        int second_byte = buff[start_of_byte + 1] & 3;
+        int third_byte = buff[start_of_byte + 2] & 3;	
+        int fourth_byte = buff[start_of_byte + 3] & 3;
+        std::cout <<(char) ((first_byte << 6) | (second_byte << 4) | (third_byte << 2) | fourth_byte);
+        start_of_byte += 4;
+    }
 }
