079c003bd0a3e98e559332d007b7142b387c4fe7
Vladimir1222 <itsmevova1@gmail.com>
2022-12-24 12:29:41 +0530

    Changed

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,15 +1,25 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 int main()
 {
-	int num_1=7, num_2=3;
-	cout << "7+3= " << num_1 + num_2 << endl;
-	cout << "7 - 3 = " << num_1 - num_2 << endl;
-	cout << "7 * 3 = " << num_1 * num_2 << endl;
-	cout << "7 / 3 = " << num_1 / num_2 << endl;
-	cout << "7.0 / 3.0 = " << (float)num_1 / (float)num_2 << endl;
-	cout << "7 % 3 = " << num_1 % num_2 << endl;
-	cout << "7 + 3 * 5 = " << num_1 + num_2 * 5 << endl;
-	cout << "(7 + 3) * 5 = " << (num_1 + num_2) * 5 << endl;
-	return 0;
+    float num_1, num_2, num_3;
+    std::cout << "Type the first number: ";
+    std::cin >> num_1;
+    std::cout << "Type the second number: ";
+    std::cin >> num_2;
+    std::cout << "Type the third number: ";
+    std::cin >> num_3;
+
+    if ((num_1 > num_2) && (num_1, num_3 >= 0) && (num_2 > 0)) {
+        std::cout << num_1 << " + " << num_2 << " = " << num_1 + num_2 << std::endl;
+        std::cout << num_1 << " - " << num_2 << " = " << num_1 - num_2 << std::endl;
+        std::cout << num_1 << " * " << num_2 << " = " << num_1 * num_2 << std::endl;
+        std::cout << num_1 << " / " << num_2 << " = " << int(num_1) / int(num_2) << std::endl;
+        std::cout << num_1 << ".0" << " / " << num_2 << ".0" << " = " << num_1 / num_2 << std::endl;
+        std::cout << num_1 << " % " << num_2 << " = " << int(num_1) % int(num_2) << std::endl;
+        std::cout << num_1 << " + " << num_2 << " * " << num_3 << " = " << num_1 + num_2 * num_3 << std::endl;
+        std::cout << "(" << num_1 << " + " << num_2 << ")" << " * " << num_3 << " = " << (num_1 + num_2) * num_3 << std::endl;
+    }
+    else {
+        std::cout << "Invalid data" << std::endl;
+    }
 }
