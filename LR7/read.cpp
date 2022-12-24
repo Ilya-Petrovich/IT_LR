0fea82f6298522f87277f466919d1ca90bc0d5ce
Aleksey Rudoy <alekseyrudoy228@mail.ru>
2022-12-24 20:26:52 +0300

    xdd

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 
-void readText(char* buff, long fileSize, FILE* file, int smech) {
-	int firstbyte, secondbyte, thirdbyte, fourthbyte, simbol;
-	fread(buff, sizeof(char), fileSize, file);
-	for (int i = smech; i <= fileSize; i += 4) {
-		firstbyte = (buff[i] & 3) << 6;
-		secondbyte = (buff[i + 1] & 3) << 4;
-		thirdbyte = (buff[i + 2] & 3) << 2;
-		fourthbyte = (buff[i + 3] & 3);
-		simbol = (firstbyte | secondbyte | thirdbyte | fourthbyte);
-		std::cout << char(simbol);
-	}
-}
+void changeColor(char* buff, FILE* file, char filename[], long fileSize, int smech);
+void readText(char* buff, long fileSize, FILE* file, int smech);
 
 int main() {
 	FILE* file;
@@ -26,8 +16,21 @@ int main() {
 	char* buff = new char[fileSize]();
 	int smech = fileSize - 20 * 20 * 3;
 	readText(buff, fileSize, file, smech);
-	fclose(file);
 	free(buff);
+	fclose(file);
+}
+
+void readText(char* buff, long fileSize, FILE* file, int smech) {
+	int firstbyte, secondbyte, thirdbyte, fourthbyte, simbol;
+	fread(buff, sizeof(char), fileSize, file);
+	for (int i = smech; i < fileSize; i += 4) {
+		firstbyte = (buff[i] & 3) << 6;
+		secondbyte = (buff[i + 1] & 3) << 4;
+		thirdbyte = (buff[i + 2] & 3) << 2;
+		fourthbyte = (buff[i + 3] & 3);
+		simbol = (firstbyte | secondbyte | thirdbyte | fourthbyte);
+		std::cout << char(simbol);
+	}
 }
 
 
