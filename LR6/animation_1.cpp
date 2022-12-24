fb018e7dd44e274737014ab5322b8571164529ae
Egor Khazov <alecsandr2356@mail.ru>
2022-12-24 19:11:09 +0300

    Changed LR6

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,134 +1,107 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
 #include <unistd.h>
-
 using namespace std;
-const int w = 10, h = 12;
-int main() {
+void print(vector<string>a);
+void anim();
+void print(vector<string>a) {
+    for (auto c : a)
+        cout << c << endl;
+}
+void anim() {
+    vector <string> a = { "   ()()   " ,
+                          "   ()()   " ,
+                          "   ()()   " ,
+                          "   |..|   " ,
+                          "  \\|  |/  " ,
+                          "  /|UU|\\  " ,
+                          "  --\\/--  " ,
+                          " /  ||  \\ " ,
+                          "/   ||   \\" ,
+                          "    /\\    " ,
+                          "    ||    " ,
+                          "   _||_   "
+    };
+    print(a);
+    int i = 0;
 
-
-    string a[h]{
-                        "   ()()   ",//0
-                      "   ()()   ",//1
-                      "   ()()   ",//2
-                      "   |..|   ",//3
-                      "  \\|  |/  ",//4
-                      "  /|UU|\\  ",//5
-                      "  --\\/--  ",//6
-                      " /  ||  \\ ",//7
-                      "/   ||   \\",//8
-                      "    /\\    ",//9
-                      "    ||    ",//10
-                      "   _||_   " };//11
-
-    for (int i = 0; i < 2; i++) {
-
-        for (int i = 0; i < h; i++) cout << a[i] << "\n"; //1
-        //Sleep(1000);
-        //system("cls");
+    while (i < 2) {
         a[6] = " ---\\/--- ";
         a[7] = "/   ||   \\";
         a[8] = "    ||    ";
+        print(a);
 
-
-        for (int i = 0; i < h; i++) cout << a[i] << "\n"; //2
-        //Sleep(1000);
-        //system("cls");
         a[6] = "  --\\/----";
         a[7] = " /  ||    ";
         a[8] = "/   ||    ";
+        print(a);
 
-
-        for (int i = 0; i < h; i++) cout << a[i] << "\n"; //3
-        //Sleep(1000);
-        //system("cls");
         a[5] = "  /|UU|\\ /";
         a[6] = " ---\\/--- ";
         a[7] = "/   ||    ";
         a[8] = "    ||    ";
-
-        for (int i = 0; i < h; i++) cout << a[i] << "\n"; //4
-        //Sleep(1000);
-        //system("cls");
+        print(a);
 
         a[4] = "  \\|  |/ /";
         a[5] = "  /|UU|\\/ ";
         a[6] = "  --\\/--  ";
         a[7] = " /  ||    ";
         a[8] = "/   ||    ";
+        print(a);
 
-        for (int i = 0; i < h; i++) cout << a[i] << "\n"; //5
-        //Sleep(1000);
-        //system("cls");
         a[4] = "  \\|  |/ *";
         a[6] = " ---\\/--  ";
         a[7] = "/   ||    ";
         a[8] = "    ||    ";
+        print(a);
 
-        for (int i = 0; i < h; i++) cout << a[i] << "\n"; //6
-        //Sleep(1000);
-        //system("cls");
         a[4] = "  \\|  |/ /";
         a[6] = "----\\/--  ";
         a[7] = "    ||    ";
         a[8] = "    ||    ";
+        print(a);
 
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//7
-        //Sleep(1000);
-        //system("cls");
         a[4] = "  \\|  |/ *";
         a[5] = "\\ /|UU|\\/ ";
         a[6] = " ---\\/--  ";
+        print(a);
 
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//8
-        //Sleep(1000);
-        //system("cls");
         a[4] = "\\ \\|  |/ /";
         a[5] = " \\/|UU|\\/ ";
         a[6] = "  --\\/--  ";
+        print(a);
 
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//9
-        //Sleep(1000);
-        //system("cls");
         a[4] = "* \\|  |/ *";
+        print(a);
 
-
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//10
-        //Sleep(1000);
-        //system("cls");
         a[4] = "  \\|  |/  ";
         a[5] = "* /|UU|\\ *";
         a[6] = " ---\\/--- ";
+        print(a);
 
-
-
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//11
-        //Sleep(1000);
-        //system("cls");
         a[5] = "  /|UU|\\  ";
         a[6] = "*---\\/---*";
+        print(a);
 
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//12
-        //Sleep(1000);
-        //system("cls");
         a[6] = " ---\\/--- ";
         a[7] = "*   ||   *";
+        print(a);
 
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//13
-        //Sleep(1000);
-        //system("cls");
         a[6] = "  --\\/--  ";
         a[7] = " /  ||  \\ ";
         a[8] = "*   ||   *";
+        print(a);
 
-
-
-        for (int i = 0; i < h; i++) cout << a[i] << "\n";//14
-        //Sleep(1000);
-        //system("cls");
         a[8] = "/   ||   \\";
+        print(a);
 
+        i++;
     }
-    for (int i = 0; i < h; i++) cout << a[i] << "\n";
+}
 
+int main()
+{
+    anim();
     return 0;
 }
