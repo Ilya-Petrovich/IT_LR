1073447cc31538799eef53f7a4cee372bb08a90b
Aleksey Vladislavovich <aleksstalker8690@gmail.com>
2022-12-25 03:17:53 +0300

    lr

---
+++ LR6/animation_1.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<unistd.h>
+
+using namespace std;
+
+void ShowArray(char pers[12][11], int height, int width) {
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width - 1; j++) {
+			cout << pers[i][j];
+		}
+
+		cout << endl;
+	}
+}
+
+void SSS(char pers[][11], int t) {
+	ShowArray(pers, 12, 11);
+	//Sleep(t);
+	//system("cls");
+}
+
+
+int main() {
+	const int h = 12, w = 11, t = 750;
+
+	char pers[h][w] = {
+		{"    /\\    "},
+		{"   |oo|   "},
+		{"  (|..|)  "},
+		{"   |--|   "},
+		{"   \\__/   "},
+		{"    ||    "},
+		{"  -| *|-  "},
+		{" / |  | \\ "},
+		{"/   \\/   \\"},
+		{"    /\\    "},
+		{"    ||    "},
+		{"   _||_   "}
+	};
+
+	SSS(pers, t);
+
+	for (int i = 0; i < 2; i++) {
+		pers[6][1] = '-'; pers[7][1] = ' ';
+		pers[6][8] = '-'; pers[7][8] = ' ';
+		pers[8][0] = ' '; pers[8][9] = ' ';
+		pers[7][0] = '/'; pers[7][9] = '\\';
+
+		SSS(pers, t);
+
+		pers[6][1] = ' '; pers[7][0] = ' ';
+		pers[7][9] = ' '; pers[6][9] = '-';
+		pers[7][1] = '/'; pers[8][0] = '/';
+
+		SSS(pers, t);
+
+		pers[6][9] = ' '; pers[6][1] = '-';
+		pers[7][1] = ' '; pers[8][0] = ' ';
+		pers[7][0] = '/'; pers[5][9] = '/';
+
+		SSS(pers, t);
+
+		pers[6][1] = ' '; pers[7][0] = ' ';
+		pers[7][1] = '/'; pers[8][0] = '/';
+		pers[6][8] = ' '; pers[5][10] = ' ';
+		pers[5][8] = '/'; pers[5][9] = ' ';
+		pers[4][9] = '/';
+
+		SSS(pers, t);
+
+		pers[4][9] = '*'; pers[7][1] = ' ';
+		pers[8][0] = ' '; pers[6][1] = '-';
+		pers[7][0] = '/';
+
+		SSS(pers, t);
+
+		pers[4][9] = '/'; pers[7][0] = ' ';
+		pers[6][0] = '-';
+
+		SSS(pers, t);
+
+		pers[4][9] = '*'; pers[6][0] = ' ';
+		pers[5][0] = '\\';
+
+		SSS(pers, t);
+
+		pers[4][9] = '/'; pers[6][1] = ' ';
+		pers[5][1] = '\\'; pers[4][0] = '\\';
+		pers[5][0] = ' ';
+
+		SSS(pers, t);
+
+		pers[4][0] = '*'; pers[4][9] = '*';
+
+		SSS(pers, t);
+
+		pers[4][0] = ' '; pers[4][9] = ' ';
+		pers[5][1] = ' '; pers[5][8] = ' ';
+		pers[6][1] = '-'; pers[6][8] = '-';
+		pers[5][0] = '*'; pers[5][9] = '*';
+
+		SSS(pers, t);
+
+		pers[5][0] = ' '; pers[5][9] = ' ';
+		pers[6][0] = '*'; pers[6][9] = '*';
+
+		SSS(pers, t);
+
+		pers[6][0] = ' '; pers[6][9] = ' ';
+		pers[7][0] = '*'; pers[7][9] = '*';
+
+		SSS(pers, t);
+
+		pers[7][0] = ' '; pers[7][9] = ' ';
+		pers[6][1] = ' '; pers[6][8] = ' ';
+		pers[7][1] = '/'; pers[7][8] = '\\';
+		pers[8][0] = '*'; pers[8][9] = '*';
+
+		SSS(pers, t);
+
+		pers[8][0] = '/'; pers[8][9] = '\\';
+
+		SSS(pers, t);
+
+	}
+}
