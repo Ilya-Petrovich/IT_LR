b8f1bed659937f1e4d59ca6e2aa24d60db013b3b
Rinal Sadykov <sadykov.rina2l@mail.ru>
2022-12-25 15:01:12 +0300

    7

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,33 +1,52 @@
-#include <iostream>
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 
-void readText(char* buff, long fileSize);
+#include <iostream>
+#include <cstdio>
+#include <conio.h>
+
+FILE* image;
+
+using namespace std;
+
+void readText(char* buff, long& fileSize);
+
+long countFileSize();
 
 int main() {
-	FILE* file;
-	char filename[100];
-	std::cin >> filename;
-	file = fopen(filename, "rb");
-	long fileSize;
-	fseek(file, 0, SEEK_END);
-	fileSize = ftell(file);
-	rewind(file);
-	char* buff = new char[fileSize]();
-	fread(buff, sizeof(char), fileSize, file);
+	long fileSize = countFileSize();
+	char* buff = new char[fileSize];
+
 	readText(buff, fileSize);
-	fclose(file);
-	free(buff);
+
+	delete[] buff;
+	_getch();
 	return 0;
 }
 
-void readText(char* buff, long fileSize) {
-	int start = fileSize - 1200;
-	while (start < fileSize)
-	{
-		int fByte = buff[start] & 3;
-		int sByte = buff[start + 1] & 3;
-		int tByte = buff[start + 2] & 3;
-		int foByte = buff[start + 3] & 3;
-		std::cout << (char)((fByte << 6) | (sByte << 4) | (tByte << 2) | foByte);
-		start += 4;
+void readText(char* buff, long& fileSize) {
+	fread(buff, 1, fileSize, image);
+	fclose(image);
+
+	int offset = fileSize - 20 * 20 * 3;
+
+	for (offset; offset < fileSize; offset += 4) {
+		char firstByte = buff[offset] & 3;
+		firstByte <<= 6;
+		char secondByte = buff[offset + 1] & 3;
+		secondByte <<= 4;
+		char thirdByte = buff[offset + 2] & 3;
+		thirdByte <<= 2;
+		char fourthByte = buff[offset + 3] & 3;
+		char byte = firstByte | secondByte | thirdByte | fourthByte;
+		cout << byte;
 	}
+}
+
+long countFileSize() {
+	image = fopen("changed_image.bmp", "rb");
+	long fileSize;
+	fseek(image, 0, SEEK_END);
+	fileSize = ftell(image);
+	rewind(image);
+	return fileSize;
+}
