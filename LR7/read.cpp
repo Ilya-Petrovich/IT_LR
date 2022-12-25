61cba68e5c4a4d0b1f47abca2053728ef2c7fa28
Vadim Aminov <vadimfdgd@yandex.ru>
2022-12-26 01:33:58 +0300

    777

---
+++ LR7/read.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <conio.h>
+#include <iostream>
+
+
+FILE* image;
+
+
+using namespace std;
+
+void readText(char* buff, long& fileSize);
+
+long countFileSize();
+
+int main() {
+	long fileSize = countFileSize();
+	char* buff = new char[fileSize];
+	readText(buff, fileSize);
+	delete[] buff;
+	_getch();
+	return 0;
+}
+
+void readText(char* buff, long& fileSize) {
+	fread(buff, 1, fileSize, image);
+
+	fclose(image);
+	int offset = fileSize - 20 * 20 * 3;
+	for (offset; offset < fileSize; offset += 4) {
+
+		char firstByte = buff[offset] & 3;
+		firstByte <<= 6;
+
+		char secondByte = buff[offset + 1] & 3;
+		secondByte <<= 4;
+
+		char thirdByte = buff[offset + 2] & 3;
+		thirdByte <<= 2;
+
+		char fourthByte = buff[offset + 3] & 3;
+		char byte = firstByte | secondByte | thirdByte | fourthByte;
+		cout << byte;
+	}
+}
+
+long countFileSize() 
+	{image = fopen("changed_image.bmp", "rb");
+	long fileSize;
+	fseek(image, 0, SEEK_END);
+	fileSize = ftell(image);
+	rewind(image);
+	return fileSize;}
