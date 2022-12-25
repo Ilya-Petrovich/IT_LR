270e3a4eda2059c4ba0fabc6bd5d669045deefd7
Vadim Aminov <vadimfdgd@yandex.ru>
2022-12-26 02:06:24 +0300

    777

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,227 +1,153 @@
 #include <iostream>
 #include <unistd.h>
-#include <stdio.h>
-#include <array>
 
 using namespace std;
+void current(string arr[]);
+void firstStage(string arr[]);
+void secondStage(string arr[]);
+void thirdStage(string arr[]);
+void fourthStage(string arr[]);
+void fifthStage(string arr[]);
+void sixthStage(string arr[]);
+void seventhStage(string arr[]);
+void eightStage(string arr[]);
+void nineStage(string arr[]);
+void tenStage(string arr[]);
+void elevenStage(string arr[]);
+void twelveStage(string arr[]);
+void thirteenStage(string arr[]);
+void finalStage(string arr[]);
 
 int main()
 {
-    int j = 0;
-    for (j; j < 3; j++)
-    {
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(   _><_   )" << '\n';
-        cout << R"(  -\. /-  )" << '\n';
-        cout << R"( / |. | \ )" << '\n';
-        cout << R"(/  |. |  \)" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+	//system("clear");	
+	string S[]
+	{
+			"   _[]_   ",   
+			"   |~~|   ",     
+			"   |oq|   ",     
+			"  (|<>|)  ",    
+			"   \\--/  ",    
+			"   _><_   ",   
+			"  -\\. /-  ",   
+			" / |. | \\ ",    
+			"/  |. |  \\",   
+			"   |__|   ",    
+			"    ||    ",     
+			"   _||_   ",     
+	};
+	void (*func[14])(string[]) = { firstStage, secondStage, thirdStage, fourthStage, fifthStage, sixthStage, seventhStage, eightStage, nineStage, tenStage, elevenStage, twelveStage, thirteenStage, finalStage };
+	for (int z = 0; z < 2; z++) {
+		for (int i = 0; i < 14; i++) {
+			current(S);
+			func[i](S);
+			//system("clear");
+			if (z == 1 && i == 13) current(S);
+		}
+	}
+	return 0;
+}
 
-        //Sleep(100);
-        //system("clr");
-        if (j == 2) {
-            break;
-        }
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(   _><_   )" << '\n';
-        cout << R"( --\. /-- )" << '\n';
-        cout << R"(/  |. |  \)" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(   _><_   )" << '\n';
-        cout << R"(  -\. /---)" << '\n';
-        cout << R"( / |. |   )" << '\n';
-        cout << R"(/  |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+void current(string arr[]) {
+	for (int i = 0; i < 12; i++) {
+		cout << arr[i] << endl;
+	}
+	//sleep(1);
+}
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(   _><_  /)" << '\n';
-        cout << R"( --\. /-- )" << '\n';
-        cout << R"(/  |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/  /)" << '\n';
-        cout << R"(   _><_ / )" << '\n';
-        cout << R"(  -\. /-  )" << '\n';
-        cout << R"( / |. |   )" << '\n';
-        cout << R"(/  |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+void firstStage(string arr[]) {
+	arr[6] = " --\\. /--  ";
+	arr[7] = "/  |. |  \\ ";
+	arr[8] = "   |. |  ";
+}
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/  *)" << '\n';
-        cout << R"(   _><_ / )" << '\n';
-        cout << R"( --\. /-  )" << '\n';
-        cout << R"(/  |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+void secondStage(string arr[]) {
+	arr[6] = "  -\\. /---  ";
+	arr[7] = " / |. |  ";
+	arr[8] = "/  |. |  ";
+}
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/  /)" << '\n';
-        cout << R"(   _><_ / )" << '\n';
-        cout << R"(---\. /-  )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/  *)" << '\n';
-        cout << R"(\  _><_ / )" << '\n';
-        cout << R"( --\. /-  )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+void thirdStage(string arr[]) {
+	arr[5] = "   _><_  / ";
+	arr[6] = " --\\. /--  ";
+	arr[7] = "/  |. |  ";
+	arr[8] = "   |. |  ";
+}
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(\  \--/  /)" << '\n';
-        cout << R"( \ _><_ / )" << '\n';
-        cout << R"(  -\. /-  )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(*  \--/  *)" << '\n';
-        cout << R"( \ _><_ / )" << '\n';
-        cout << R"(  -\. /-  )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+void fourthStage(string arr[]) {
+	arr[4] = "   \\--/  / ";
+	arr[5] = "   _><_ / ";
+	arr[6] = "  -\\. /-  ";
+	arr[7] = " / |. |  ";
+	arr[8] = "/  |. |  ";
+}
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(*  _><_  *)" << '\n';
-        cout << R"( --\. /-- )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(   _><_   )" << '\n';
-        cout << R"(*--\. /--*)" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+void fifthStage(string arr[]) {
+	arr[4] = "   \\--/  * ";
+	arr[5] = "   _><_ / ";
+	arr[6] = " --\\. /-  ";
+	arr[7] = "/  |. |  ";
+	arr[8] = "   |. |  ";
+}
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(   _><_   )" << '\n';
-        cout << R"( --\. /-- )" << '\n';
-        cout << R"(*  |. |  *)" << '\n';
-        cout << R"(   |. |   )" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
 
-        //Sleep(100);
-        //system("clr");
-        cout << R"(   _[]_   )" << '\n';
-        cout << R"(   |~~|   )" << '\n';
-        cout << R"(   |oq|   )" << '\n';
-        cout << R"(  (|<>|)  )" << '\n';
-        cout << R"(   \--/   )" << '\n';
-        cout << R"(   _><_   )" << '\n';
-        cout << R"(  -\. /-  )" << '\n';
-        cout << R"( / |. | \ )" << '\n';
-        cout << R"(*  |. |  *)" << '\n';
-        cout << R"(   |__|   )" << '\n';
-        cout << R"(    ||    )" << '\n';
-        cout << R"(   _||_   )" << '\n';
+void sixthStage(string arr[]) {
+	arr[4] = "   \\--/  / ";
+	arr[6] = "---\\. /-  ";
+	arr[7] = "   |. |  ";
+}
 
-        //Sleep(100);
-        //system("clr");
-    }
+
+void seventhStage(string arr[]) {
+	arr[4] = "   \\--/  * ";
+	arr[5] = "\\  _><_ / ";
+	arr[6] = " --\\. /-  ";
+}
+
+
+void eightStage(string arr[]) {
+	arr[4] = "\\  \\--/  / ";
+	arr[5] = " \\ _><_ / ";
+	arr[6] = "  -\\. /-  ";
+}
+
+
+void nineStage(string arr[]) {
+	arr[4] = "*  \\--/  * ";
+}
+
+
+void tenStage(string arr[]) {
+	arr[4] = "   \\--/    ";
+	arr[5] = "*  _><_  * ";
+	arr[6] = " --\\. /--  ";
+}
+
+
+void elevenStage(string arr[]) {
+	arr[5] = "   _><_    ";
+	arr[6] = "*--\\. /--*  ";
+}
+
+
+void twelveStage(string arr[]) {
+	arr[6] = " --\\. /--  ";
+	arr[7] = "*  |. |  * ";
+}
+
+
+void thirteenStage(string arr[]) {
+	arr[6] = "  -\\. /-  ";
+	arr[7] = " / |. | \\ ";
+	arr[8] = "*  |. |  *";
+}
+
+
+void finalStage(string arr[]) {
+	arr[8] = "/  |. |  \\ ";
 }
