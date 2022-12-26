cc074d6cf578b9d86666ed8639a368fb498ebea9
Katya Toroshina <toroshinakatya@gmail.com>
2022-12-26 22:17:42 +0300

    LR7

---
+++ LR7/read.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <iostream>
+
+void ChangeColor(char* buff, long fSize);
+void WriteText(char* buff, long fSize);
+
+int main() {
+    FILE* file;
+    char filename[100];
+    std::cout << "Input filename for read: "; std::cin >> filename; // חהוסü גגמההטרü image.bmp
+    file = fopen(filename, "rb");
+
+    long fSize;
+    fseek(file, 0, SEEK_END);
+    fSize = ftell(file);
+    rewind(file);
+
+    char* buff = new char[fSize]();
+    fread(buff, 1, fSize, file);
+    fclose(file);
+
+    std::cout << "Input filename for write: "; std::cin >> filename; // changed_image.bmp גגוסעט
+    file = fopen(filename, "wb");
+
+    ChangeColor(buff, fSize);
+    WriteText(buff, fSize);
+
+    fwrite(buff, fSize, 1, file);
+
+    fclose(file);
+
+    free(buff);
+}
+
+void ChangeColor(char* buff, long fSize) {
+    std::cout << std::endl << std::endl << std::endl;
+    for (int i = 138; i < fSize;) {
+        buff[i] = 205 & 0xfc;
+        buff[i + 1] = 250 & 0xfc;
+        buff[i + 2] = 255 & 0xfc;
+        i += 3;
+    }
+}
+void WriteText(char* buff, long fSize) {
+    std::string text = "TRP-1-22-26";
+    int start_of_byte = fSize - 1200;
+    std::string::iterator it = text.begin();
+    for (std::string::iterator it = text.begin(); it != text.end(); ++it) {
+        char symbol = *it;
+        int first_byte_of_symbol = (int)((symbol & (3 << 6)) >> 6);
+        int second_byte_of_symbol = (int)((symbol & (3 << 4)) >> 4);
+        int third_byte_of_symbol = (int)((symbol & (3 << 2)) >> 2);
+        int fourth_byte_of_symbol = (int)(symbol & 3);
+        buff[start_of_byte] = (buff[start_of_byte] & 0xfc) | first_byte_of_symbol;
+        buff[start_of_byte + 1] = (buff[start_of_byte + 1] & 0xfc) | second_byte_of_symbol;
+        buff[start_of_byte + 2] = (buff[start_of_byte + 2] & 0xfc) | third_byte_of_symbol;
+        buff[start_of_byte + 3] = (buff[start_of_byte + 3] & 0xfc) | fourth_byte_of_symbol;
+        start_of_byte += 4;
+    }
+}
