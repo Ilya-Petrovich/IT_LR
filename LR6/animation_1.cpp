9f760f9aea98db99dd366d04ea2562e3a15c12a3
Vadim Aminov <vadimfdgd@yandex.ru>
2022-12-26 22:15:48 +0300

    777

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,153 +1,157 @@
 #include <iostream>
 #include <unistd.h>
-
 using namespace std;
-void current(string arr[]);
-void firstStage(string arr[]);
-void secondStage(string arr[]);
-void thirdStage(string arr[]);
-void fourthStage(string arr[]);
-void fifthStage(string arr[]);
-void sixthStage(string arr[]);
-void seventhStage(string arr[]);
-void eightStage(string arr[]);
-void nineStage(string arr[]);
-void tenStage(string arr[]);
-void elevenStage(string arr[]);
-void twelveStage(string arr[]);
-void thirteenStage(string arr[]);
-void finalStage(string arr[]);
+//void sleep() {
+//	Sleep(1000);
+	//system("cls");
+//}
+void print(string character[12]) {
+	for (int i = 0; i < 12; i++) {
+		cout << character[i] << endl;
+	}
+}
+void change_character(string person[12], int state) {
+	switch (state) {
+	case 1:
+	
+		person[6] = " --\\. /-- ";
+		person[7] = "/  |. |  \\";
+		person[8] = "   |. |   ";
+		break;
+	case 2:
+	
+		person[6] = "  -\\. /---";
+		person[7] = " / |. |   ";
+		person[8] = "/  |. |   ";
+		break;
+	case 3:
+	
+		person[5] = "   _><_  /";
+		person[6] = " --\\. /-- ";
+		person[7] = "/  |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 4:
+	
+		person[4] = "   \\--/  /";
+		person[5] = "   _><_ / ";
+		person[6] = "  -\\. /-  ";
+		person[7] = " / |. |   ";
+		person[8] = "/  |. |   ";
+		break;
+	case 5:
+	
+		person[4] = "   \\--/  *";
+		person[6] = " --\\. /-  ";
+		person[7] = "/  |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 6:
+	
+		person[4] = "   \\--/  /";
+		person[6] = "---\\. /-  ";
+		person[7] = "   |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 7:
+	
+		person[4] = "   \\--/  *";
+		person[5] = "\\  _><_ / ";
+		person[6] = " --\\. /-  ";
+		person[7] = "   |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 8:
+	
+		person[4] = "\\  \\--/  /";
+		person[5] = " \\ _><_ / ";
+		person[6] = "  -\\. /-  ";
+		person[7] = "   |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 9:
+	
+		person[4] = "*  \\--/  *";
+		person[5] = " \\ _><_ / ";
+		person[6] = "  -\\. /-  ";
+		person[7] = "   |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 10:
+	
+		person[4] = "   \\--/   ";
+		person[5] = "*  _><_  *";
+		person[6] = " --\\. /-- ";
+		person[7] = "   |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 11:
+	
+		person[4] = "   \\--/   ";
+		person[5] = "   _><_   ";
+		person[6] = "*--\\. /--*";
+		person[7] = "   |. |   ";
+		person[8] = "   |. |   ";
+		break;
+	case 12:
+	
+		person[4] = "   \\--/   ";
+		person[5] = "   _><_   ";
+		person[6] = " --\\. /-- ";
+		person[7] = "*  |. |  *";
+		person[8] = "   |. |   ";
+		break;
+	case 13:
+	
+		person[4] = "   \\--/   ";
+		person[5] = "   _><_   ";
+		person[6] = "  -\\. /-  ";
+		person[7] = " / |. | \\ ";
+		person[8] = "*  |. |  *";
+		break;
+	case 14:
+	
+		person[4] = "   \\--/   ";
+		person[5] = "   _><_   ";
+		person[6] = "  -\\. /-  ";
+		person[7] = " / |. | \\ ";
+		person[8] = "/  |. |  \\";
+		break;
+	}
+}
 
-int main()
-{
-	//system("clear");	
-	string S[]
-	{
-			"   _[]_   ",   
-			"   |~~|   ",     
-			"   |oq|   ",     
-			"  (|<>|)  ",    
-			"   \\--/  ",    
-			"   _><_   ",   
-			"  -\\. /-  ",   
-			" / |. | \\ ",    
-			"/  |. |  \\",   
-			"   |__|   ",    
-			"    ||    ",     
-			"   _||_   ",     
-	};
-	void (*func[14])(string[]) = { firstStage, secondStage, thirdStage, fourthStage, fifthStage, sixthStage, seventhStage, eightStage, nineStage, tenStage, elevenStage, twelveStage, thirteenStage, finalStage };
-	for (int z = 0; z < 2; z++) {
-		for (int i = 0; i < 14; i++) {
-			current(S);
-			func[i](S);
-			//system("clear");
-			if (z == 1 && i == 13) current(S);
+
+
+int main() {
+	
+	int c = 1;
+	
+	string character[12]{
+		"   _[]_   ",//0
+		"   |~~|   ",//1
+		"   |oq|   ",//2
+		"  (|<>|)  ",//3
+	  R"(   \--/   )",//4
+		"   _><_   ",//5
+	  R"(  -\. /-  )",//6
+      R"( / |. | \ )",//7
+	  R"(/  |. |  \)",//8
+		"   |__|   ",//9
+		"    ||    ",//10
+		"   _||_   " };//11
+	print(character);
+	for (int i = 0; i < 2; i++) {
+		
+		for (int j = 1; j <= 14; j++) {
+			
+			change_character(character, j);
+			print(character);
+			//sleep();
 		}
 	}
+	//change_character(character, 14);
+	//print(character);
+	//sleep();
+	
 	return 0;
-}
-
-
-void current(string arr[]) {
-	for (int i = 0; i < 12; i++) {
-		cout << arr[i] << endl;
-	}
-	//sleep(1);
-}
-
-
-void firstStage(string arr[]) {
-	arr[6] = " --\\. /--  ";
-	arr[7] = "/  |. |  \\ ";
-	arr[8] = "   |. |  ";
-}
-
-void secondStage(string arr[]) {
-	arr[6] = "  -\\. /---  ";
-	arr[7] = " / |. |  ";
-	arr[8] = "/  |. |  ";
-}
-
-
-void thirdStage(string arr[]) {
-	arr[5] = "   _><_  / ";
-	arr[6] = " --\\. /--  ";
-	arr[7] = "/  |. |  ";
-	arr[8] = "   |. |  ";
-}
-
-
-void fourthStage(string arr[]) {
-	arr[4] = "   \\--/  / ";
-	arr[5] = "   _><_ / ";
-	arr[6] = "  -\\. /-  ";
-	arr[7] = " / |. |  ";
-	arr[8] = "/  |. |  ";
-}
-
-
-void fifthStage(string arr[]) {
-	arr[4] = "   \\--/  * ";
-	arr[5] = "   _><_ / ";
-	arr[6] = " --\\. /-  ";
-	arr[7] = "/  |. |  ";
-	arr[8] = "   |. |  ";
-}
-
-
-void sixthStage(string arr[]) {
-	arr[4] = "   \\--/  / ";
-	arr[6] = "---\\. /-  ";
-	arr[7] = "   |. |  ";
-}
-
-
-void seventhStage(string arr[]) {
-	arr[4] = "   \\--/  * ";
-	arr[5] = "\\  _><_ / ";
-	arr[6] = " --\\. /-  ";
-}
-
-
-void eightStage(string arr[]) {
-	arr[4] = "\\  \\--/  / ";
-	arr[5] = " \\ _><_ / ";
-	arr[6] = "  -\\. /-  ";
-}
-
-
-void nineStage(string arr[]) {
-	arr[4] = "*  \\--/  * ";
-}
-
-
-void tenStage(string arr[]) {
-	arr[4] = "   \\--/    ";
-	arr[5] = "*  _><_  * ";
-	arr[6] = " --\\. /--  ";
-}
-
-
-void elevenStage(string arr[]) {
-	arr[5] = "   _><_    ";
-	arr[6] = "*--\\. /--*  ";
-}
-
-
-void twelveStage(string arr[]) {
-	arr[6] = " --\\. /--  ";
-	arr[7] = "*  |. |  * ";
-}
-
-
-void thirteenStage(string arr[]) {
-	arr[6] = "  -\\. /-  ";
-	arr[7] = " / |. | \\ ";
-	arr[8] = "*  |. |  *";
-}
-
-
-void finalStage(string arr[]) {
-	arr[8] = "/  |. |  \\ ";
 }
