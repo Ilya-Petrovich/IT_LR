c11c2b1c6187d0b2d71195956b273fb2495acf69
Guzel Khairullina <guzel1116@mail.ru>
2022-12-26 09:21:45 +0300

    Changed LR7

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,79 +1,39 @@
-//#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
-void writeText(char* buff, long fileSize);
-void readText(char* buff, long fileSize);
-void changeColor(char* buff, long fileSize);
+void ReadText(char* buff, long fSize);
 
 int main() {
-	FILE* file;
+    FILE* file;
+    char filename[100];
+    cout << "Input file name"; cin >> filename;
+    file = fopen(filename, "rb");
 
-	char filename[100];
-	cin >> filename;
+    long fSize;
+    fseek(file, 0, SEEK_END);
+    fSize = ftell(file);
+    rewind(file);
 
-	file = fopen(filename, "rb");
-	long fileSize;
-	fseek(file, 0, SEEK_END);
-	fileSize = ftell(file);
-	rewind(file);
-	char* buff = new char[fileSize]();
-	fread(buff, sizeof(char), fileSize, file);
-	readText(buff, fileSize);
+    char* buff = new char[fSize]();
+    fread(buff, 1, fSize, file);
+    ReadText(buff, fSize);
 
-	file = fopen(filename, "wb");
-	//changeColor(buff, fileSize);
-	//writeText(buff, fileSize);
-	fwrite(buff, sizeof(char), fileSize, file);
-
-	fclose(file);
-	free(buff);
-	return 0;
+    fclose(file);
+    free(buff);
+    return 0;
 }
 
-void changeColor(char* buff, long fileSize) {
-	int m = 3, b = 230, g = 216, r = 173;
+void ReadText(char* buff, long fSize) {
+    int i = fSize - 1200;
 
-	for (int i = 138; i < fileSize; i += 3) {
-		buff[i] = b;
-		buff[i + 1] = g;
-		buff[i + 2] = r;
-	}
-}
-
-void readText(char* buff, long fileSize) {
-	int fsize = fileSize, m = 3, eb;
-	unsigned int fb, sb, tb, fob;
-
-	for (int i = 138; i < fsize; i += 4) {
-		fb = (buff[i] & m) << 6;
-		sb = (buff[i + 1] & m) << 4;
-		tb = (buff[i + 2] & m) << 2;
-		fob = (buff[i + 3] & m);
-		eb = fb | sb | tb | fob;
-		printf("%c", eb);
-	}
-}
-
-void writeText(char* buff, long fileSize) {
-	char n[10] = { 'P','I','-','2','-','2','2','-','2','2' };
-	int count = 0,clean_byte = 252,ten_byte=2;
-
-	for (int i = 138; i < fileSize; i += 4) {
-
-		if (count < 10) {
-			buff[i] = (buff[i] & clean_byte) | ((n[count] >> 6) & 3);
-			buff[i + 1] = (buff[i + 1] & clean_byte) | ((n[count] >> 4) & 3);
-			buff[i + 2] = (buff[i + 2] & clean_byte) | ((n[count] >> 2) & 3);
-			buff[i + 3] = (buff[i + 3] & clean_byte) | ((n[count]) & 3);
-		} else {
-			buff[i] = buff[i] & clean_byte;
-			buff[i + 1] = (buff[i+ 1] & clean_byte) | ten_byte;
-			buff[i + 2] = buff[i + 2] & clean_byte;
-			buff[i + 3] = buff[i + 3] & clean_byte;	
-		}
-		count++;
-	}
+    while (i < fSize) {
+        int first_byte = buff[i] & 3;
+        int second_byte = buff[i + 1] & 3;
+        int third_byte = buff[i + 2] & 3;
+        int fourth_byte = buff[i + 3] & 3;
+        cout << (char)((first_byte << 6) | (second_byte << 4) | (third_byte << 2) | fourth_byte);
+        i += 4;
+    }
 }
