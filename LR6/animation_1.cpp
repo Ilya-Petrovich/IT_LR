7cb9f0a35b6c57f02c6d9809f80c573ab4f4d170
PoL-3 <pauvel.makrow.russkiy@gmail.com>
2022-12-26 19:44:20 +0300

    LR6

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,137 +1,126 @@
 #include<iostream>
 #include<unistd.h>
+
 using namespace std;
-void concl(char pers[12][11], int h, int w);
+
+void ShowArray(char pers[12][11], int height, int width) {
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width - 1; j++) {
+			cout << pers[i][j];
+		}
+
+		cout << endl;
+	}
+}
+
+void SSS(char pers[][11], int t) {
+	ShowArray(pers, 12, 11);
+	//Sleep(t);
+	//system("cls");
+}
+
 
 int main() {
-	const int W = 11, H = 12;
-	int i = 0;
-	char a = '/', b = ' ', c = '\\', d = '-', e = '*';
-	char pers[H][W] = {
+	const int h = 12, w = 11, t = 750;
+
+	char pers[h][w] = {
 		{"   _[]_   "},
 		{"   |~~|   "},
 		{"   |oq|   "},
 		{"  (|<>|)  "},
 		{"   \\--/   "},
-		{"   _><_  "},
+		{"   _><_   "},
 		{"  -\\. /-  "},
 		{" / |. | \\ "},
 		{"/  |. |  \\"},
 		{"   |__|   "},
-		{"    ||  "},
-		{"   _||_  "},
+		{"    ||    "},
+		{"   _||_   "}
 	};
-	concl(pers, H, W);
-	//Sleep(500);
-	//system("cls");
 
-	while (i != 2) {
-		pers[6][1] = pers[6][8] = d;
-		pers[7][0] = a;
-		pers[7][9] = c;
-		pers[7][8] = pers[8][0] = pers[8][9] = pers[7][1] = b;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+	SSS(pers, t);
 
-		pers[6][9] = d;
-		pers[7][1] = pers[8][0] = a;
-		pers[7][9] = pers[7][0] = pers[6][1] = b;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+	for (int i = 0; i < 2; i++) {
+		pers[6][1] = '-'; pers[7][1] = ' ';
+		pers[6][8] = '-'; pers[7][8] = ' ';
+		pers[8][0] = ' '; pers[8][9] = ' ';
+		pers[7][0] = '/'; pers[7][9] = '\\';
 
-		pers[6][9] = pers[8][0] = pers[7][1] = b;
-		pers[6][1] = d;
-		pers[5][9] = pers[7][0] = a;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		SSS(pers, t);
 
-		pers[6][1] = pers[7][0] = pers[6][8] = pers[5][9] = b;
-		pers[4][9] = pers[7][1] = pers[8][0] = pers[5][8] = a;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		pers[6][1] = ' '; pers[7][0] = ' ';
+		pers[7][9] = ' '; pers[6][9] = '-';
+		pers[7][1] = '/'; pers[8][0] = '/';
 
-		pers[7][1] = pers[8][0] = b;
-		pers[6][1] = d;
-		pers[4][9] = e;
-		pers[7][0] = a;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		SSS(pers, t);
 
-		pers[7][0] = b;
-		pers[6][0] = d;
-		pers[4][9] = a;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		pers[6][9] = ' '; pers[6][1] = '-';
+		pers[7][1] = ' '; pers[8][0] = ' ';
+		pers[7][0] = '/'; pers[5][9] = '/';
 
-		pers[6][0] = b;
-		pers[5][0] = c;
-		pers[4][9] = e;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		SSS(pers, t);
 
-		pers[5][0] = pers[6][1] = b;
-		pers[4][9] = a;
-		pers[4][0] = pers[5][1] = c;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		pers[6][1] = ' '; pers[7][0] = ' ';
+		pers[7][1] = '/'; pers[8][0] = '/';
+		pers[6][8] = ' '; pers[5][10] = ' ';
+		pers[5][8] = '/'; pers[5][9] = ' ';
+		pers[4][9] = '/';
 
-		pers[4][9] = pers[4][0] = e;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		SSS(pers, t);
 
-		pers[4][9] = pers[4][0] = pers[5][1] = pers[5][8] = b;
-		pers[6][1] = pers[6][8] = d;
-		pers[5][0] = pers[5][9] = e;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		pers[4][9] = '*'; pers[7][1] = ' ';
+		pers[8][0] = ' '; pers[6][1] = '-';
+		pers[7][0] = '/';
 
-		pers[5][0] = pers[5][9] = b;
-		pers[6][0] = pers[6][9] = e;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		SSS(pers, t);
 
-		pers[6][0] = pers[6][9] = b;
-		pers[7][0] = pers[7][9] = e;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		pers[4][9] = '/'; pers[7][0] = ' ';
+		pers[6][0] = '-';
 
-		pers[7][0] = pers[7][9] = pers[6][1] = pers[6][8] = b;
-		pers[8][0] = pers[8][9] = e;
-		pers[7][1] = a;
-		pers[7][8] = c;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
+		SSS(pers, t);
 
-		pers[8][0] = a;
-		pers[8][9] = c;
-		concl(pers, H, W);
-		//Sleep(500);
-		//system("cls");
-		i++;
-	}
+		pers[4][9] = '*'; pers[6][0] = ' ';
+		pers[5][0] = '\\';
 
-	return 0;
-}
+		SSS(pers, t);
 
-void concl(char pers[12][11], int h, int w) {
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w - 1; j++) {
-			cout << pers[i][j];
-		}
+		pers[4][9] = '/'; pers[6][1] = ' ';
+		pers[5][1] = '\\'; pers[4][0] = '\\';
+		pers[5][0] = ' ';
 
-		cout << endl;
+		SSS(pers, t);
+
+		pers[4][0] = '*'; pers[4][9] = '*';
+
+		SSS(pers, t);
+
+		pers[4][0] = ' '; pers[4][9] = ' ';
+		pers[5][1] = ' '; pers[5][8] = ' ';
+		pers[6][1] = '-'; pers[6][8] = '-';
+		pers[5][0] = '*'; pers[5][9] = '*';
+
+		SSS(pers, t);
+
+		pers[5][0] = ' '; pers[5][9] = ' ';
+		pers[6][0] = '*'; pers[6][9] = '*';
+
+		SSS(pers, t);
+
+		pers[6][0] = ' '; pers[6][9] = ' ';
+		pers[7][0] = '*'; pers[7][9] = '*';
+
+		SSS(pers, t);
+
+		pers[7][0] = ' '; pers[7][9] = ' ';
+		pers[6][1] = ' '; pers[6][8] = ' ';
+		pers[7][1] = '/'; pers[7][8] = '\\';
+		pers[8][0] = '*'; pers[8][9] = '*';
+
+		SSS(pers, t);
+
+		pers[8][0] = '/'; pers[8][9] = '\\';
+
+		SSS(pers, t);
+
 	}
 }
