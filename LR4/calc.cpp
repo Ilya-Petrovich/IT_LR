33d0dc233736248e1d601e8c61343268103bc64d
PoL-3 <pauvel.makrow.russkiy@gmail.com>
2022-12-26 12:13:58 +0300

    LR4

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
-using namespace std;
-int main() {
-	int a, b, c;
-	cout << "Type the first number:";
-	cin >> a;
-	cout << "Type the second number:";
-	cin >> b;
-	cout << "Type the third number:";
-	cin >> c;
 
-	if ((b > 0)& (c >= 0)& (a > b)) {
-		cout << a << " + " << b << " = " << a + b << endl;
-		cout << a << " - " << b << " = " << a - b << endl;
-		cout << a << " * " << b << " = " << a * b << endl;
-		cout << a << " / " << b << " = " << a / b << endl;
-		cout << a << ".0" << " / " << b << ".0" << " = " << float(a)/float(b) << endl;
-		cout << a << " % " << b << " = " << a % b << endl;
-		cout << a << " + " << b << " * " << c << " = " << a + b * c << endl;
-		cout << "(" << a << " + " << b << ")" << " * " << c << " = " << (a + b) * c << endl;
-	}
-	else {
-		cout << "Invalid data" << endl;
-	}
-	return 0;
+using namespace std;
+
+int main()
+{
+    int n_1, n_2, n_3;
+    cout << "Type the first number: ";
+    cin >> n_1;
+    cout << "Type the second number: ";
+    cin >> n_2;
+    cout << "Type the third number: ";
+    cin >> n_3;
+
+    if (n_1 > n_2 and n_1 >= 0 and n_2 > 0 and n_3 >= 0)
+    {
+        cout << n_1 << " + " << n_2 << " = " << n_1 + n_2 << endl;
+        cout << n_1 << " - " << n_2 << " = " << n_1 - n_2 << endl;
+        cout << n_1 << " * " << n_2 << " = " << n_1 * n_2 << endl;
+        cout << n_1 << " / " << n_2 << " = " << n_1 / n_2 << endl;
+        cout << n_1 << ".0" << " / " << n_2 << ".0" << " = " << double(n_1) / double(n_2) << endl;
+        cout << n_1 << " % " << n_2 << " = " << n_1 % n_2 << endl;
+        cout << n_1 << " + " << n_2 << " * " << n_3 << " = " << n_1 + n_2 * n_3 << endl;
+        cout << "(" << n_1 << " + " << n_2 << ")" << " * " << n_3 << " = " << (n_1 + n_2) * n_3 << endl;
+    }
+    else {
+        cout << "Invalid data" << endl;
+    }
+
 }
