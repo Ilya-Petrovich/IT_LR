3c741ec54de2a8ef0d52c277de3e905d25228fe6
Anastasia Soloveva <solanand04@mail.ru>
2022-12-26 17:02:47 +0300

    LR6/animation_1.cpp

--- LR6/animation_1.cpp
+++ LR6/animation_1.cpp
@@ -1,124 +1,96 @@
-#include <unistd.h>
-#include <vector>
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
-using namespace std;
+#include <stdio.h>
 
-void print(vector<string> person);
-void dance(vector<string> person);
+void ReadText(char* buff, long fileSize)
+{
+    int first_byte, second_byte, third_byte, fourth_byte, symbol;
+    std::string result = "";
+
+    for (int i = fileSize - 1200; i < fileSize; i += 4) {
+        first_byte = buff[i];
+        second_byte = buff[i + 1];
+        third_byte = buff[i + 2];
+        fourth_byte = buff[i + 3];
+
+        symbol = ((first_byte & 3) << 6) | ((second_byte & 3) << 4) | ((third_byte & 3) << 2) | (fourth_byte & 3);
+        result += char(symbol);
+    }
+    std::cout << result << std::endl;
+}
+
+void changeColor(char* buff, long fileSize)
+{
+    FILE* file_changeColor;
+    file_changeColor = fopen("changed_image.bmp", "wb");
+
+    for (int i = fileSize - 1200; i < fileSize; i += 3) {
+        buff[i] = 215; //&252 ???
+        buff[i + 1] = 235;
+        buff[i + 2] = 250;
+    }
+    fwrite(buff, sizeof(char), fileSize, file_changeColor);
+    fclose(file_changeColor);
+}
+
+void writeText(char* buff, long fileSize)
+{
+    int first_part, second_part, third_part, fourth_part;
+    std::string text = "TRP-1-22-24";
+
+    FILE* file_write;
+    file_write = fopen("changed_image.bmp", "rb");
+
+    fread(buff, sizeof(char), fileSize, file_write);
+
+    //cleaning
+    for (int i = fileSize - 1200; i < fileSize; i++) {
+        buff[i] &= 252;
+    }
+
+    //writing
+    for (unsigned int i = 0; i < text.size(); i++) {
+        first_part = (text[i] & 192) >> 6;
+        second_part = (text[i] & 48) >> 4;
+        third_part = (text[i] & 12) >> 2;
+        fourth_part = text[i] & 3;
+
+        buff[fileSize - 1200 + i * 4] = (buff[fileSize - 1200 + i * 4] & 252) | first_part;
+        buff[fileSize - 1200 + i * 4 + 1] = (buff[fileSize - 1200 + i * 4 + 1] & 252) | second_part;
+        buff[fileSize - 1200 + i * 4 + 2] = (buff[fileSize - 1200 + i * 4 + 2] & 252) | third_part;
+        buff[fileSize - 1200 + i * 4 + 3] = (buff[fileSize - 1200 + i * 4 + 3] & 252) | fourth_part;
+    }
+
+    file_write = fopen("changed_image.bmp", "wb");
+
+    fwrite(buff, sizeof(char), fileSize, file_write);
+    fclose(file_write);
+
+}
 
 int main()
 {
-    vector<string> person =
-    {
-    "    /\\    ",//0
-    "   |oo|   ",//1
-    "  (|..|)  ",//2
-    "   |--|   ",//3
-    "   \\__/   ",//4
-    "    ||    ",//5
-    "  -| *|-  ",//6
-    " / |  | \\ ",//7
-    "/   \\/   \\",//8
-    "    /\\    ",//9
-    "    ||    ",//10
-    "   _||_   ",//11
-    };
-    print(person);
-    dance(person);
+    FILE* file;
+    char filename[100];
+    std::cin >> filename;
+    file = fopen(filename, "rb");
+
+    long fileSize;
+    fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    rewind(file);
+
+    char* buff = new char[fileSize]();
+
+    fread(buff, sizeof(char), fileSize, file);
+
+    ReadText(buff, fileSize);
+    changeColor(buff, fileSize);
+
+    writeText(buff, fileSize);
+
+    fclose(file);
+    free(buff);
+
     return 0;
-}
-void print(vector<string> person)
-{
-    for (auto c : person)
-        cout << c << endl;
-}
-void dance(vector<string> person)
-{
-    int delay = 350 * 1000;
-    for (int i = 0; i < 2; i++)
-    {
-        //usleep(delay);
-        //system("clear");
-        person[6] = " --| *|-- ";
-        person[7] = "/  |  |  \\";
-        person[8] = "    \\/    ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[6] = "  -| *|---";
-        person[7] = " / |  |   ";
-        person[8] = "/   \\/    ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[5] = "    ||   /";
-        person[6] = " --| *|-- ";
-        person[7] = "/  |  |   ";
-        person[8] = "    \\/    ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[4] = "   \\__/  /";
-        person[5] = "    ||  / ";
-        person[6] = "  -| *|-  ";
-        person[7] = " / |  |   ";
-        person[8] = "/   \\/    ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[4] = "   \\__/  *";
-        person[6] = " --| *|-  ";
-        person[7] = "/  |  |   ";
-        person[8] = "    \\/    ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[4] = "   \\__/  /";
-        person[6] = "---| *|-  ";
-        person[7] = "   |  |   ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[4] = "   \\__/  *";
-        person[5] = "\\   ||  / ";
-        person[6] = " --| *|-  ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[4] = "\\  \\__/  /";
-        person[5] = " \\  ||  / ";
-        person[6] = "  -| *|-  ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[4] = "*  \\__/  *";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[4] = "   \\__/   ";
-        person[5] = "*   ||   *";
-        person[6] = " --| *|-- ";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[5] = "    ||    ";
-        person[6] = "*--| *|--*";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[6] = " --| *|-- ";
-        person[7] = "*  |  |  *";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[6] = "  -| *|-  ";
-        person[7] = " / |  | \\ ";
-        person[8] = "*   \\/   *";
-        print(person);
-        //usleep(delay);
-        //system("clear");
-        person[8] = "/   \\/   \\";
-        print(person);
-    }
 }
