3c3f761ad887bae249c40f2087fd27e63830b8e5
Bulat Galimov <bulatbulatgit@gmail.com>
2022-12-28 03:39:38 +0300

    read.cpp

--- LR7/read.cpp
+++ LR7/read.cpp
@@ -1,36 +1,31 @@
-#include <stdio.h>
-#include <iostream>
-
-void ReadText(char* buff, long fSize);
-
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream> 
+#include <stdio.h> 
+using namespace std;
+void readText(char*& buff, long& fileSize);
 int main() {
-    FILE* file;
-    char filename[100];
-    std::cin >> filename;
-    file = fopen(filename, "rb");
-
-    long fSize;
-    fseek(file, 0, SEEK_END);
-    fSize = ftell(file);
-    rewind(file);
-
-    char* buff = new char[fSize]();
-    fread(buff, 1, fSize, file);
-    ReadText(buff, fSize);
-
-    fclose(file);
-    free(buff);
-}
-
-void ReadText(char* buff, long fSize) {
-    int start_of_byte = fSize - 1200;
-
-    while (start_of_byte < fSize) {
-        int first_byte = buff[start_of_byte] & 3;
-        int second_byte = buff[start_of_byte + 1] & 3;
-        int third_byte = buff[start_of_byte + 2] & 3;
-        int fourth_byte = buff[start_of_byte + 3] & 3;
-        std::cout << (char)((first_byte << 6) | (second_byte << 4) | (third_byte << 2) | fourth_byte);
-        start_of_byte += 4;
+    char firstByte, secondByte, thirdByte, fourthByte, united_byte, mask = 0x3;
+    char* buff = NULL;
+    long fileSize = 0, start = 138;
+    readText(buff, fileSize);
+    for (int i = 142; i < fileSize; i += 4) {
+        firstByte = (buff[i - 4] & mask) << 6;
+        secondByte = (buff[i - 3] & mask) << 4;
+        thirdByte = (buff[i - 2] & mask) << 2;
+        fourthByte = (buff[i - 1] & mask);
+        united_byte = firstByte | secondByte | thirdByte | fourthByte;
+        cout << united_byte;
     }
+    return 0;
+}
+void readText(char*& buff, long& fileSize) {
+    FILE* file;
+    char filename[] = "image.bmp";
+    file = fopen(filename, "rb");
+    fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    rewind(file);
+    buff = new char[fileSize]();
+    fread(buff, sizeof(char), fileSize, file);
+    fwrite(buff, sizeof(char), fileSize, file);
 }
