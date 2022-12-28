abc7af4acee89844631aee0fe66abaf9a2de5a52
Misha Cheremykhin <mishamisha7266@gmail.com>
2022-12-28 15:58:58 +0300

    calc.cpp

--- LR4/calc.cpp
+++ LR4/calc.cpp
@@ -1,29 +1,30 @@
-#include "stdafx.h"
 #include <iostream>
-#include <math.h>
- using namespace std;
+using namespace std;
+int main() {
+	int num1, num2, num3;
+	cout << "Type the first number: "; cin >> num1;
+	cout << "Type the second number: "; cin >> num2;
+	cout << "Type the third number: "; cin >> num3;
+	if (num1 > 0 && num2 > 0 && num3 > 0) {
+		if (num1 > num2) {
+			cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+			cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+			cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+			cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+			cout << num1 << ".0 / " << num2 << ".0 = " << (float)num1 * 1.0 / float(num2) << endl;
+			cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+			cout << num1 << " + " << num2 << " * " << num3 << " = " << num1 + num2 * num3 << endl;
+			cout << "(" << num1 << " + " << num2 << ")" << " * " << num3 << " = " << (num1 + num2) * num3 << endl;
+			return 0;
+		}
+		else {
+			cout << "Invalid data" << endl;
+			return 0;
+		}
+	}
+	else {
+		cout << "Invalid data" << endl;
+		return 0;
+	}
 
-int main(){
-	setlocale(LC_ALL,"Rus");
-    int a ; 
-	int b ;
-	int c;
-	cout << "¬ведите первое число = ";
-	cin>>a;
-	cout << "¬ведите второе число = ";
-	cin>>b;
-	cout << "¬ведите третье число = ";
-	cin>>c;
-	cout<<endl;
-    cout << a << " + " << b << " = "<<a+b<<endl;
-    cout << a << " - " << b << " = "<<a-b<<endl;
-    cout << a << " * " << b << " = "<<a*b<<endl;
-	cout << a << " / " << b << " = "<< a / b <<endl;
-    cout << a <<".0"<< " / " << b <<".0"<< " = "<< double(a) / double(b) <<endl;
-	cout << a << " % " << b << " = "<< a % b<<endl;
-	cout << a << " + " << b << " * "<< c << " = "<< a + b * c<<endl;
-    cout <<"("<< a << " + " << b <<")"<< " * "<< c << " = "<<( a + b ) * c<<endl;
-    
-
-    return 0;
 }
